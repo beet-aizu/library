7c2acbc2f7c77d092fc6793e718070af246381a8
beet-uoa <aki.bdash@gmail.com>
2016-12-10 13:44:33 +0900

    add mod_pow

--- mod.cpp
+++ mod.cpp
@@ -44,6 +44,17 @@ void euler_phi2(){
   }
 }
 
+
+ll mod_pow(ll x,ll n,ll mod){
+  ll res=1;
+  while(n>0){
+    if(n&1) res=res*x%mod;
+    x=x*x%mod;
+    n>>=1;
+  }
+  return res;
+}
+
 ll mod_fact(ll n,ll p,ll& e){
   e=0;
   if(n==0) return 1;
