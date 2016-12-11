57e713e3dba5671d6fbf77a293875b2522b16423
beet-uoa <aki.bdash@gmail.com>
2016-12-11 20:12:15 +0900

    fix

--- unionfindtree.cpp
+++ unionfindtree.cpp
@@ -1,31 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> r,p;
-void init(int size){
-  r.resize(size,0);
-  p.resize(size,0);
-  for(int i=0;i<size;i++) r[i]=0,p[i]=i;
-}
-
-int find(int x){
-  if(x!=p[x]) p[x]=find(p[x]);
-  return p[x];
-}
-
-bool same(int x,int y){
-  return find(x)==find(y);
-}
-void unite(int x,int y){
-  x=find(x);y=find(y);
-  if(x==y) return;
-  if(r[x]>r[y]) {
-    p[y]=x;
-  }else {
-    p[x]=y;
-    if(r[x]==r[y]) r[y]++;
+struct UnionFind{
+  vector<int> r,p;
+  UnionFind(){}
+  UnionFind(int size){init(size);}
+  void init(int size){
+    r.resize(size,0);
+    p.resize(size,0);
+    for(int i=0;i<size;i++) r[i]=1,p[i]=i;
   }
-}
-
+  int find(int x){
+    return (x==p[x]?x:p[x]=find(p[x]));
+  }
+  bool same(int x,int y){
+    return find(x)==find(y);
+  }
+  void unite(int x,int y){
+    x=find(x);y=find(y);
+    if(x==y) return;
+    if(r[x]<r[y]) swap(x,y);
+    r[x]+=r[y];
+    p[y]=x;
+  }
+};
 int main(){
   return 0;
 }
