99bff97b1f93099d08f4f466366d57c97398b3b4
beet-aizu <aki.bdash@gmail.com>
2017-04-28 15:51:00 +0900

    verify mod

--- mod.cpp
+++ mod.cpp
@@ -44,12 +44,11 @@ void euler_phi2(){
   }
 }
 
-
 int mod_pow(int x,int n,int mod){
   int res=1;
   while(n>0){
-    if(n&1) res=res*x%mod;
-    x=x*x%mod;
+    if(n&1) (res*=x)%=mod;
+    (x*=x)%=mod;
     n>>=1;
   }
   return res;
