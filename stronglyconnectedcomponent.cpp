66f79d889e392acd1837cf0add6548469c85b789
beet-aizu <aki.bdash@gmail.com>
2017-06-12 17:27:37 +0900

    fix scc

--- stronglyconnectedcomponent.cpp
+++ stronglyconnectedcomponent.cpp
@@ -1,43 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-#define MAX_V 200050
-int V;
-vector<int> G[MAX_V],rG[MAX_V],vs;
-bool used[MAX_V];
-int cmp[MAX_V];
-void add_edge(int from,int to){
-  G[from].push_back(to);
-  rG[to].push_back(from);
-}
-void dfs(int v){
-  used[v]=1;
-  for(int i=0;i<(int)G[v].size();i++){
-    if(!used[G[v][i]]) dfs(G[v][i]);
+struct SCC{
+  int V;
+  vector<vector<int> > G,rG,T;
+  vector<int> vs,used,cmp;
+  SCC(){}
+  SCC(int V):V(V){init();}
+  void init(){
+    G.clear();
+    rG.clear();
+    vs.clear();
+    used.clear();
+    cmp.clear();
+    T.clear();
+    
+    G.resize(V);
+    rG.resize(V);
+    used.resize(V);
+    cmp.resize(V);
   }
-  vs.push_back(v);
-}
-void rdfs(int v,int k){
-  used[v]=1;
-  cmp[v]=k;
-  for(int i=0;i<(int)rG[v].size();i++){
-    if(!used[rG[v][i]]) rdfs(rG[v][i],k);
+  void add_edge(int from,int to){
+    G[from].push_back(to);
+    rG[to].push_back(from);
   }
-}
-int scc(){
-  memset(used,0,sizeof(used));
-  vs.clear();
-  for(int v=0;v<V;v++){
-    if(!used[v]) dfs(v);
+  void dfs(int v){
+    used[v]=1;
+    for(int i=0;i<(int)G[v].size();i++){
+      if(!used[G[v][i]]) dfs(G[v][i]);
+    }
+    vs.push_back(v);
   }
-  memset(used,0,sizeof(used));
-  int k=0;
-  for(int i=vs.size()-1;i>=0;i--){
-    if(!used[vs[i]]) rdfs(vs[i],k++);
+  void rdfs(int v,int k){
+    used[v]=1;
+    cmp[v]=k;
+    T[k].push_back(v);
+    for(int i=0;i<(int)rG[v].size();i++){
+      if(!used[rG[v][i]]) rdfs(rG[v][i],k);
+    }
   }
-  return k;
-}
+  
+  int scc(){
+    fill(used.begin(),used.end(),0);
+    vs.clear();
+    for(int v=0;v<V;v++){
+      if(!used[v]) dfs(v);
+    }
+    fill(used.begin(),used.end(),0);
+    int k=0;
+    for(int i=vs.size()-1;i>=0;i--){
+      if(!used[vs[i]]){
+	T.push_back(vector<int>());
+	rdfs(vs[i],k++);
+      }
+    }
+    return k;
+  }
+};
 signed main(){
   
   return 0;
 }
+/*
+verified 2017/06/12
+http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2598
+*/
