d37574ddf941c912d9c6c5c80de8c427dabc88f7
beet-aizu <aki.bdash@gmail.com>
2017-06-29 12:45:19 +0900

    add beet.el

---
+++ beet.el
@@ -0,0 +1,17 @@
+(defun beet () 
+  (interactive) 
+  (let (file buf str from to) 
+    (setq file (read-file-name "File Name: "))
+    (setq buf (find-file-noselect file))
+    (setq str
+	  (with-current-buffer buf
+	    (buffer-substring-no-properties (point-min) (point-max))))
+    (setq from (search "//BEGIN CUT HERE" str))
+    (setq to (search "//END CUT HERE" str))
+    (insert 
+	  (with-current-buffer buf
+	    (buffer-substring-no-properties (+ from 17) (+ to 1))))
+    (insert "\n")))
+
+(define-key global-map (kbd "C-x l") 'beet)
+
