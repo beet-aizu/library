61c2dd900bf10fc7eb3c31d56d2c92e6bc6cd925
beet-aizu <aki.bdash@gmail.com>
2017-09-14 12:23:33 +0900

    fix latte

--- segtree/impl.cpp
+++ segtree/impl.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 template <typename T,typename E>
 struct SegmentTree{
-  typedef function<T(T,T)> F;
   typedef function<T(T,E)> G;
+  typedef function<T(E,E)> H;
   int n;
-  F f;
   G g;
+  H h;
   T d1;
   E d0;
   vector<T> dat;
-  SegmentTree(int n_,F f,G g,T d1,
+  vector<E> laz;
+  SegmentTree(int n_,G g,H h,T d1,E d0,
 	      vector<T> v=vector<T>()){
-    this->f=f;
     this->g=g;
+    this->h=h;
     this->d1=d1;
+    this->d0=d0;
     init(n_);
     if(n_==(int)v.size()) build(n_,v);
   }
@@ -22,33 +24,53 @@ struct SegmentTree{
     n=1;
     while(n<n_) n*=2;
     dat.clear();
-    dat.resize(2*n-1,d1);
+    dat.resize(n,d1);
+    laz.clear();
+    laz.resize(2*n-1,d0);
   }
   void build(int n_, vector<T> v){
-    for(int i=0;i<n_;i++) dat[i+n-1]=v[i];
-    for(int i=n-2;i>=0;i--)
-      dat[i]=f(dat[i*2+1],dat[i*2+2]);
+    for(int i=0;i<n_;i++) dat[i]=v[i];
   }
-  void update(int k,E a){
+  void update(int a,int b,E x,int k,int l,int r){
+    if(r<=a||b<=l) return;
+    if(a<=l&&r<=b){
+      laz[k]=h(laz[k],x);
+      return;
+    }
+    update(a,b,x,k*2+1,l,(l+r)/2);
+    update(a,b,x,k*2+2,(l+r)/2,r);
+  }
+  void update(int a,int b,E x){
+    update(a,b,x,0,0,n);
+  }
+  T query(int k){
+    T c=dat[k];
     k+=n-1;
-    dat[k]=g(dat[k],a);
+    E x=laz[k];
     while(k>0){
       k=(k-1)/2;
-      dat[k]=f(dat[k*2+1],dat[k*2+2]);
+      x=h(x,laz[k]);
     }
-  }
-  T query(int a,int b,int k,int l,int r){
-    if(r<=a||b<=l) return d1;
-    if(a<=l&&r<=b) return dat[k];
-    T vl=query(a,b,k*2+1,l,(l+r)/2);
-    T vr=query(a,b,k*2+2,(l+r)/2,r);
-    return f(vl,vr);
-  }
-  T query(int a,int b){
-    return query(a,b,0,0,n);
+    return g(c,x);
   }
 };
 signed main(){
-  
+  int n,q;
+  cin>>n>>q;
+  SegmentTree<int,int> raq(n,
+			   [](int a,int b){return a+b;},
+			   [](int a,int b){return a+b;},
+			   0,0);
+  for(int i=0;i<q;i++){
+    int c,s,t,x;
+    cin>>c;
+    if(c){
+      cin>>x;
+      cout<<raq.query(x-1)<<endl;
+    }else{
+      cin>>s>>t>>x;
+      raq.update(s-1,t,x);
+    }
+  }
   return 0;
 }
