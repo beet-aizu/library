d29d34780ac00e164cc1bc1b722d41896eaf6af6
beet-aizu <aki.bdash@gmail.com>
2017-10-20 18:13:42 +0900

    acc sa

--- suffixarray.cpp
+++ suffixarray.cpp
@@ -1,88 +1,79 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long
 //BEGIN CUT HERE
 struct SuffixArray{
   int n,k;
   string S;
-  vector<int> r,r2,t,sa,lcp;
+  vector<int> sa,lcp;
   SuffixArray(){}
-  SuffixArray(string S):S(S){init();}
+  SuffixArray(string& S):S(S){init();}
   void init(){
-    n=S.size();
-    r.resize(n+1,0);
-    r2.resize(n+1,0);
-    t.resize(n+1,0);
-    sa.resize(n+1,0);
-    lcp.resize(n+1,0);
+    n=S.length();
     build_sa();
     build_lcp();
     build_rmq();
   }
-  bool compare_sa(int i,int j){
-    if(r[i]!=r[j]) return r[i]<r[j];
-    else{
-      int ri=i+k<=n?r[i+k]:-1;
-      int rj=j+k<=n?r[j+k]:-1;
-      return ri<rj;
-    }
-  }
   void build_sa(){
-    n=S.length();
-    for(int i=0;i<=n;i++){
-      sa[i]=i;
-      r[i]=i<n?S[i]:-1;
-    }
-    for(k=1;k<=n;k*=2){
-      sort(sa.begin(),sa.end(),[&](const int &i, const int &j){
-	  if(r[i]!=r[j]) return r[i]<r[j];
-	  else{
-	    int ri=i+k<=n?r[i+k]:-1;
-	    int rj=j+k<=n?r[j+k]:-1;
-	    return ri<rj;
-	  }
-	});
-      t[sa[0]]=0;
-      for(int i=1;i<=n;i++){
-	t[sa[i]]=t[sa[i-1]]+(compare_sa(sa[i-1],sa[i])?1:0);
-      }
+    sa.clear();
+    sa.resize(n+1,0);
+    S.push_back('$');
+    iota(sa.begin(),sa.end(),0);
+    sort(sa.begin(),sa.end(),
+	 [&](int a,int b){
+	   if(S[a]==S[b]) return a>b;
+	   return S[a]<S[b];
+	 });
+    vector<int> c(n+1,0),r(n+1),cnt(n+1),s(n+1);
+    for(int i=0;i<=n;i++) r[i]=S[i];
+    for(int len=1;len<=n;len*=2){
       for(int i=0;i<=n;i++){
-	r[i]=t[i];
+	 c[sa[i]]=
+	   i>0 &&
+	   r[sa[i-1]]==r[sa[i]] &&
+	   sa[i-1]+len<=n &&
+	   r[sa[i-1]+len/2]==r[sa[i]+len/2] ?
+	   c[sa[i-1]]:i;
       }
+      iota(cnt.begin(),cnt.end(),0);
+      copy(sa.begin(),sa.end(),r.begin());
+      for(int i=0;i<=n;i++){
+	int s1=r[i]-len;
+	if(s1>=0) sa[cnt[c[s1]]++]=s1;
+      }
+      c.swap(r);
     }
-  }
-  bool contains(string T){
-    int a=0,b=S.length()+1;
-    while(a+1<b){
-      int c=(a+b)/2;
-      if(S.compare(sa[c],T.length(),T)<0) a=c;
-      else b=c;
-    }
-    if(b==(int)S.length()+1) b--;
-    return S.compare(sa[b],T.length(),T)==0;
+    S.pop_back();
   }
   
   // O(|T|*log|S|)
-  int count(string T){
+  pair<int,int> count(string& T){
     int sl=S.length(),tl=T.length();
     int a[2],b[2];
     for(int i=0;i<2;i++){
       a[i]=0;
       b[i]=sl;
       while(a[i]+1<b[i]){
-    int c=(a[i]+b[i])/2;
-    if(S.compare(sa[c],tl,T)<0||
-       (i&&S.compare(sa[c],tl,T)==0)) a[i]=c;
-    else b[i]=c;
+	int c=(a[i]+b[i])/2;
+	if(S.compare(sa[c],tl,T)<0||
+	   (i&&S.compare(sa[c],tl,T)==0)) a[i]=c;
+	else b[i]=c;
       }
     }
-    if(S.compare(sa[b[0]],tl,T)!=0) return 0;
+    if(S.compare(sa[b[0]],tl,T)!=0) return make_pair(0,0);
     if(a[1]<sl&&S.compare(sa[a[1]+1],tl,T)==0) a[1]++;
     if(b[0]> 0&&S.compare(sa[b[0]-1],tl,T)==0) b[0]--;
-    return a[1]-b[0]+1;
+    return make_pair(b[0],a[1]+1);
+  }
+  
+  bool contains(string& T){
+    auto tmp=count(T);
+    return tmp.first!=tmp.second;
   }
   
   void build_lcp(){
+    lcp.clear();
+    lcp.resize(n+1,0);
+    vector<int> r2(n+1);
     for(int i=0;i<=n;i++) r2[sa[i]]=i;
     int h=0;
     lcp[0]=0;
@@ -150,7 +141,7 @@ struct SuffixArray{
   }
   
   // O(|T|+log|S|)
-  int count2(string T){
+  pair<int,int> count2(string& T){
     int a[2],b[2];
     int sl=S.length(),tl=T.length();
     for(int i=0;i<2;i++){
@@ -198,29 +189,59 @@ struct SuffixArray{
     if(a[1]<sl&&getlcp(sa[a[1]+1],T,0)==tl) a[1]++;
     if(b[0]> 0&&getlcp(sa[b[0]-1],T,0)==tl) b[0]--;
    
-    if(getlcp(sa[b[0]],T,0)!=tl) return 0;
-   
-    return a[1]-b[0]+1;
+    if(getlcp(sa[b[0]],T,0)!=tl) return make_pair(0,0);
+
+    return make_pair(b[0],a[1]+1);
+  }
+
+  bool contains2(string& T){
+    auto tmp=count2(T);
+    return tmp.first!=tmp.second;
   }
 };
 //END CUT HERE
 
+
+/*//
 char buf[1000001];
 signed main(){
   scanf("%s",buf);
   string T(buf);
   SuffixArray sa(T);
   int q;
-  scanf("%lld",&q);
+  scanf("%d",&q);
   while(q--){
     scanf("%s",buf);
     string P(buf);
-    printf("%lld\n",(int)sa.contains(P));
-    assert(sa.count(P)==sa.count2(P));
+    printf("%d\n",sa.contains2(P));
+    //assert(sa.count(P)==sa.count2(P));
   }
   return 0;
 }
+//*/
 /*
-verified on 2017/06/22
+verified on 2017/10/20
 http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_14_D
+*/
+
+//*/
+char buf[1145141];
+signed main(){
+  int n;
+  scanf("%d",&n);
+  int m;
+  scanf("%d",&m);
+  scanf("%s",buf);
+  string s(buf);
+  SuffixArray sa(s);
+  string t="I";
+  for(int i=0;i<n;i++) t=t+"OI";
+  auto tmp=sa.count2(t);
+  printf("%d\n",tmp.second-tmp.first);
+  return 0;
+}
+//*/
+/*
+  verified on 2017/10/20
+http://joi2009ho.contest.atcoder.jp/tasks/joi2009ho_a
 */
