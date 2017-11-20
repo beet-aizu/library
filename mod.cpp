8f5f06b2c47834f1fd08377d7c77c39f8bd0cab2
beet-aizu <aki.bdash@gmail.com>
2017-11-20 23:06:02 +0900

    fix

--- mod.cpp
+++ mod.cpp
@@ -16,10 +16,10 @@ Int extgcd(Int a,Int b,Int& x,Int& y){
   }
   return d;
 }
-Int mod_inverse(Int a,Int m){
+Int mod_inverse(Int a,Int mod){
   Int x,y;
-  extgcd(a,m,x,y);
-  return (m+x%m)%m;
+  extgcd(a,mod,x,y);
+  return (mod+x%mod)%mod;
 }
 Int euler_phi(Int n){
   Int res=n;
@@ -71,12 +71,12 @@ Int mod_fact(Int n,Int p,Int& e){
   if(n/p%2!=0)return res*(p-fact[n%p]) %p;
   return res*fact[n%p]%p;
 } 
-Int mod_comb(Int n,Int k,Int p){
+Int mod_comb(Int n,Int k,Int mod){
   if(n==k||k==0) return 1;
   Int e1,e2,e3;
-  Int a1=mod_fact(n,p,e1),a2=mod_fact(k,p,e2),a3=mod_fact(n-k,p,e3);
+  Int a1=mod_fact(n,mod,e1),a2=mod_fact(k,mod,e2),a3=mod_fact(n-k,mod,e3);
   if(e1>e2+e3) return 0;
-  return a1*mod_inverse(a2*a3%p,p)%p;
+  return a1*mod_inverse(a2*a3%mod,mod)%mod;
 }
 
 Int mod_comb2(Int n,Int k,Int mod){
