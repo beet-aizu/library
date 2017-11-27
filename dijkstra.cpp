f975da593394f0f671a104e8cf2b5400771ef776
beet-aizu <aki.bdash@gmail.com>
2017-11-27 14:15:40 +0900

    fix

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -3,10 +3,11 @@ using namespace std;
 using Int = long long;
 //BEGIN CUT HERE
 template <typename T>
-vector<T> dijkstra(int s,vector<vector<pair<T,int> > > & G,T INF){
-  using P = pair<T,int>;
+vector<T> dijkstra(int s,vector<vector<pair<int, T> > > & G,T INF){
+  using P = pair<T, int>;
   int n=G.size();
   vector<T> d(n,INF);
+  vector<int> b(n,-1);
   priority_queue<P,vector<P>,greater<P> > q;
   d[s]=0;
   q.push(P(d[s],s));
@@ -14,12 +15,13 @@ vector<T> dijkstra(int s,vector<vector<pair<T,int> > > & G,T INF){
     P p=q.top();q.pop();
     int v=p.second;
     if(d[v]<p.first) continue;
-    for(P e:G[v]){
+    for(auto& e:G[v]){
       int u=e.first;
       T c=e.second;
       if(d[u]>d[v]+c){
 	d[u]=d[v]+c;
-	q.push(P(d[u],u));
+	b[u]=v;
+	q.emplace(d[u],u);
       }
     }
   }
@@ -30,20 +32,20 @@ vector<T> dijkstra(int s,vector<vector<pair<T,int> > > & G,T INF){
 signed main(){
   int n,l;
   cin>>n>>l;
-  using P = pair<Int,int>;
+  using P = pair<int, Int>;
   vector<vector<P> > G(l+1);
   for(int i=0;i<n;i++){
     int a,b,c;
     cin>>a>>b>>c;
-    G[a].push_back(P(b,c));
+    G[a].emplace_back(b,c);
   }
   for(int i=0;i<l;i++)
-    G[i+1].push_back(P(i,0));
+    G[i+1].emplace_back(i,0);
   cout<<dijkstra(0,G,(1LL<<55LL))[l]<<endl;
   return 0;
 }
 
 /*
-verified on 2017/10/29
+verified on 2017/11/27
 http://arc026.contest.atcoder.jp/tasks/arc026_3
 */
