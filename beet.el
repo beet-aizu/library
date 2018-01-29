b24243422c38e3c672a9b33b8d8fc8251c8d8d0b
beet-aizu <aki.bdash@gmail.com>
2018-01-29 15:38:46 +0900

    fix

--- beet.el
+++ beet.el
@@ -1,17 +1,21 @@
 (defun beet () 
-  (interactive) 
-  (let (file buf str from to) 
-    (setq file (read-file-name "File Name: " "~/git-library/"))
-    (setq buf (find-file-noselect file))
-    (setq str
-	  (with-current-buffer buf
-	    (buffer-substring-no-properties (point-min) (point-max))))
-    (setq from (search "//BEGIN CUT HERE" str))
-    (setq to (search "//END CUT HERE" str))
-    (insert 
-	  (with-current-buffer buf
-	    (buffer-substring-no-properties (+ from 17) (+ to 1))))
-    (insert "\n")))
+  (interactive)
+  (let ((pos (point)))
+    (beginning-of-buffer)
+    (goto-char (- (search-forward "//INSERT ABOVE HERE") 19))
+    (let (file buf str from to)
+      (setq file (read-file-name "File Name: " "~/git-library/"))
+      (setq buf (find-file-noselect file))
+      (setq str
+	    (with-current-buffer buf
+	      (buffer-substring-no-properties (point-min) (point-max))))
+      (setq from (search "//BEGIN CUT HERE" str))
+      (setq to (search "//END CUT HERE" str))
+      (insert 
+       (with-current-buffer buf
+	 (buffer-substring-no-properties (+ from 17) (+ to 1))))
+      (insert "\n"))
+    (goto-char pos)))
 
 (define-key global-map (kbd "C-x l") 'beet)
 
