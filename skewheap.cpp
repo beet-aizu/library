9b3ca3d7b375eaa2c3d38a664a6bea39f2ba28ca
beet-aizu <aki.bdash@gmail.com>
2018-02-08 17:12:42 +0900

    fix

--- skewheap.cpp
+++ skewheap.cpp
@@ -58,34 +58,126 @@ struct SkewHeap{
   
 };
 //END CUT HERE
-signed main(){
-  cin.tie(0);
-  ios::sync_with_stdio(0);
+struct ALDS1_9_C{
+  signed solve(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
 
-  using Heap=SkewHeap<int, int>;
-  Heap::F f=[](int a,int b){return a+b;};
-  Heap::G g=[](int a,int b){return a+b;};
-  Heap::C c=[](int a,int b){return a<b;};
+    using Heap=SkewHeap<int, int>;
+    Heap::F f=[](int a,int b){return a+b;};
+    Heap::G g=[](int a,int b){return a+b;};
+    Heap::C c=[](int a,int b){return a<b;};
 
-  Heap heap(f,g,c,0);
-  auto base=heap.push(0);
+    Heap heap(f,g,c,0);
+    auto base=heap.push(0);
   
-  string s;
-  while(cin>>s,s!="end"){
-    int x;
-    if(s=="insert"){
-      cin>>x;
-      base=heap.meld(base,heap.push(x));
+    string s;
+    while(cin>>s,s!="end"){
+      int x;
+      if(s=="insert"){
+	cin>>x;
+	base=heap.meld(base,heap.push(x));
+      }
+      if(s=="extract"){
+	x=heap.top(base);
+	base=heap.pop(base);
+	cout<<x<<endl;
+      }
     }
-    if(s=="extract"){
-      x=heap.top(base);
-      base=heap.pop(base);
-      cout<<x<<endl;
-    }
+    return 0;
   }
-  return 0;
+  /*
+    verified on 2018/02/08
+    http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_9_C&lang=jp
+  */
+};
+
+struct APC001_D{
+  struct UnionFind{
+    int n;
+    vector<int> r,p;
+    UnionFind(){}
+    UnionFind(int sz):n(sz),r(sz,1),p(sz,0){iota(p.begin(),p.end(),0);}
+    int find(int x){
+      return (x==p[x]?x:p[x]=find(p[x]));
+    }
+    bool same(int x,int y){
+      return find(x)==find(y);
+    }
+    void unite(int x,int y){
+      x=find(x);y=find(y);
+      if(x==y) return;
+      if(r[x]<r[y]) swap(x,y);
+      r[x]+=r[y];
+      p[y]=x;
+    }
+  };
+  
+  signed solve(){
+    using Heap = SkewHeap<Int, Int>;
+    Int n,m;
+    cin>>n>>m;
+    vector<Int> a(n);
+    for(Int i=0;i<n;i++) cin>>a[i];
+    
+    Heap::F f=[](auto a,auto b){return a+b;};
+    Heap::G g=[](auto a,auto b){return a+b;};
+    Heap::C c=[](auto a,auto b){return a>b;};
+    
+    Heap heap(f,g,c,0);
+    vector<Heap::Node*> v(n);
+    for(Int i=0;i<n;i++) v[i]=heap.push(a[i]);
+    
+    UnionFind uf(n);
+    for(Int i=0;i<m;i++){
+      Int x,y;
+      cin>>x>>y;
+      x=uf.find(x);y=uf.find(y);
+      uf.unite(x,y);
+      if(uf.find(x)!=x) swap(x,y);
+      v[x]=heap.meld(v[x],v[y]);
+      v[y]=NULL;
+    }
+    
+    if(m==n-1){
+      cout<<0<<endl;
+      return 0;
+    }
+    
+    Heap::Node* base=NULL;
+    
+    Int ans=0,cnt=0;
+    for(Int i=0;i<n;i++){
+      if(uf.find(i)==i){
+	ans+=heap.top(v[i]);
+	v[i]=heap.pop(v[i]);
+	base=heap.meld(base,v[i]);
+	cnt++;
+      }
+    }
+    
+    while(m*2+cnt<(n-1)*2){
+      if(base==NULL){
+	cout<<"Impossible"<<endl;
+	return 0;
+      }
+      ans+=heap.top(base);
+      base=heap.pop(base);
+      cnt++;
+    }
+  
+    cout<<ans<<endl;
+    return 0;
+  }
+  /*
+    verified on 2018/02/08
+    https://beta.atcoder.jp/contests/apc001/tasks/apc001_d
+  */
+};
+
+signed main(){
+  ALDS1_9_C ans;
+  //APC001_D ans;
+  
+  ans.solve();
 }
-/*
-  verified on 2018/02/08
-  http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_9_C&lang=jp
-*/
