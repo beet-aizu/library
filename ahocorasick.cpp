0ec8d300dae9372ea632610121c0ac15a43bd601
beet-aizu <aki.bdash@gmail.com>
2018-05-20 18:41:41 +0900

    verified

--- ahocorasick.cpp
+++ ahocorasick.cpp
@@ -174,7 +174,102 @@ signed TENKA12016_C(){
   https://beta.atcoder.jp/contests/tenka1-2016-final/tasks/tenka1_2016_final_c
 */
 
+signed dp[2][2][501][21][601];
+bool used[2][2][501][21][601];
+signed AOJ_2257(){
+  Int n,m,k;
+  while(cin>>n>>m>>k,n){
+    memset(dp,0,sizeof(dp));
+    memset(used,0,sizeof(used));
+    
+    vector<string> from(n),to(n);
+    for(Int i=0;i<n;i++) cin>>from[i]>>to[i];
+    vector<string> season(k);
+    for(Int i=0;i<k;i++) cin>>season[i];
+
+    Trie<27>::F f=[](char c){return c-'a';};
+    AhoCorasick<26> aho(f);
+    for(Int i=0;i<k;i++) aho.add(season[i],i);
+    aho.build();
+    Int l=aho.v.size();
+    
+    vector<string> v;
+    for(Int i=0;i<n;i++){
+      v.emplace_back(from[i]);
+      v.emplace_back(to[i]);
+    }
+    sort(v.begin(),v.end());
+    v.erase(unique(v.begin(),v.end()),v.end());
+    map<string, Int> idx;
+    for(Int i=0;i<(Int)v.size();i++) idx[v[i]]=i;
+
+    vector<vector<Int> > G(v.size());
+    for(Int i=0;i<n;i++)
+      G[idx[from[i]]].emplace_back(idx[to[i]]);
+
+    const Int MOD = 1e9+7;
+    using T = tuple<Int, Int, Int, Int, Int>;
+    queue<T> q;
+    for(Int i=0;i<(Int)v.size();i++){
+      dp[0][0][i][0][0]=1;
+      q.emplace(0,0,i,0,0);
+    }
+    while(!q.empty()){
+      Int c,d,x,p,pos;
+      tie(c,d,x,p,pos)=q.front();q.pop();      
+      if(d==m) break;
+      Int f=d&1;
+      
+      //cout<<c<<" "<<d<<" "<<x<<" "<<p<<" "<<pos<<endl;
+      //cout<<v[x]<<":"<<dp[c][f][x][p][pos]<<endl;
+      
+      if(p==(Int)v[x].size()){
+	for(Int y:G[x]){
+	  Int npos=aho.move(pos,v[y][0]);
+	  if(aho.cnt[npos]>1) continue;
+	  Int nc=aho.cnt[npos];
+	  if(c&&nc) continue;
+	  dp[c||nc][!f][y][1][npos]+=dp[c][f][x][p][pos];
+	  dp[c||nc][!f][y][1][npos]%=MOD;
+	  if(!used[c||nc][!f][y][1][npos])
+	    q.emplace((c||nc),d+1,y,1,npos);
+	  used[c||nc][!f][y][1][npos]=1;
+	}
+      }else{
+	Int npos=aho.move(pos,v[x][p]);
+	Int np=p+1;
+	if(aho.cnt[npos]>1) goto END;
+	Int nc=aho.cnt[npos];
+	if(c&&nc) goto END;
+	dp[c||nc][!f][x][np][npos]+=dp[c][f][x][p][pos];
+	dp[c||nc][!f][x][np][npos]%=MOD;
+	if(!used[c||nc][!f][x][np][npos])
+	  q.emplace((c||nc),d+1,x,np,npos);
+	used[c||nc][!f][x][np][npos]=1;
+      }
+    END:
+      dp[c][f][x][p][pos]=0;
+      used[c][f][x][p][pos]=0;
+    }
+    Int ans=0;
+    for(Int i=0;i<(Int)v.size();i++){
+      for(Int j=0;j<l;j++){
+	ans+=dp[1][m&1][i][v[i].size()][j];
+	ans%=MOD;
+      }
+    }
+    cout<<ans<<endl;
+  }
+  return 0;
+}
+
+/*
+  verified on 2018/05/20
+  http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2257
+*/
+
 signed main(){
-  TENKA12016_C();
+  //TENKA12016_C();
+  AOJ_2257();
   return 0;
 }
