b8e0ecee45f3830fd3b963045d5fce3fd3521e39
beet-aizu <aki.bdash@gmail.com>
2018-08-28 11:45:52 +0900

    fix

--- kruskal.cpp
+++ kruskal.cpp
@@ -2,32 +2,15 @@
 using namespace std;
 using Int = long long;
 //BEGIN CUT HERE
+template<typename T>
 struct Kruskal{
-
-  struct UnionFind{
-    int n;
-    vector<int> r,p;
-    UnionFind(){}
-    UnionFind(int sz):n(sz),r(sz,1),p(sz,0){iota(p.begin(),p.end(),0);}
-    int find(int x){
-      return (x==p[x]?x:p[x]=find(p[x]));
-    }
-    bool same(int x,int y){
-      return find(x)==find(y);
-    }
-    void unite(int x,int y){
-      x=find(x);y=find(y);
-      if(x==y) return;
-      if(r[x]<r[y]) swap(x,y);
-      r[x]+=r[y];
-      p[y]=x;
-    }
-  };
   
   struct edge{
-    int from,to,cost,used;
+    int from,to;
+    T cost;
+    int used;
     edge(){}
-    edge(int from,int to,int cost):
+    edge(int from,int to,T cost):
       from(from),to(to),cost(cost),used(0){}
     bool operator<(const edge& e) const{
       return cost<e.cost;
@@ -35,31 +18,55 @@ struct Kruskal{
   };
 
   int n;
+  vector<int> p,r;
   vector<edge> edges;
 
   Kruskal(){}
-  Kruskal(int sz):n(sz){}
+  Kruskal(int n):n(n){}
+
+  void init(int n){
+    r.assign(n,1);
+    p.resize(n);
+    iota(p.begin(),p.end(),0);
+  }
   
-  void add_edge(int u,int v,int c){
+  int find(int x){
+    return (x==p[x]?x:p[x]=find(p[x]));
+  }
+
+  bool same(int x,int y){
+    return find(x)==find(y);
+  }
+
+  void unite(int x,int y){
+    x=find(x);y=find(y);
+    if(x==y) return;
+    if(r[x]<r[y]) swap(x,y);
+    r[x]+=r[y];
+    p[y]=x;
+  }
+
+  void add_edge(int u,int v,T c){
     edges.emplace_back(u,v,c);
   }
 
   void input(int m,int offset=0){
-    int a,b,c;
+    int a,b;
+    T c;
     for(int i=0;i<m;i++){
       cin>>a>>b>>c;
       add_edge(a+offset,b+offset,c);
     }
   }
   
-  int build(){
+  T build(){
     sort(edges.begin(),edges.end());
-    UnionFind uf(n+1);
-    int res=0;
+    init(n);
+    T res=0;
     for(auto &e:edges){
-      if(!uf.same(e.from,e.to)){
+      if(!same(e.from,e.to)){
 	res+=e.cost;
-	uf.unite(e.from,e.to);
+	unite(e.from,e.to);
 	e.used=1;
       }
     }
@@ -70,13 +77,13 @@ struct Kruskal{
 int main(){
   int V,E;
   cin>>V>>E;
-  Kruskal kruskal(V);
+  Kruskal<int> kruskal(V);
   kruskal.input(E);
   cout<<kruskal.build()<<endl;
   return 0;
 }
 
 /*
-  verified on 2018/01/11
+  verified on 2018/08/28
   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_2_A&lang=jp
 */
