20da012c3dd90644ecfe63a3d7ad23eb46612139
beet-aizu <aki.bdash@gmail.com>
2018-09-26 23:15:53 +0900

    add

--- mod/polynomialhash.cpp
+++ mod/polynomialhash.cpp
@@ -2,173 +2,116 @@
 using namespace std;
 using Int = long long;
 //BEGIN CUT HERE
-template<typename T>
-struct PolynomialHash{
-  using V = tuple<T, T, T>;
-  static const T MOD1 = 1e9+7;
-  static const T MOD2 = 1e9+9;
-  static const T MOD3 = 1e8+7;
-  static const T BASE = 1777771;
-  vector<T> po1,po2,po3;
-  vector<T> op1,op2,op3;
+template<typename T,T MOD = 1000000007>
+struct Mint{
+  T v;
+  Mint():v(0){}
+  Mint(signed v):v(v){}
+  Mint(long long t){v=t%MOD;if(v<0) v+=MOD;}
 
-  inline T add1(T a,T b){
-    a+=b;
-    if(a>=MOD1) a-=MOD1;
-    return a;
-  }
-  inline T add2(T a,T b){
-    a+=b;
-    if(a>=MOD2) a-=MOD2;
-    return a;
-  }
-  inline T add3(T a,T b){
-    a+=b;
-    if(a>=MOD3) a-=MOD3;
-    return a;
-  }
-  
-  inline T sub1(T a,T b){
-    return add1(a,MOD1-b);
-  }
-  inline T sub2(T a,T b){
-    return add2(a,MOD2-b);
-  }
-  inline T sub3(T a,T b){
-    return add3(a,MOD3-b);
-  }
-
-  inline T mul1(T a,T b){
-    return (1LL*a*b)%MOD1;
-  }
-
-  inline T mul2(T a,T b){
-    return (1LL*a*b)%MOD2;
-  }
-
-  inline T mul3(T a,T b){
-    return (1LL*a*b)%MOD3;
-  }
-
-  static inline constexpr T pow(T a,int n,const T MOD){
-    T res=1;
-    while(n){
-      if(n&1) res=1LL*res*a%MOD;
-      a=1LL*a*a%MOD;
-      n>>=1;
+  Mint pow(int k){
+    Mint res(1),tmp(v);
+    while(k){
+      if(k&1) res*=tmp;
+      tmp*=tmp;
+      k>>=1;
     }
     return res;
   }
   
-  static inline constexpr T inv(T a,T MOD){
-    return pow(a,MOD-2,MOD);
-  }
+  Mint inv(){return pow(MOD-2);}
   
-  static constexpr T INV1 = inv(BASE,MOD1);
-  static constexpr T INV2 = inv(BASE,MOD2);
-  static constexpr T INV3 = inv(BASE,MOD3);
+  Mint& operator+=(Mint a){v+=a.v;if(v>=MOD)v-=MOD;return *this;}
+  Mint& operator-=(Mint a){v+=MOD-a.v;if(v>=MOD)v-=MOD;return *this;}
+  Mint& operator*=(Mint a){v=1LL*v*a.v%MOD;return *this;}
+  Mint& operator/=(Mint a){return (*this)*=a.inv();}
   
-  PolynomialHash(const int MAX){
-    po1.resize(MAX);
-    po2.resize(MAX);
-    po3.resize(MAX);
-    po1[0]=po2[0]=po3[0]=1;
-    for(int i=1;i<MAX;i++){
-      po1[i]=mul1(po1[i-1],BASE);
-      po2[i]=mul2(po2[i-1],BASE);
-      po3[i]=mul3(po3[i-1],BASE);
-    }
+  Mint operator+(Mint a){return Mint(v)+=a;};
+  Mint operator-(Mint a){return Mint(v)-=a;};
+  Mint operator*(Mint a){return Mint(v)*=a;};
+  Mint operator/(Mint a){return Mint(v)/=a;};
+
+  Mint operator-(){return v?MOD-v:v;}
+
+  bool operator==(const Mint a)const{return v==a.v;}
+  bool operator!=(const Mint a)const{return v!=a.v;}
+  
+  bool operator<(const Mint a)const{return v<a.v;}
+
+  static vector<Mint> po,op;
+  
+  static void init(int n, T BASE){
+    if(n+1<=(signed)po.size()) return;
+    po.assign(n+1,1);
+    op.assign(n+1,1);
     
-    op1.resize(MAX);
-    op2.resize(MAX);
-    op3.resize(MAX);
-    op1[0]=op2[0]=op3[0]=1;
-    for(int i=1;i<MAX;i++){
-      op1[i]=mul1(op1[i-1],INV1);
-      op2[i]=mul2(op2[i-1],INV2);
-      op3[i]=mul3(op3[i-1],INV3);
-    }
+    Mint x(BASE);
+    Mint y=Mint(1)/x;
+    for(int i=1;i<=n;i++) po[i]=po[i-1]*x;
+    for(int i=1;i<=n;i++) op[i]=op[i-1]*y;
+  }
+};
+template<typename T,T MOD>
+vector<Mint<T, MOD> > Mint<T, MOD>::po = vector<Mint<T, MOD> >();
+template<typename T,T MOD>
+vector<Mint<T, MOD> > Mint<T, MOD>::op = vector<Mint<T, MOD> >();
+
+template<typename T,typename M1,typename M2,typename M3>
+struct PolynomialHash{
+  using V = tuple<M1, M2, M3>;
+  static const T BASE = 1777771;
+  
+  PolynomialHash(const int n){
+    M1::init(n, BASE);
+    M2::init(n, BASE);
+    M3::init(n, BASE);
   }
 
   V build(const vector<T> &v){
     int n=v.size();
-    T r1=0,r2=0,r3=0;
+    M1 r1;M2 r2;M3 r3;
     for(int i=0;i<n;i++){
-      r1=add1(r1,mul1(v[i],po1[i]));
-      r2=add2(r2,mul2(v[i],po2[i]));
-      r3=add3(r3,mul3(v[i],po3[i]));
+      r1+=M1(v[i])*M1::po[i];
+      r2+=M2(v[i])*M2::po[i];
+      r3+=M3(v[i])*M3::po[i];
     }
     return V(r1,r2,r3);
   }
-  
+
   V add(const V &a,const V &b){
-    T a1,a2,a3,b1,b2,b3;
-    tie(a1,a2,a3)=a;
-    tie(b1,b2,b3)=b;
-    T r1=add1(a1,b1);
-    T r2=add2(a2,b2);
-    T r3=add3(a3,b3);
-    return V(r1,r2,r3);
+    M1 a1=get<0>(a),b1=get<0>(b);
+    M2 a2=get<1>(a),b2=get<1>(b);
+    M3 a3=get<2>(a),b3=get<2>(b);
+    return V(a1+b1,a2+b2,a3+b3);
   }
   
   V sub(const V &a,const V &b){
-    T a1,a2,a3,b1,b2,b3;
-    tie(a1,a2,a3)=a;
-    tie(b1,b2,b3)=b;
-    T r1=sub1(a1,b1);
-    T r2=sub2(a2,b2);
-    T r3=sub3(a3,b3);
-    return V(r1,r2,r3);
+    M1 a1=get<0>(a),b1=get<0>(b);
+    M2 a2=get<1>(a),b2=get<1>(b);
+    M3 a3=get<2>(a),b3=get<2>(b);
+    return V(a1-b1,a2-b2,a3-b3);
   }
   
   V mul(const V &a,const V &b){
-    T a1,a2,a3,b1,b2,b3;
-    tie(a1,a2,a3)=a;
-    tie(b1,b2,b3)=b;
-    T r1=mul1(a1,b1);
-    T r2=mul2(a2,b2);
-    T r3=mul3(a3,b3);
-    return V(r1,r2,r3);
+    M1 a1=get<0>(a),b1=get<0>(b);
+    M2 a2=get<1>(a),b2=get<1>(b);
+    M3 a3=get<2>(a),b3=get<2>(b);
+    return V(a1*b1,a2*b2,a3*b3);
   }
   
   V inv(const V &a){
-    T a1,a2,a3;
+    M1 a1;M2 a2;M3 a3;
     tie(a1,a2,a3)=a;
-    T r1=inv(a1,MOD1);
-    T r2=inv(a2,MOD2);
-    T r3=inv(a3,MOD3);
-    return V(r1,r2,r3);
+    return V(a1.inv(),a2.inv(),a3.inv());
   }
 
   V term(int x,T k){
-    T a1=mul1(po1[x],k);
-    T a2=mul2(po2[x],k);
-    T a3=mul3(po3[x],k);
-    return V(a1,a2,a3);
-  }
-
-  V shl(const V &a,int k){
-    T a1,a2,a3;
-    tie(a1,a2,a3)=a;
-    a1=mul(a1,op1[k]);
-    a2=mul(a2,op2[k]);
-    a3=mul(a3,op3[k]);
+    M1 a1=M1::po[x]*M1(k);
+    M2 a2=M2::po[x]*M2(k);
+    M3 a3=M3::po[x]*M3(k);
     return V(a1,a2,a3);
   }
   
-  V shr(const V &a,int k){
-    T a1,a2,a3;
-    tie(a1,a2,a3)=a;
-    a1=mul(a1,po1[k]);
-    a2=mul(a2,po2[k]);
-    a3=mul(a3,po3[k]);
-    return V(a1,a2,a3);
-  }
-
-  V mv(const V &a,int k){
-    if(k<0) return shl(a,-k);
-    return shr(a,k);
-  }
 };
 
 //END CUT HERE
@@ -200,14 +143,19 @@ signed CODEFLYER2018_F(){
       for(int i=0;i<k;i++) a[i]=v[i];
       return a;
     };
+
+  using M1 = Mint<int, int(1e9+7)>;
+  using M2 = Mint<int, int(1e9+9)>;
+  using M3 = Mint<int, int(1e8+7)>;
   
-  PolynomialHash<int> ph(MAX);
-  using T = PolynomialHash<int>::V;
-  vector<T> vrs(h),vcs(w);
+  PolynomialHash<int, M1, M2, M3> ph(MAX);
+  using V = decltype(ph)::V;
+  
+  vector<V> vrs(h),vcs(w);
   vector<int> tmp(w,1);
-  T ri=ph.build(tmp);
+  V ri=ph.build(tmp);
   tmp.assign(h,1);
-  T ci=ph.build(tmp);
+  V ci=ph.build(tmp);
   
   for(int i=0;i<h;i++){
     auto v=conv(rs[i],w);
@@ -221,7 +169,7 @@ signed CODEFLYER2018_F(){
     if(cs[j][0]) vcs[j]=ph.sub(ci,vcs[j]);
   }
   
-  map<T, set<int> > mrs, mcs;
+  map<V, set<int> > mrs, mcs;
   for(int i=0;i<h;i++) mrs[vrs[i]].emplace(i);
   for(int j=0;j<w;j++) mcs[vcs[j]].emplace(j);
 
@@ -412,12 +360,6 @@ signed CODEFLYER2018_F(){
   }
   
   return 0;
-}
-
-signed ARC099_F(){
-  int n;
-  string s;
-  cin>>n>>s;
 }
 
 //INSERT ABOVE HERE
