72ad760066357fc8132da1f27404bb4d839d21ca
beet-aizu <aki.bdash@gmail.com>
2019-03-03 22:50:14 +0900

    add verify

--- datastructure/unionfindtree.cpp
+++ datastructure/unionfindtree.cpp
@@ -26,19 +26,65 @@ struct UnionFind{
 };
 //END CUT HERE
 
-signed main(){
+struct FastIO{
+  FastIO(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+  }
+}fastio_beet;
+
+//INSERT ABOVE HERE
+
+signed DSL_1_A(){
   int n,q;
   cin>>n>>q;
   UnionFind uf(n);
   for(int i=0;i<q;i++){
     int c,x,y;
     cin>>c>>x>>y;
-    if(c) cout<<uf.same(x,y)<<endl;
+    if(c) cout<<uf.same(x,y)<<"\n";
     else uf.unite(x,y);
   }
+  cout<<flush;
   return 0;
 }
 /*
-  verified on 2017/10/29
+  verified on 2019/03/03
   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A&lang=jp
 */
+
+signed ABC120_D(){
+  int n,m;
+  cin>>n>>m;
+  vector<int> a(m),b(m);
+  for(int i=0;i<m;i++) cin>>a[i]>>b[i];
+
+  UnionFind uf(n);
+  vector<Int> ans(m+1,0);
+  ans[m]=(Int)n*(n-1)/2;
+  for(int i=m-1;i>=0;i--){
+    a[i]--;b[i]--;
+    ans[i]=ans[i+1];
+    if(uf.same(a[i],b[i])) continue;
+    
+    Int x=uf.size(a[i]),y=uf.size(b[i]);
+    ans[i]-=x*y;
+    
+    uf.unite(a[i],b[i]);
+  }
+  ans.erase(ans.begin());
+  for(Int x:ans) cout<<x<<"\n";
+  cout<<flush;
+  return 0;
+}
+
+/*
+  verified on 2019/03/03
+  https://atcoder.jp/contests/abc120/tasks/abc120_d
+*/
+
+signed main(){
+  //DSL_1_A();
+  //ABC120_D();
+  return 0; 
+}
