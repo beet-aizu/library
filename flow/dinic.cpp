148e96d92dd33a6588e3be522e208c94fddafd6b
beet-aizu <aki.bdash@gmail.com>
2019-03-17 15:28:27 +0900

    fix dinic

--- flow/dinic.cpp
+++ flow/dinic.cpp
@@ -2,105 +2,96 @@
 using namespace std;
 using Int = long long;
 //BEGIN CUT HERE
+template<typename T,bool directed>
 struct Dinic{
-  const int INF=1<<28;
-  
   struct edge {
-    int to,cap,rev;
+    int to;
+    T cap;
+    int rev;
     edge(){}
-    edge(int to,int cap,int rev):to(to),cap(cap),rev(rev){}
+    edge(int to,T cap,int rev):to(to),cap(cap),rev(rev){}
   };
 
-  int n;
+  T INF;
   vector<vector<edge> > G;
-  vector<map<int,int> > M;
   vector<int> level,iter;
 
-  Dinic(){}
-  Dinic(int sz):n(sz),G(n),M(n),level(n),iter(n){}
+  Dinic(){}  
+  Dinic(int n,T INF):INF(INF),G(n),level(n),iter(n){}
   
   void add_edge(int from,int to,int cap){
-    M[from][to]=G[from].size();
-    M[to][from]=G[to].size();
-    G[from].push_back(edge(to,cap,G[to].size()));
-    // undirected
-    //G[to].push_back(edge(from,cap,G[from].size()-1));
-    // directed
-    G[to].push_back(edge(from,0,G[from].size()-1));
+    G[from].emplace_back(to,cap,G[to].size());
+    G[to].emplace_back(from,directed?0:cap,G[from].size()-1);
   }
   
   void bfs(int s){
     fill(level.begin(),level.end(),-1);
     queue<int> que;
     level[s]=0;
-    que.push(s);
+    que.emplace(s);
     while(!que.empty()){
       int v=que.front();que.pop();
       for(int i=0;i<(int)G[v].size();i++){
-        edge &e = G[v][i];
+        edge &e=G[v][i];
         if(e.cap>0&&level[e.to]<0){
           level[e.to]=level[v]+1;
-          que.push(e.to);
+          que.emplace(e.to);
         }
       }
     }
   }
   
-  int dfs(int v,int t,int f){
+  T dfs(int v,int t,T f){
     if(v==t) return f;
     for(int &i=iter[v];i<(int)G[v].size();i++){
       edge &e=G[v][i];
       if(e.cap>0&&level[v]<level[e.to]){
-        int d = dfs(e.to,t,min(f,e.cap));
-        if(d>0){
-          e.cap-=d;
-          G[e.to][e.rev].cap+=d;
-          return d;
-        }
+        T d=dfs(e.to,t,min(f,e.cap));
+        if(d==0) continue;
+        e.cap-=d;
+        G[e.to][e.rev].cap+=d;
+        return d;      
       }
     }
     return 0;
   }
   
-  int flow(int s,int t,int lim){
-    int fl=0;
-    for(;;){
+  T flow(int s,int t,T lim){
+    T fl=0;
+    while(1){
       bfs(s);
-      if(level[t]<0||lim==0) return fl;
+      if(level[t]<0||lim==0) break;
       fill(iter.begin(),iter.end(),0);
-      int f;
-      while((f=dfs(s,t,lim))>0){
+      
+      while(1){
+        T f=dfs(s,t,lim);
+        if(f==0) break;
         fl+=f;
         lim-=f;
       }
     }
+    return fl;
   }
 
-  int flow(int s,int t){
+  T flow(int s,int t){
     return flow(s,t,INF);
-  }
-
-  //cap==1 only
-  bool back_edge(int s,int t,int from, int to){
-    for(int i=0;i<(int)G[from].size();i++) {
-      edge& e=G[from][i];
-      if(e.to==to) {
-        if(e.cap==0&&flow(from,to,1)==0) {
-          flow(from,s,1);
-          flow(t,to,1);
-          return 1;
-        }
-      }
-    }
-    return 0;
   }
 };
 //END CUT HERE
 
-signed main(){
+
+struct FastIO{
+  FastIO(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+  }
+}fastio_beet;
+
+//INSERT ABOVE HERE
+signed GRL_6_A(){
   int V,E;
   cin>>V>>E;
-  Dinic dinic(V);
+  Dinic<int, true> dinic(V,1<<28);
   for(int i=0;i<E;i++){
     int u,v,c;
     cin>>u>>v>>c;
@@ -111,6 +102,32 @@ signed main(){
 }
 
 /*
-  verified on 2017/10/29
+  verified on 2019/03/17
   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_A&lang=jp
 */
+
+
+signed SPOJ_FASTFLOW(){
+  Int n,m;
+  cin>>n>>m;
+  Dinic<Int, false> G(n,1LL<<55);
+  for(Int i=0;i<m;i++){
+    Int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b) continue;
+    a--;b--;
+    G.add_edge(a,b,c);
+  }
+  cout<<G.flow(0,n-1)<<endl;
+  return 0;
+}
+/*
+  verified on 2019/03/17
+  https://www.spoj.com/problems/FASTFLOW/
+*/
+
+signed main(){
+  //GRL_6_A();
+  //SPOJ_FASTFLOW();
+  return 0;
+}
