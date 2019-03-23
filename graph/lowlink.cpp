8a3a80e0145c6621c35e59e5215a0a60eb721304
beet-aizu <aki.bdash@gmail.com>
2019-03-23 20:40:21 +0900

    add verify

--- graph/lowlink.cpp
+++ graph/lowlink.cpp
@@ -3,13 +3,14 @@ using namespace std;
 //BEGIN CUT HERE
 struct LowLink{
   int n,pos;
-  vector<int> ord,low,par,blg;
+  vector<int> ord,low,par,blg,num;
   vector<vector<int> > G,C,T;
   
   vector<int> ap;
   vector<pair<int, int> > bs;
 
-  LowLink(int n):n(n),pos(0),ord(n,-1),low(n),par(n,-1),blg(n,-1),G(n){}
+  LowLink(int n):n(n),pos(0),ord(n,-1),low(n),
+                 par(n,-1),blg(n,-1),num(n,1),G(n){}
 
   void add_edge(int u,int v){
     G[u].emplace_back(v);
@@ -31,6 +32,7 @@ struct LowLink{
       }
       par[u]=v;
       dfs(u);
+      num[v]+=num[u];
       low[v]=min(low[v],low[u]);
       if(is_bridge(u,v)) bs.emplace_back(u,v);
     }
@@ -83,6 +85,30 @@ struct LowLink{
 };
 //END CUT HERE
 
+struct UnionFind{
+  int n;
+  vector<int> r,p;
+  UnionFind(){}
+  UnionFind(int sz):n(sz),r(sz,1),p(sz,0){iota(p.begin(),p.end(),0);}
+  int find(int x){
+    return (x==p[x]?x:p[x]=find(p[x]));
+  }
+  bool same(int x,int y){
+    return find(x)==find(y);
+  }
+  void unite(int x,int y){
+    x=find(x);y=find(y);
+    if(x==y) return;
+    if(r[x]<r[y]) swap(x,y);
+    r[x]+=r[y];
+    p[y]=x;
+  }
+  int size(int x){
+    return r[find(x)];
+  }
+};
+
+//INSERT ABOVE HERE
 signed GRL_3_A(){
   int n,m;
   scanf("%d %d",&n,&m);
@@ -98,7 +124,10 @@ signed GRL_3_A(){
   for(int v:ap) printf("%d\n",v);
   return 0;
 }
-
+/*
+  verified on 2019/03/23
+  http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_A&lang=jp
+*/
 signed GRL_3_B(){
   int n,m;
   scanf("%d %d",&n,&m);
@@ -120,9 +149,84 @@ signed GRL_3_B(){
   
   return 0;
 }
+/*
+  verified on 2019/03/23
+  http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_B&lang=jp
+*/
+
+signed ARC045_D(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+    
+  int n;
+  cin>>n;
+  vector<int> xs(2*n+1),ys(2*n+1);
+  for(int i=0;i<2*n+1;i++) cin>>xs[i]>>ys[i];
+
+  vector<vector<int> > R(2*n+2),C(2*n+2);
+  for(int i=0;i<2*n+1;i++) R[xs[i]].emplace_back(i);
+  for(int i=0;i<2*n+1;i++) C[ys[i]].emplace_back(i);
+
+  UnionFind uf(2*n+1);
+  for(auto &v:R)
+    for(auto u:v) uf.unite(v[0],u);
+  for(auto &v:C)
+    for(auto u:v) uf.unite(v[0],u);
+
+  vector<int> vs;
+  for(int i=0;i<2*n+1;i++){
+    if(uf.find(i)!=i) continue;
+    if(uf.size(i)&1) vs.emplace_back(i);
+  }
+  assert(!vs.empty());
+  
+  if(vs.size()>1u){
+    for(int i=0;i<2*n+1;i++) cout<<"NG\n";
+    cout<<flush;
+    return 0;    
+  }
+  
+  LowLink G(2*n+1);
+  auto add_edge=
+    [&](auto &V)->void{
+      for(auto &v:V){
+        if(v.empty()) continue;
+        if(!uf.same(vs[0],v[0])) continue;
+        if(v.size()>0u) for(auto u:v) G.add_edge(v[0],u);
+        if(v.size()>1u) for(auto u:v) G.add_edge(v[1],u);
+      }
+    };
+  add_edge(R);
+  add_edge(C);
+
+  G.build();
+  auto ap=G.ap; 
+  
+  vector<int> ans(2*n+1,0);
+  for(int i=0;i<2*n+1;i++)
+    if(uf.same(vs[0],i)) ans[i]=1;
+
+  for(int v:ap){
+    if(!uf.same(vs[0],v)) continue;
+    for(int u:G.G[v]){
+      if(G.par[u]!=v) continue;    
+      if(~G.par[v]&&G.ord[v]>G.low[u]) continue;     
+      if(G.num[u]&1) ans[v]=0;
+    }
+  }
+  
+  for(int i=0;i<2*n+1;i++) cout<<(ans[i]?"OK\n":"NG\n");
+  cout<<flush;
+  return 0;
+}
+/*
+  verified on 2019/03/23
+  https://atcoder.jp/contests/arc045/tasks/arc045_d
+*/
 
 signed main(){
   //GRL_3_A();
   //GRL_3_B();
+  //ARC045_D();  
   return 0;
 }
