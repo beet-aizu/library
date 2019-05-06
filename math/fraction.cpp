e57f3c2be06b581bda035655cf40c86156ee2d79
beet-aizu <aki.bdash@gmail.com>
2019-05-06 16:19:41 +0900

    fix fraction

--- math/fraction.cpp
+++ math/fraction.cpp
@@ -1,49 +1,104 @@
 #include<bits/stdc++.h>
 using namespace std;
 using Int = long long;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 //BEGIN CUT HERE
-struct frac{
-  int num,dom;
-  frac(){}
-  frac(int num,int dom):num(num),dom(dom){}
-  frac norm(){
-    if(num==0) return frac(0,1);
-    int tmp=__gcd(num,dom);
-    return frac(num/tmp,dom/tmp);
+template<typename T>
+struct fraction{
+  T num,dom;
+  fraction(){}
+  fraction(T num,T dom):num(num),dom(dom){}
+  fraction norm(){
+    if(num==T(0)) return fraction(0,1);
+    if(dom<0) num*=-1,dom*=-1;
+    T tmp=__gcd(abs(num),abs(dom));
+    return fraction(num/tmp,dom/tmp);
   }
-  frac norm2(){
-    if(num==0) return frac(0,1);
-    while(num<0) num+=dom;
-    while(num>=dom) num-=dom;
-    int tmp=__gcd(num,dom);
-    return frac(num/tmp,dom/tmp);
+  fraction operator+(const fraction a) const{
+    return fraction(num*a.dom+a.num*dom,dom*a.dom).norm();
   }
-  frac operator+(frac a){return frac(num*a.dom+a.num*dom,dom*a.dom).norm();}
-  frac operator-(frac a){return frac(num*a.dom-a.num*dom,dom*a.dom).norm();}
-  frac operator*(frac a){return frac(num*a.num,dom*a.dom).norm();}
-  frac operator/(frac a){return frac(num*a.dom,dom*a.num).norm();}
-  frac operator*(int k){return frac(num*k,dom).norm();}
-  frac operator/(int k){return frac(num,dom*k).norm();}
-  bool operator<(const frac a)const{
+  fraction operator-(const fraction a) const{
+    return fraction(num*a.dom-a.num*dom,dom*a.dom).norm();
+  }
+  fraction operator*(const fraction a) const{
+    return fraction(num*a.num,dom*a.dom).norm();
+  }
+  fraction operator/(const fraction a){
+    return fraction(num*a.dom,dom*a.num).norm();
+  }
+  fraction operator*(T k) const{return fraction(num*k,dom).norm();}
+  fraction operator/(T k) const{return fraction(num,dom*k).norm();}
+  bool operator<(const fraction a)const{
     return num*a.dom<a.num*dom;
   }
-  bool operator>(const frac a)const{
+  bool operator>(const fraction a)const{
     return num*a.dom>a.num*dom;
   }
-  bool operator==(const frac a)const{
+  bool operator==(const fraction a)const{
     return num*a.dom==a.num*dom;
   }
-  bool operator!=(const frac a)const{
+  bool operator!=(const fraction a)const{
     return num*a.dom!=a.num*dom;
   }
-  bool operator<=(const frac a)const{
+  bool operator<=(const fraction a)const{
     return num*a.dom<=a.num*dom;
   }
-  bool operator>=(const frac a)const{
+  bool operator>=(const fraction a)const{
     return num*a.dom>=a.num*dom;
   }
 };
 //END CUT HERE
+
+struct FastIO{
+  FastIO(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+  }
+}fastio_beet;
+
+//INSERT ABOVE HERE
+signed CPSCO2019_Session3_G(){
+  Int n,x;
+  cin>>n>>x;
+  vector<Int> a(n);
+  for(Int i=0;i<n;i++) cin>>a[i];
+  using F = fraction<Int>;
+  using T = tuple<F, Int, Int>;
+  priority_queue<T> pq;
+
+  Int sum=0;
+  for(Int i=0;i<n;i++) sum+=a[i];
+
+  for(Int i=0;i<n;i++){
+    Int p=x*a[i]/sum;
+    pq.emplace(F(1,a[i]),i,p);
+    if((x*a[i])%sum!=0)
+      pq.emplace(F(2*x,sum)-F(2*p+1,a[i]),i,1);  
+    pq.emplace(F(-1,a[i]),i,x+1);
+  }
+  
+  vector<Int> b(n,0);
+  Int k=x;
+  while(k>0){
+    Int i,l;
+    tie(ignore,i,l)=pq.top();pq.pop();
+    
+    Int t=min(k,l);
+    b[i]+=t;
+    k-=t;
+  }
+  
+  for(Int i=0;i<n;i++) cout<<b[i]<<"\n";
+  cout<<flush;
+  return 0;
+}
+/*
+  verified on 2019/05/06
+  https://atcoder.jp/contests/cpsco2019-s3/tasks/cpsco2019_s3_g 
+*/
+
 signed main(){
+  CPSCO2019_Session3_G();
   return 0;
 }
