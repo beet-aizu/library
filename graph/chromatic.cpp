ea372b74dee322539b8377d38778e1e111a36a63
beet-aizu <aki.bdash@gmail.com>
2019-05-08 20:51:37 +0900

    fix chromatic

--- graph/chromatic.cpp
+++ graph/chromatic.cpp
@@ -2,54 +2,64 @@
 using namespace std;
 using Int = long long;
 //BEGIN CUT HERE
-#define MAX 11
-bool e[MAX][MAX];
-int mod_pow(int x,int n,int mod){
-  int res=1;
-  while(n>0){
-    if(n&1) (res*=x)%=mod;
-    (x*=x)%=mod;
-    n>>=1;
-  }
-  return res;
-}
-int I[1<<MAX],bn[1<<MAX];
-bool check(int n,int k){
-  int g=0;
-  int MOD=10009;
-  for(int i=0;i<(1<<n);i++){
-    if(bn[i]%2==0) g+=mod_pow(I[i],k,MOD);
-    else g-=mod_pow(I[i],k,MOD);
-  }
-  return (g%MOD+MOD)%MOD!=0;
-}
-int paint(int n){
-  vector<int> N(1<<n);
-  for(int i=0;i<n;i++){
-    int bit=(1<<i);
+int chromatic(vector< vector<int> > &G){
+  int n=G.size();
+  if(n==0) return 0;
+  assert(n<=30);
+  
+  vector<int> es(n,0);  
+  for(int i=0;i<n;i++)
     for(int j=0;j<n;j++)
-      if(e[i][j]) bit|=(1<<j);
-    N[i]=bit;
+      es[i]|=G[i][j]<<j;
+  
+  int s=1<<n;
+  vector<int> dp(s);
+  for(int b=0;b<s;b++)
+    dp[b]=((n-__builtin_popcount(b))&1?-1:1);
+
+  auto calc=
+    [&](const int MOD){
+      vector<int> res(s);
+      res[0]=1;
+      for(int b=1;b<s;b++){
+        int ctz=__builtin_ctz(b);
+        res[b]=res[b-(1<<ctz)]+res[(b-(1<<ctz))&~es[ctz]];
+        if(res[b]>=MOD) res[b]=MOD;
+      }
+      return res;
+    };
+  
+  constexpr int MOD1 = 1077563119;
+  constexpr int MOD2 = 1e9+7;
+  vector<int> ind1=calc(MOD1);
+  vector<int> ind2=calc(MOD2);
+  
+  using ll = long long;
+  vector<int> dp1=dp,dp2=dp;
+  for(int i=1;i<n;i++){
+    ll sum1=0,sum2=0;
+    for(int b=0;b<s;b++){
+      dp1[b]=((ll)dp1[b]*ind1[b])%MOD1;
+      sum1+=dp1[b];
+      
+      dp2[b]=((ll)dp2[b]*ind2[b])%MOD2;
+      sum2+=dp2[b];
+    }
+    if(sum1%MOD1!=0) return i;
+    if(sum2%MOD2!=0) return i;
   }
-  memset(I,0,sizeof(I));
-  I[0]=1;
-  for(int S=1;S<(1<<n);S++){
-    bn[S]=__builtin_popcountll(S);
-    int v=0;
-    while(!((S>>v)&1)) v++;
-    I[S]=I[S-(1<<v)]+I[S&~N[v]];
-  }
-  int l=0,r=n;
-  while(l+1<r){
-    int m=(l+r)/2;
-    if(check(n,m)) r=m;
-    else l=m;
-  }
-  return r;
+  return n;
 }
 //END CUT HERE
 
+struct FastIO{
+  FastIO(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+  }
+}fastio_beet;
 
+//INSERT ABOVE HERE
 
 #define EPS (1e-10)
 
@@ -65,7 +75,6 @@ struct Point{
 
 typedef Point Vector;
 typedef vector<Point> Polygon;
-
 
 double norm(Vector a){
   return a.x*a.x+a.y*a.y;
@@ -90,12 +99,11 @@ bool calc(Point a1,Point a2,Point b1,Point b2){
   return (ml+EPS<abs(a1-a2)+abs(b1-b2));
 }
 
-
 signed main(){
   int n;
   while(cin>>n,n){
-    Polygon p[n];
-    string name[n];
+    vector<Polygon> p(n);
+    vector<string> name(n);
     map<string,int> m;
     for(int i=0;i<n;i++){
       cin>>name[i];
@@ -109,23 +117,24 @@ signed main(){
         p[i].push_back(Point(x,y));
       }
     }
-    memset(e,0,sizeof(e));
+    
+    vector<vector<int> > G(m.size(),vector<int>(m.size(),0));
     for(int i=0;i<n;i++){
       for(int j=i+1;j<n;j++){
         if(name[i]==name[j]) continue;
         for(int k=0;k<(int)p[i].size();k++){
           for(int l=0;l<(int)p[j].size();l++){
-            e[m[name[i]]][m[name[j]]]|=
+            G[m[name[i]]][m[name[j]]]|=
               calc(p[i][k],p[i][(k+1)%p[i].size()],
                    p[j][l],p[j][(l+1)%p[j].size()]);
-            e[m[name[j]]][m[name[i]]]|=
+            G[m[name[j]]][m[name[i]]]|=
               calc(p[i][k],p[i][(k+1)%p[i].size()],
                    p[j][l],p[j][(l+1)%p[j].size()]);
           }
         }
       }
     }
-    cout<<paint(m.size())<<endl;
+    cout<<chromatic(G)<<endl;
   }
   return 0;
 }
