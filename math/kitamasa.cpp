b850031e9608c24ab6672b261673e08a9e8f53de
beet-aizu <aki.bdash@gmail.com>
2019-05-19 18:40:21 +0900

    fix

--- math/kitamasa.cpp
+++ math/kitamasa.cpp
@@ -2,66 +2,98 @@
 using namespace std;
 using Int = long long;
 //BEGIN CUT HERE
-template<typename T>
+template<typename R>
 struct Kitamasa{
-  using VT = vector<T>;
-  using F = function<T(T,T)>;
-  VT c;
-  vector<VT> rs;
+  using VR = vector<R>;
+  VR c;
+  vector<VR> rs;
   int m;
-  F add;
-  F mul;
-  T d0,d1;
   
-  Kitamasa(const VT &C,F add,F mul,T d0,T d1)
-    :c(C),rs(1),m(c.size()),add(add),mul(mul),d0(d0),d1(d1){
-    rs[0].assign(2*m+1,d0);
-    rs[0][1]=d1;
+  Kitamasa(const VR &C)
+    :c(C),rs(1),m(c.size()){
+    rs[0].assign(2*m+1,R::add_identity());
+    rs[0][1]=R::mul_identity();
   }
 
-  VT merge(const VT &x,const VT &y){
-    VT z(2*m+1,d0);
+  VR merge(const VR &x,const VR &y){
+    VR z(2*m+1,R::add_identity());
     for(int i=1;i<=m;i++)
       for(int j=1;j<=m;j++)
-        z[i+j]=add(z[i+j],mul(x[i],y[j]));
+        z[i+j]=z[i+j]+(x[i]*y[j]);
     
-    for(int i=2*m;i>m;z[i--]=d0)
+    for(int i=2*m;i>m;z[i--]=R::add_identity())
       for(int j=1;j<=m;j++)
-        z[i-j]=add(z[i-j],mul(c[m-j],z[i]));
-    
+        z[i-j]=z[i-j]+(c[m-j]*z[i]);
     return z;
   }
 
-  T calc(const VT &A,long long n){
+  R calc(const VR &A,long long n){
     assert((int)A.size()==m);
-    VT res(rs[0]);
+    VR res(rs[0]);
     for(int i=0;n;i++,n>>=1){
       if(i>=(int)rs.size())
         rs.emplace_back(merge(rs[i-1],rs[i-1]));
       if(~n&1) continue;
       res=merge(res,rs[i]);
     }
-    T ans=d0;
-    for(int i=1;i<=m;i++) ans=add(ans,mul(res[i],A[i-1]));
+    R ans=R::add_identity();
+    for(int i=1;i<=m;i++) ans=ans+(res[i]*A[i-1]);
     return ans;
-  }
-  
+  }  
 };
 //END CUT HERE
 
+template<typename T,T MOD = 1000000007>
+struct Mint{
+  T v;
+  Mint():v(0){}
+  Mint(signed v):v(v){}
+  Mint(long long t){v=t%MOD;if(v<0) v+=MOD;}
+
+  Mint pow(long long k){
+    Mint res(1),tmp(v);
+    while(k){
+      if(k&1) res*=tmp;
+      tmp*=tmp;
+      k>>=1;
+    }
+    return res;
+  }
+  
+  static Mint add_identity(){return Mint(0);}
+  static Mint mul_identity(){return Mint(1);}
+  
+  Mint inv(){return pow(MOD-2);}
+  
+  Mint& operator+=(Mint a){v+=a.v;if(v>=MOD)v-=MOD;return *this;}
+  Mint& operator-=(Mint a){v+=MOD-a.v;if(v>=MOD)v-=MOD;return *this;}
+  Mint& operator*=(Mint a){v=1LL*v*a.v%MOD;return *this;}
+  Mint& operator/=(Mint a){return (*this)*=a.inv();}
+  
+  Mint operator+(Mint a) const{return Mint(v)+=a;};
+  Mint operator-(Mint a) const{return Mint(v)-=a;};
+  Mint operator*(Mint a) const{return Mint(v)*=a;};
+  Mint operator/(Mint a) const{return Mint(v)/=a;};
+
+  Mint operator-() const{return v?Mint(MOD-v):Mint(v);}
+
+  bool operator==(const Mint a)const{return v==a.v;}
+  bool operator!=(const Mint a)const{return v!=a.v;}
+  bool operator <(const Mint a)const{return v <a.v;}
+};
+
+//INSERT ABOVE HERE
 signed TDPC_F(){
   using ll = long long;
   int n;
   ll k;
   scanf("%d %lld",&n,&k);
-  constexpr ll MOD = 1e9+7;
   
-  vector<int> a(n,1),c(n,1);
-  auto add=[](int a,int b){return (a+b)%MOD;};
-  auto mul=[](int a,int b){return ((ll)a*b)%MOD;};
-  Kitamasa<int> fib(c,add,mul,0,1);
-
-  printf("%d\n",fib.calc(a,--k));
+  using M = Mint<int>;
+  vector<M> a(n,1),c(n,1);
+  Kitamasa<M> fib(c);
+  
+  printf("%d\n",fib.calc(a,--k).v);
   return 0;
 }
 /*
@@ -73,14 +105,22 @@ signed ABC009_D(){
   int k,m;
   scanf("%d %d",&k,&m);
   
-  vector<unsigned> a(k),c(k);
-  for(int i=0;i<k;i++) scanf("%u",&a[i]);
-  for(int i=0;i<k;i++) scanf("%u",&c[k-(i+1)]);
-  auto add=[](unsigned a,unsigned b){return a^b;};
-  auto mul=[](unsigned a,unsigned b){return a&b;};
-  Kitamasa<unsigned> v(c,add,mul,0u,~0u);
+  struct M{
+    uint32_t v;    
+    M(){*this=add_identity();}
+    M(uint32_t v):v(v){}
+    M operator+(const M &a)const{return M(v^a.v);}
+    M operator*(const M &a)const{return M(v&a.v);}
+    static M add_identity(){return M(0);}
+    static M mul_identity(){return M(~0u);}
+  };
+  
+  vector<M> a(k),c(k);
+  for(int i=0;i<k;i++) scanf("%u",&a[i].v);
+  for(int i=0;i<k;i++) scanf("%u",&c[k-(i+1)].v);
+  Kitamasa<M> v(c);
 
-  printf("%u\n",v.calc(a,--m));
+  printf("%u\n",v.calc(a,--m).v);
   return 0;
 }
 /*
