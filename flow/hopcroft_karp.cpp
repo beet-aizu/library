72be21b62ea83974e360c5f980b7b800c4c0a5b7
beet-aizu <aki.bdash@gmail.com>
2019-05-26 17:40:13 +0900

    speedup

--- flow/hopcroft_karp.cpp
+++ flow/hopcroft_karp.cpp
@@ -8,45 +8,54 @@ struct BiMatch{
   vector<int> match,level;
   
   BiMatch(){}
-  BiMatch(int L,int R):L(L),R(R),G(L+R),match(L+R,-1),level(L){}
+  BiMatch(int L,int R):L(L),R(R),G(L+R),match(L+R,-1),level(L+R){}
   
   void add_edge(int u,int v){
-    G[u].push_back(v+L);
-    G[v+L].push_back(u);
+    G[u].emplace_back(v+L);
+    G[v+L].emplace_back(u);
   }
   
   bool bfs(){
+    fill(level.begin(),level.end(),-1);
     queue<int> q;
     for(int i=0;i<L;i++){
-      level[i]=-1;
       if(match[i]<0){
         level[i]=0;
         q.emplace(i);
       }
     }
+    bool found=false;
     while(!q.empty()){
       int v=q.front();q.pop();
       for(int u:G[v]){
+        if(~level[u]) continue;
+        level[u]=level[v]+1;
         int w=match[u];
-        if(w<0) return true;
-        if(level[w]<0){
-          level[w]=level[v]+1;
-          q.emplace(w);
-        }
+        if(w==-1){
+          found=true;
+          continue;
+        }        
+        if(~level[w]) continue;
+        level[w]=level[u]+1;
+        q.emplace(w);
       }
     }
-    return false;
+    return found;
   }
 
   bool dfs(int v){
     for(int u:G[v]){
+      if(level[v]+1!=level[u]) continue;
+      level[u]=-1;
       int w=match[u];
-      if(w<0||(level[w]>level[v]&&dfs(w))){
+      if(w<0||dfs(w)){
         match[v]=u;
         match[u]=v;
+        level[v]=-1;
         return true;
       }
     }
+    level[v]=-1;
     return false;
   }
   
@@ -62,7 +71,26 @@ struct BiMatch{
 };
 //END CUT HERE
 
+template<typename T>
+vector<T> compress(vector<T> v){
+  sort(v.begin(),v.end());
+  v.erase(unique(v.begin(),v.end()),v.end());
+  return v;
+}
+
+template<typename T>
+map<T, int> dict(const vector<T> &v){
+  map<T, int> res;
+  for(int i=0;i<(int)v.size();i++)
+    res[v[i]]=i;
+  return res;
+}
+
+//INSERT ABOVE HERE
 signed AOJ_GRL_7A(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+    
   int X,Y,E;
   cin>>X>>Y>>E;
   BiMatch bm(X,Y);
@@ -75,11 +103,14 @@ signed AOJ_GRL_7A(){
   return 0;
 }
 /*
-  verified on 2018/02/28
+  verified on 2019/05/26
   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_7_A&lang=jp
 */
 
-signed SPOJ_MATCHING(){
+signed SPOJ_MATCHING(){  
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+    
   int X,Y,E;
   cin>>X>>Y>>E;
   BiMatch bm(X,Y);
@@ -92,12 +123,42 @@ signed SPOJ_MATCHING(){
   return 0;
 }
 /*
-  verified on 2018/02/28
+  verified on 2019/05/26
   http://www.spoj.com/problems/MATCHING/
 */
 
+signed chokudai_S002_K(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  int n;
+  cin>>n;
+  vector<int> as(n),bs(n);
+  for(int i=0;i<n;i++) cin>>as[i]>>bs[i];
+  
+  vector<int> vs;
+  for(int x:as) vs.emplace_back(x);
+  for(int x:bs) vs.emplace_back(x);
+  vs=compress(vs);
+  auto dc=dict(vs);
+
+  BiMatch bm(n,dc.size());
+  for(int i=0;i<n;i++){
+    bm.add_edge(i,dc[as[i]]);
+    bm.add_edge(i,dc[bs[i]]);
+  }
+  
+  cout<<bm.build()<<endl;
+  return 0;
+}
+/*
+  verified on 2019/05/26
+  https://atcoder.jp/contests/chokudai_S002/tasks/chokudai_S002_k
+*/
+
+
 signed main(){
   //AOJ_GRL_7A();
-  SPOJ_MATCHING();
+  //SPOJ_MATCHING();
+  //chokudai_S002_K();
   return 0;
 }
