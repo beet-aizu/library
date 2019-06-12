eccf60e468cb022c4813d783992459051d726064
beet-aizu <aki.bdash@gmail.com>
2019-06-12 20:22:35 +0900

    add ostream

--- mod/mint.cpp
+++ mod/mint.cpp
@@ -19,17 +19,17 @@ struct Mint{
     }
     return res;
   }
-  
+
   static Mint add_identity(){return Mint(0);}
   static Mint mul_identity(){return Mint(1);}
-  
+
   Mint inv(){return pow(MOD-2);}
-  
+
   Mint& operator+=(Mint a){v+=a.v;if(v>=MOD)v-=MOD;return *this;}
   Mint& operator-=(Mint a){v+=MOD-a.v;if(v>=MOD)v-=MOD;return *this;}
   Mint& operator*=(Mint a){v=1LL*v*a.v%MOD;return *this;}
   Mint& operator/=(Mint a){return (*this)*=a.inv();}
-  
+
   Mint operator+(Mint a) const{return Mint(v)+=a;};
   Mint operator-(Mint a) const{return Mint(v)-=a;};
   Mint operator*(Mint a) const{return Mint(v)*=a;};
@@ -63,7 +63,7 @@ struct Mint{
     assert(0);
     return T(-1);
   }
-  
+
   static Mint comb(long long n,int k){
     Mint num(1),dom(1);
     for(int i=0;i<k;i++){
@@ -74,6 +74,8 @@ struct Mint{
   }
 };
 template<typename T,T MOD> constexpr T Mint<T, MOD>::mod;
+template<typename T,T MOD>
+ostream& operator<<(ostream &os,Mint<T, MOD> m){os<<m.v;return os;}
 //END CUT HERE
 
 
@@ -83,7 +85,7 @@ struct Kitamasa{
   VR c;
   vector<VR> rs;
   int m;
-  
+
   Kitamasa(const VR &C)
     :c(C),rs(1),m(c.size()){
     rs[0].assign(2*m+1,R::add_identity());
@@ -95,7 +97,7 @@ struct Kitamasa{
     for(int i=1;i<=m;i++)
       for(int j=1;j<=m;j++)
         z[i+j]=z[i+j]+(x[i]*y[j]);
-    
+
     for(int i=2*m;i>m;z[i--]=R::add_identity())
       for(int j=1;j<=m;j++)
         z[i-j]=z[i-j]+(c[m-j]*z[i]);
@@ -114,7 +116,7 @@ struct Kitamasa{
     R ans=R::add_identity();
     for(int i=1;i<=m;i++) ans=ans+(res[i]*A[i-1]);
     return ans;
-  }  
+  }
 };
 
 template<typename T>
@@ -162,50 +164,50 @@ signed CFR536_F(){
   Kitamasa<P> ktms(bs);
   vector<P> as(k,0);
   as[k-1]=1;
-  
+
   int v=ktms.calc(as,--n).v;
-  int h=M::log(3,m)%(MOD-1);  
+  int h=M::log(3,m)%(MOD-1);
 
   if(v!=0||h!=0){
     int g=__gcd(v,h);
     v/=g;
     h/=g;
   }
-  
+
   if(h!=0&&__gcd(v,MOD-1)!=1){
     cout<<-1<<endl;
     return 0;
   }
-  
-  cout<<M(3).pow((long long)h*mod_inverse(v,MOD-1)).v<<endl;
+
+  cout<<M(3).pow((long long)h*mod_inverse(v,MOD-1))<<endl;
   return 0;
 }
 /*
-  verified on 2019/05/26
+  verified on 2019/06/12
   https://codeforces.com/contest/1106/problem/F
 */
 
-signed ABC127_E(){  
+signed ABC127_E(){
   cin.tie(0);
   ios::sync_with_stdio(0);
-  
+
   int h,w,k;
   cin>>h>>w>>k;
   using M = Mint<int>;
- 
+
   M ans{0};
   for(int d=1;d<h;d++)
     ans+=M(d)*M(h-d)*M(w)*M(w);
- 
+
   for(int d=1;d<w;d++)
     ans+=M(d)*M(w-d)*M(h)*M(h);
-  
+
   ans*=M::comb(h*w-2,k-2);
-  cout<<ans.v<<endl;
+  cout<<ans<<endl;
   return 0;
 }
 /*
-  verified on 2019/05/26
+  verified on 2019/06/12
   https://atcoder.jp/contests/abc127/tasks/abc127_e
 */
 
