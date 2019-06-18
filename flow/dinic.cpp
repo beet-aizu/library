ad8045ff128c6479843149420e7df070ff4f4111
beet-aizu <aki.bdash@gmail.com>
2019-06-18 14:16:36 +0900

    add cut

--- flow/dinic.cpp
+++ flow/dinic.cpp
@@ -18,9 +18,10 @@ struct Dinic{
   Dinic(){}
   Dinic(int n):G(n),level(n),iter(n){}
 
-  void add_edge(int from,int to,T cap){
+  int add_edge(int from,int to,T cap){
     G[from].emplace_back(to,cap,G[to].size());
     G[to].emplace_back(from,directed?0:cap,G[from].size()-1);
+    return G[to].back().rev;
   }
 
   void bfs(int s){
@@ -75,9 +76,37 @@ struct Dinic{
   T flow(int s,int t){
     return flow(s,t,numeric_limits<T>::max()/2);
   }
+
+  T cut(int s,int t,int x,int a){
+    static_assert(directed, "must be directed");
+    auto &e=G[x][a];
+    int y=e.to;
+    T ce=e.cap,cr=G[y][e.rev].cap;
+    if(cr==0) return 0;
+    e.cap=G[y][e.rev].cap=0;
+    T cap=cr-flow(x,y,cr);
+    if(x!=s&&cap!=0) flow(x,s,cap);
+    if(t!=y&&cap!=0) flow(t,y,cap);
+    e.cap=ce+cr;
+    return cap;
+  }
 };
 //END CUT HERE
 
+template<typename T>
+vector<T> compress(vector<T> v){
+  sort(v.begin(),v.end());
+  v.erase(unique(v.begin(),v.end()),v.end());
+  return v;
+}
+
+template<typename T>
+map<T, int> dict(const vector<T> &v){
+  map<T, int> res;
+  for(int i=0;i<(int)v.size();i++)
+    res[v[i]]=i;
+  return res;
+}
 //INSERT ABOVE HERE
 signed GRL_6_A(){
   cin.tie(0);
@@ -149,9 +178,110 @@ signed SPOJ_BANKROB(){
   https://www.spoj.com/problems/BANKROB/
 */
 
+signed AOJ_1599(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n;
+  cin>>n;
+  vector<string> es(n);
+  for(int i=0;i<n;i++) cin>>es[i];
+
+  const int L=5;
+  vector<string> vs;
+  for(string &s:es){
+    for(char &c:s){
+      if(isdigit(c)) c=c-'0';
+      else c=c-'a'+10;
+    }
+    for(int i=0;i<L;i++){
+      if(s[i]&1) continue;
+      s[i]=(s[i]+1)%16;
+      vs.emplace_back(s);
+      s[i]=(s[i]+15)%16;
+
+      s[i]=(s[i]+15)%16;
+      vs.emplace_back(s);
+      s[i]=(s[i]+1)%16;
+    }
+  }
+  es=compress(es);
+  vs=compress(vs);
+  auto vd=dict(vs);
+
+  auto restore=
+    [](string s){
+      for(char &c:s){
+        if(c>=10) c='a'+(c-10);
+        else c='0'+c;
+      }
+      return s;
+    };
+
+  int m=vd.size();
+  Dinic<int, true> G(m+2);
+  int S=m,T=m+1;
+
+  vector<int> used(m,0);
+  using P = pair<int, int>;
+  map<string, P> mp;
+
+  for(string &s:es){
+    for(int i=0;i<L;i++){
+      if(s[i]&1) continue;
+
+      s[i]=(s[i]+1)%16;
+      int x=vd[s];
+      s[i]=(s[i]+15)%16;
+
+      s[i]=(s[i]+15)%16;
+      int y=vd[s];
+      s[i]=(s[i]+1)%16;
+
+      int sum=0;
+      for(char c:vs[x]) sum+=c/2;
+      if(sum&1) swap(x,y);
+
+      mp[s]=P(x,G.add_edge(x,y,1));
+
+      if(!used[x]){
+        mp[vs[x]]=P(S,G.add_edge(S,x,1));
+        used[x]=1;
+      }
+
+      if(!used[y]){
+        mp[vs[y]]=P(y,G.add_edge(y,T,1));
+        used[y]=1;
+      }
+    }
+  }
+
+  vector<string> as;
+  for(auto s:es) as.emplace_back(s);
+  for(auto s:vs) as.emplace_back(s);
+  sort(as.begin(),as.end());
+
+  cout<<G.flow(S,T)<<"\n";
+  for(auto s:as){
+    int x=mp[s].first;
+    int a=mp[s].second;
+    if(G.cut(S,T,x,a)==1){
+      G.G[x][a].cap=0;
+      cout<<restore(s)<<"\n";
+    }
+  }
+  cout<<flush;
+  return 0;
+}
+/*
+  verified on 2019/06/18
+  http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1599
+*/
+
 signed main(){
   //GRL_6_A();
   //SPOJ_FASTFLOW();
   //SPOJ_BANKROB();
+  //AOJ_1599();
   return 0;
 }
