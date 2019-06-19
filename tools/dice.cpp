9547849baaaab7e7c0effc92ead437c8fdeb8dbb
beet-aizu <aki.bdash@gmail.com>
2019-06-19 13:37:52 +0900

    add reference

--- tools/dice.cpp
+++ tools/dice.cpp
@@ -3,13 +3,13 @@ using namespace std;
 using Int = long long;
 //BEGIN CUT HERE
 struct Dice{
-  int s[6];  
-  int top()   {return s[0];}
-  int south() {return s[1];}
-  int east()  {return s[2];} 
-  int west()  {return s[3];}
-  int north() {return s[4];} 
-  int bottom(){return s[5];}
+  int s[6];
+  int &top()   {return s[0];}
+  int &south() {return s[1];}
+  int &east()  {return s[2];}
+  int &west()  {return s[3];}
+  int &north() {return s[4];}
+  int &bottom(){return s[5];}
   void roll(char c){
     //the view from above
     // N
@@ -46,11 +46,11 @@ vector<Dice> makeDices(Dice d){
   vector<Dice> res;
   for(int i=0;i<6;i++){
     Dice t(d);
-    if(i==1) t.roll('N'); 
-    if(i==2) t.roll('S'); 
+    if(i==1) t.roll('N');
+    if(i==2) t.roll('S');
     if(i==3) t.roll('S'),t.roll('S');
-    if(i==4) t.roll('L'); 
-    if(i==5) t.roll('R'); 
+    if(i==4) t.roll('L');
+    if(i==5) t.roll('R');
     for(int k=0;k<4;k++){
       res.push_back(t);
       t.roll('E');
@@ -69,7 +69,7 @@ signed ITP1_11_A(){
   return 0;
 }
 /*
-  verified on 2018/02/09
+  verified on 2019/06/19
   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_11_A&lang=jp
 */
 
@@ -88,13 +88,13 @@ int ITP1_11_B(){
   return 0;
 }
 /*
-  verified on 2018/02/09
+  verified on 2019/06/19
   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_11_B&lang=jp
 */
 
 int ITP1_11_C(){
   Dice d[2];
-  for(int j=0;j<2;j++) 
+  for(int j=0;j<2;j++)
     for(int i=0;i<6;i++)
       cin >> d[j].s[i];
   bool f=false;
@@ -104,7 +104,7 @@ int ITP1_11_C(){
   return 0;
 }
 /*
-  verified on 2018/02/09
+  verified on 2019/06/19
   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_11_C&lang=jp
 */
 
@@ -112,11 +112,11 @@ int ITP1_11_D(){
   int n;
   cin>>n;
   vector<Dice> ds(n);
-  for(int j=0;j<n;j++) 
+  for(int j=0;j<n;j++)
     for(int i=0;i<6;i++)
       cin >> ds[j].s[i];
   bool ff=true;
-  for(int i=0;i<n;i++){    
+  for(int i=0;i<n;i++){
     auto vd=makeDices(ds[i]);
     for(int j=0;j<i;j++){
       bool f=false;
@@ -128,7 +128,7 @@ int ITP1_11_D(){
   return 0;
 }
 /*
-  verified on 2018/02/09
+  verified on 2019/06/19
   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_11_D&lang=jp
 */
 
@@ -147,12 +147,76 @@ signed AOJ_0502(){
     }
     cout<<ans<<endl;
   }
-  
+
   return 0;
 }
 /*
-  verified on 2018/02/09
+  verified on 2019/06/19
   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0502
+*/
+
+signed AOJ_1197(){
+  vector<int> ts(6);
+  do{
+    for(int i=0;i<6;i++) cin>>ts[i];
+    int sum=0;
+    for(int t:ts) sum+=t;
+    if(sum==0) break;
+
+    auto check=
+      [&](Dice d)->int{
+        for(int i=0;i<6;i++)
+          if(d.s[i]<0) return 0;
+        int a=d.top()+d.bottom();
+        int b=d.east()+d.west();
+        int c=d.south()+d.north();
+        if(a>b+c) return 0;
+        if(b>(a+1)+c) return 0;
+        if(c>(a+1)+b) return 0;
+        return 1;
+      };
+
+    string ans(sum,'Z');
+    sort(ts.begin(),ts.end());
+    do{
+      Dice d;
+      for(int i=0;i<6;i++) d.s[i]=ts[i];
+      if(!check(d)) continue;
+
+      string res(sum,'Z');
+      int flg=0;
+      for(int i=0;i<sum;i++){
+        string B="ENSW";
+        string R="WSNE";
+        for(int j=0;j<4;j++){
+          d.roll(B[j]);
+          d.bottom()--;
+          if(check(d)){
+            res[i]=B[j];
+            break;;
+          }
+          d.bottom()++;
+          d.roll(R[j]);
+        }
+        flg|=res[i]<ans[i];
+        if(flg) continue;
+        if(res[i]>ans[i]) break;
+      }
+
+      if(flg) ans=res;
+    }while(next_permutation(ts.begin(),ts.end()));
+
+    int p,q;
+    cin>>p>>q;
+    p--;
+    if(ans[0]!='Z') cout<<ans.substr(p,q-p)<<endl;
+    else cout<<"impossible"<<endl;
+  }while(1);
+  return 0;
+}
+/*
+  verified on 2019/06/19
+  http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1197&lang=jp
 */
 
 signed main(){
@@ -161,5 +225,6 @@ signed main(){
   //ITP1_11_C();
   //ITP1_11_D();
   //AOJ_0502();
+  //AOJ_1197();
   return 0;
 }
