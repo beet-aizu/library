cc2c75dcdf37d52934baeacc627a336c108e7f62
beet-aizu <aki.bdash@gmail.com>
2019-06-19 19:18:03 +0900

    fix

--- tools/fixpoint.cpp
+++ tools/fixpoint.cpp
@@ -10,7 +10,7 @@ struct FixPoint : F{
   template<typename... Args>
   decltype(auto) operator()(Args&&... args) const{
     return F::operator()(*this,forward<Args>(args)...);
-  }  
+  }
 };
 template<typename F>
 inline decltype(auto) MFP(F&& f){
@@ -19,30 +19,33 @@ inline decltype(auto) MFP(F&& f){
 //END CUT HERE
 
 //INSERT ABOVE HERE
-signed main(){  
+signed main(){
   int h,w;
   cin>>h>>w;
   vector<string> st(h);
   for(int i=0;i<h;i++) cin>>st[i];
-  
+
   vector<vector<int> > used(h,vector<int>(w,0));
-  
+
   auto dfs=
-    makeFixPoint([&](auto dfs,int y,int x)->void{
-                   used[y][x]=1;
-                   if(y  >0&&st[y-1][x]!='#'&&!used[y-1][x]) dfs(y-1,x);
-                   if(y+1<h&&st[y+1][x]!='#'&&!used[y+1][x]) dfs(y+1,x);
-                   if(x  >0&&st[y][x-1]!='#'&&!used[y][x-1]) dfs(y,x-1);
-                   if(x+1<w&&st[y][x+1]!='#'&&!used[y][x+1]) dfs(y,x+1);
-                 });
-    
+    MFP([&](auto dfs,int y,int x)->void{
+          used[y][x]=1;
+          if(y  >0&&st[y-1][x]!='#'&&!used[y-1][x]) dfs(y-1,x);
+          if(y+1<h&&st[y+1][x]!='#'&&!used[y+1][x]) dfs(y+1,x);
+          if(x  >0&&st[y][x-1]!='#'&&!used[y][x-1]) dfs(y,x-1);
+          if(x+1<w&&st[y][x+1]!='#'&&!used[y][x+1]) dfs(y,x+1);
+        });
+
   for(int i=0;i<h;i++)
     for(int j=0;j<w;j++)
       if(st[i][j]=='s') dfs(i,j);
-  
+
   for(int i=0;i<h;i++)
     for(int j=0;j<w;j++)
       if(st[i][j]=='g')
-        cout<<(used[i][j]?"Yes":"No")<<endl;  
+        cout<<(used[i][j]?"Yes":"No")<<endl;
   return 0;
 }
+/*
+  https://atcoder.jp/contests/atc001/tasks/dfs_a
+*/
