71847159a466209586a134b4f469deb747ecf623
beet-aizu <aki.bdash@gmail.com>
2019-07-08 15:44:41 +0900

    fix

--- tree/eulertourforedge.cpp
+++ tree/eulertourforedge.cpp
@@ -6,7 +6,7 @@ template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 //BEGIN CUT HERE
 class EulerTourForEdge{
 private:
-  vector<int> ds,us;
+  vector<int> ds,us,dep;
   int pos;
 
   void dfs(int v,int p,int d){
@@ -19,7 +19,6 @@ private:
     }
   }
 public:
-  vector<int> dep;
   vector<vector<int> > G;
 
   EulerTourForEdge(){}
@@ -40,9 +39,15 @@ public:
     return dep[u]<dep[v]?v:u;
   }
 
-  template<typename T,typename F>
-  T query(int v,F f){
-    return f(0,us[v]);
+  template<typename F>
+  void query(int v,F f){
+    f(0,us[v]);
+  }
+
+  template<typename F>
+  void query(int u,int v,F f){
+    if(dep[u]>dep[v]) swap(u,v);
+    f(us[u],us[v]);
   }
 
   template<typename T,typename G>
@@ -102,6 +107,107 @@ struct BIT{
   }
 };
 
+struct LCA{
+  const int lg = 12;
+  const int sz = 1<<lg;
+  const int ms = sz-1;
+  int n;
+  vector<int> P,D,E,A,B,T,ht;
+  vector<vector<int> > G,dat;
+  LCA(int n):
+    n(n),P(n,-1),D(n),E(n*2,0),A(n*2,-1),B(n*2/lg+1),T(sz,0),G(n){}
+
+  void add_edge(int u,int v){
+    G[u].emplace_back(v);
+    G[v].emplace_back(u);
+  }
+
+  void dfs(int v,int p,int d,int &k){
+    D[v]=k;
+    A[k]=P[v]=p;
+    E[k++]=d;
+    for(int u:G[v])
+      if(u!=p) dfs(u,v,d+1,k);
+    A[k]=P[v];
+    E[k++]=d-1;
+  }
+
+  // if it need leftmost, then add: if(E[i]==E[j]) return i<j?i:j;
+  inline int comp(int i,int j){return E[i]<E[j]?i:j;};
+  inline int comp(int i,int j,int k){return comp(comp(i,j),k);};
+
+  void build(int r=0){
+    int k=0;
+    dfs(r,-1,1,k);
+
+    B[0]=1;
+    for(int i=1;i<n*2;i++) B[i/lg]|=(E[i-1]<E[i])<<(i%lg);
+
+    for(int b=0;b<sz;b++){
+      int e=0,w=1,&x=T[b];
+      for(int i=0;i<lg;i++){
+        if((b>>i)&1) e++;
+        else e--;
+        if(e<w) e=w,x=i;
+      }
+    }
+
+    int m=(n*2+lg-1)/lg;
+    int h=1;
+    while((1<<h)<m) h++;
+    dat.assign(h,vector<int>(m,-1));
+    ht.assign(m+1,0);
+    for(int j=2;j<=m;j++) ht[j]=ht[j>>1]+1;
+
+    for(int j=0;j<n*2;j++){
+      if(dat[0][j/lg]<0) dat[0][j/lg]=j;
+      dat[0][j/lg]=comp(dat[0][j/lg],j);
+    }
+
+    for(int i=1,p=1;i<h;i++,p<<=1)
+      for(int j=0;j<m;j++)
+        dat[i][j]=comp(dat[i-1][j],dat[i-1][min(j+p,m-1)]);
+  }
+
+  inline int cs(int a,int b){
+    int l=b-a;
+    return comp(dat[ht[l]][a],dat[ht[l]][b-(1<<ht[l])]);
+  }
+
+  inline int es(int i,int l,int r){
+    int x=r-i*lg+1,y=l-i*lg;
+    int b=(((B[i]|(ms<<x))>>y)|(ms<<(lg-y)))&ms;
+    return l+T[b];
+  }
+
+  inline int ls(int i,int l){
+    int k=l-i*lg;
+    int b=((B[i]>>k)|(ms<<(lg-k)))&ms;
+    return l+T[b];
+  }
+
+  inline int rs(int j,int r){
+    int k=r-j*lg+1;
+    int b=(B[j]|(ms<<k))&ms;
+    return j*lg+T[b];
+  }
+
+  inline int rmq(int l,int r){
+    int i=l/lg,j=r/lg;
+    if(i==j) return es(i,l,r);
+    if(i+1==j) return comp(ls(i,l),rs(j,r));
+    return comp(ls(i,l),cs(i+1,j),rs(j,r));
+  }
+
+  int lca(int l,int r){
+    if(l==r) return l;
+    if(D[l]>D[r]) swap(l,r);
+    int x=D[l],y=D[r];
+    int m=rmq(x,y);
+    return m==x?l:A[m];
+  }
+};
+
 //INSERT ABOVE HERE
 signed AOJ_GRL_5_D(){
   cin.tie(0);
@@ -136,8 +242,9 @@ signed AOJ_GRL_5_D(){
     if(t==1){
       int u;
       cin>>u;
-      auto f=[&](int l,int r){return bit.query0(l,r);};
-      cout<<et.query<int>(u,f)<<"\n";
+      int res=0;
+      et.query(u,[&](int l,int r){res+=bit.query0(l,r);});
+      cout<<res<<"\n";
     }
   }
   cout<<flush;
@@ -147,7 +254,99 @@ signed AOJ_GRL_5_D(){
   verified on 2019/07/08
   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_D&lang=jp
 */
+
+signed ABC133_F(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n,q;
+  cin>>n>>q;
+  vector<int> as(n),bs(n),cs(n),ds(n);
+  vector<int> xs(q),ys(q),us(q),vs(q);
+
+  vector< vector<int> > C(n),D(n);
+  EulerTourForEdge et(n);
+  LCA lca(n);
+  for(int i=1;i<n;i++){
+    cin>>as[i]>>bs[i]>>cs[i]>>ds[i];
+    as[i]--;bs[i]--;
+    et.add_edge(as[i],bs[i]);
+    lca.add_edge(as[i],bs[i]);
+    C[cs[i]].emplace_back(i);
+  }
+  et.build();
+  lca.build();
+
+  for(int i=0;i<q;i++){
+    cin>>xs[i]>>ys[i]>>us[i]>>vs[i];
+    us[i]--;vs[i]--;
+    D[xs[i]].emplace_back(i);
+  }
+
+  int ti=0;
+  BIT<int> cnt(n*2,ti);
+  BIT<int> sum(n*2,ti);
+
+  for(int i=1;i<n;i++){
+    int ch=et.child(as[i],bs[i]);
+    auto g=[&](int v,int d){sum.add0(v,d);};
+    et.update(ch,+ds[i],g);
+  }
+
+  vector<int> ans(q,0);
+  for(int i=0;i<q;i++){
+    int r=lca.lca(us[i],vs[i]);
+    auto f=[&](int l,int r){ans[i]+=sum.query0(l,r);};
+    et.query(r,us[i],f);
+    et.query(r,vs[i],f);
+  }
+
+  for(int i=1;i<n;i++){
+    int ch=et.child(as[i],bs[i]);
+    auto g=[&](int v,int d){sum.add0(v,d);};
+    et.update(ch,-ds[i],g);
+  }
+
+  for(int t=1;t<n;t++){
+    auto g1=[&](int v,int d){cnt.add0(v,d);};
+    auto g2=[&](int v,int d){sum.add0(v,d);};
+
+    for(int i:C[t]){
+      int ch=et.child(as[i],bs[i]);
+      et.update(ch,+1,g1);
+      et.update(ch,+ds[i],g2);
+    }
+
+    for(int i:D[t]){
+      int res=0,num=0;
+      auto f=[&](int l,int r){
+             num+=cnt.query0(l,r);
+             res+=sum.query0(l,r);
+           };
+      int r=lca.lca(us[i],vs[i]);
+      et.query(r,us[i],f);
+      et.query(r,vs[i],f);
+      ans[i]+=num*ys[i];
+      ans[i]-=res;
+    }
+
+    for(int i:C[t]){
+      int ch=et.child(as[i],bs[i]);
+      et.update(ch,-1,g1);
+      et.update(ch,-ds[i],g2);
+    }
+  }
+
+  for(int i=0;i<q;i++) cout<<ans[i]<<"\n";
+  cout<<flush;
+  return 0;
+}
+/*
+  verified on 2019/07/08
+  https://atcoder.jp/contests/abc133/tasks/abc133_f
+*/
 signed main(){
-  AOJ_GRL_5_D();
+  //AOJ_GRL_5_D();
+  //ABC133_F();
   return 0;
 }
