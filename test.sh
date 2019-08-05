a8f4113f69b2b190c38e9e8d7c047c65cfdcccd2
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-08-05 21:34:11 +0900

    add .travis.yml and test.sh

---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# install: pip3 install online-judge-tools==6
+set -e
+which oj > /dev/null
+
+CXX=${CXX:-g++}
+CXXFLAGS="${CXXFLANGS:--std=c++14 -O2 -Wall -g}"
+
+run() {
+    file="$1"
+    url="$(grep -o '^# *define \+PROBLEM \(https\?://.*\)' < "$file" | sed 's/.* http/http/')"
+    dir=test/$(echo -n "$url" | md5sum | sed 's/ .*//')
+    mkdir -p ${dir}
+    $CXX $CXXFLAGS -I . -o ${dir}/a.out "$file"
+    if [[ -n ${url} ]] ; then
+        if [[ ! -e ${dir}/test ]] ; then
+            sleep 2
+            oj download --system "$url" -d ${dir}/test
+        fi
+        oj test -c ${dir}/a.out -d ${dir}/test
+    else
+        ${dir}/a.out
+    fi
+}
+
+if [ $# -eq 0 ] ; then
+    for f in $(find . -name \*.test.cpp) ; do
+        run $f
+    done
+else
+    for f in "$@" ; do
+        run "$f"
+    done
+fi
