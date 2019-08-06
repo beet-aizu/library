4014c6f4027318d4f26ebbdac72bfc03c408a47d
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-08-07 00:00:10 +0900

    update test/knuthyao.test.cpp

--- test/knuthyao.test.cpp
+++ test/knuthyao.test.cpp
@@ -1,29 +1,8 @@
-#include<bits/stdc++.h>
-using namespace std;
-using Int = long long;
-//BEGIN CUT HERE
-template<typename T, typename F>
-T KnuthYao(int n,F cost){
-  vector< vector<T> > dp(n,vector<T>(n));
-  vector< vector<int> > ar(n,vector<int>(n));
-  for(int i=0;i<n;i++) dp[i][i]=T(0),ar[i][i]=i;
-  for(int w=1;w<n;w++){
-    for(int i=0;i+w<n;i++){
-      int j=i+w;
-      int p=ar[i][j-1],q=ar[i+1][j];
-      dp[i][j]=dp[i][p]+dp[p+1][j]+cost(i,p,j);
-      ar[i][j]=p;
-      for(int k=p;k<=q&&k+1<=j;k++){
-        T res=dp[i][k]+dp[k+1][j]+cost(i,k,j);
-        if(res<dp[i][j]) dp[i][j]=res,ar[i][j]=k;
-      }
-    }
-  }
-  return dp[0][n-1];
-}
-//END CUT HERE
-//INSERT ABOVE HERE
 #define PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2488
+#define main moin
+#include "../algorithm/knuthyao.cpp"
+#undef main
+
 signed main(){
   using ll = long long;
   int n;
@@ -37,4 +16,3 @@ signed main(){
   cout<<KnuthYao<ll>(n,calc)<<endl;
   return 0;
 }
-// verified on 2019/06/27
