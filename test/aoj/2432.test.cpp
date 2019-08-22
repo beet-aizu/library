0fa5e484eed7b296a37b08826b4504d1e7dd7b3d
beet-aizu <aki.bdash@gmail.com>
2019-08-22 21:50:33 +0900

    rm test

--- test/aoj/2432.test.cpp
+++ test/aoj/2432.test.cpp
@@ -7,6 +7,8 @@ using namespace std;
 #include "../../matrix/squarematrix.cpp"
 #undef call_from_test
 
+#define IGNORE
+
 template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
 template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
