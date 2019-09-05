11bdc7fb5f0e8b26e3911c9630f07ef18f06a6b7
beet-aizu <aki.bdash@gmail.com>
2019-09-05 17:25:16 +0900

    fix

--- test/aoj/1599.test.cpp
+++ test/aoj/1599.test.cpp
@@ -4,25 +4,11 @@
 using namespace std;
 
 #define call_from_test
+#include "../../tools/compress.cpp"
 #include "../../flow/dinic.cpp"
 #undef call_from_test
 
-template<typename T>
-vector<T> compress(vector<T> v){
-  sort(v.begin(),v.end());
-  v.erase(unique(v.begin(),v.end()),v.end());
-  return v;
-}
-
-template<typename T>
-map<T, int> dict(const vector<T> &v){
-  map<T, int> res;
-  for(int i=0;i<(int)v.size();i++)
-    res[v[i]]=i;
-  return res;
-}
-
-int AOJ_1599(){
+int main(){
   cin.tie(0);
   ios::sync_with_stdio(0);
 
