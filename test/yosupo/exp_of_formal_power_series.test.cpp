90b7117a1006d55db81cf0365cb5df1d75ca2578
beet-aizu <aki.bdash@gmail.com>
2019-09-20 19:13:08 +0900

    add exp

---
+++ test/yosupo/exp_of_formal_power_series.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "math/exp_of_formal_power_series"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../mod/mint.cpp"
+#include "../../convolution/numbertheoretictransform.cpp"
+#include "../../polynomial/formalpowerseries.cpp"
+#undef call_from_test
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n;
+  cin>>n;
+
+  NTT<2> ntt;
+  using M = NTT<2>::M;
+  auto conv=[&](auto as,auto bs){return ntt.multiply(as,bs);};
+  FormalPowerSeries<M> FPS(conv);
+
+  vector<M> as(n);
+  for(int i=0;i<n;i++) cin>>as[i].v;
+
+  auto bs=FPS.exp(as,n+1);
+  for(int i=0;i<n;i++){
+    if(i) cout<<" ";
+    cout<<bs[i];
+  }
+  cout<<endl;
+  return 0;
+}
