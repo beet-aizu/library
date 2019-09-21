0aa561171ceeeed2cc3531f924ba24d2a5a31cf1
beet-aizu <aki.bdash@gmail.com>
2019-09-21 12:51:56 +0900

    add 2985

--- convolution/fastfouriertransform.cpp
+++ convolution/fastfouriertransform.cpp
@@ -1,17 +1,17 @@
+#ifndef call_from_test
 #include<bits/stdc++.h>
 using namespace std;
-using Int = long long;
-
+#endif
 //BEGIN CUT HERE
 namespace FFT{
   using dbl = double;
-  
+
   struct num{
     dbl x,y;
     num(){x=y=0;}
     num(dbl x,dbl y):x(x),y(y){}
   };
-  
+
   inline num operator+(num a,num b){
     return num(a.x+b.x,a.y+b.y);
   }
@@ -24,20 +24,20 @@ namespace FFT{
   inline num conj(num a){
     return num(a.x,-a.y);
   }
- 
+
   int base=1;
   vector<num> rts={{0,0},{1,0}};
   vector<int> rev={0,1};
- 
+
   const dbl PI=acosl(-1.0);
- 
+
   void ensure_base(int nbase){
     if(nbase<=base) return;
- 
+
     rev.resize(1<<nbase);
     for(int i=0;i<(1<<nbase);i++)
       rev[i]=(rev[i>>1]>>1)+((i&1)<<(nbase-1));
- 
+
     rts.resize(1<<nbase);
     while(base<nbase){
       dbl angle=2*PI/(1<<(base+1));
@@ -49,18 +49,18 @@ namespace FFT{
       base++;
     }
   }
- 
+
   void fft(vector<num> &a,int n=-1){
     if(n==-1) n=a.size();
     assert((n&(n-1))==0);
- 
+
     int zeros=__builtin_ctz(n);
     ensure_base(zeros);
     int shift=base-zeros;
     for(int i=0;i<n;i++)
       if(i<(rev[i]>>shift))
         swap(a[i],a[rev[i]>>shift]);
- 
+
     for(int k=1;k<n;k<<=1){
       for(int i=0;i<n;i+=2*k){
         for(int j=0;j<k;j++){
@@ -71,15 +71,15 @@ namespace FFT{
       }
     }
   }
- 
+
   vector<num> fa;
- 
-  vector<Int> multiply(vector<int> &a,vector<int> &b){
+
+  vector<long long> multiply(vector<int> &a,vector<int> &b){
     int need=a.size()+b.size()-1;
     int nbase=0;
     while((1<<nbase)<need) nbase++;
     ensure_base(nbase);
- 
+
     int sz=1<<nbase;
     if(sz>(int)fa.size()) fa.resize(sz);
     for(int i=0;i<sz;i++){
@@ -88,7 +88,7 @@ namespace FFT{
       fa[i]=num(x,y);
     }
     fft(fa,sz);
- 
+
     num r(0,-0.25/sz);
     for(int i=0;i<=(sz>>1);i++){
       int j=(sz-i)&(sz-1);
@@ -98,17 +98,17 @@ namespace FFT{
       fa[i]=z;
     }
     fft(fa,sz);
- 
-    vector<Int> res(need);
+
+    vector<long long> res(need);
     for(int i=0;i<need;i++)
       res[i]=fa[i].x+0.5;
-    
+
     return res;
   }
-  
+
 };
 //END CUT HERE
-
+#ifndef call_from_test
 signed main(){
   int n;
   scanf("%d",&n);
@@ -118,8 +118,8 @@ signed main(){
   for(int i=1;i<=n*2;i++) printf("%lld\n",c[i]);
   return 0;
 }
-
 /*
   verified on 2017/11/14
   http://atc001.contest.atcoder.jp/tasks/fft_c
 */
+#endif
---
+++ test/aoj/2985.test.cpp
@@ -0,0 +1,37 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2985"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../mod/mint.cpp"
+#include "../../convolution/fastfouriertransform.cpp"
+#include "../../convolution/arbitrarymodconvolution.cpp"
+#include "../../polynomial/formalpowerseries.cpp"
+#undef call_from_test
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n;
+  cin>>n;
+
+  using M = Mint<int>;
+  ArbitraryModConvolution<M> arb;
+  auto conv=[&](auto as,auto bs){return arb.multiply(as,bs);};
+  FormalPowerSeries<M> FPS(conv);
+
+  vector<M> line(n+1,0),quad(n+1,0);
+  for(int i=1;i<=n;i++){
+    line[i]=M(i).pow(0);
+    quad[i]=M(i).pow(1);
+  }
+
+  M v1=FPS.exp(line,n+1)[n];
+  M v2=FPS.exp(quad,n+1)[n];
+
+  M ans=v2-v1*v1;
+  cout<<ans<<endl;
+  return 0;
+}
