6a2c3d357de1afca399e38af89d30a04abccb95c
beet-aizu <aki.bdash@gmail.com>
2019-09-22 14:27:36 +0900

    add verofy

--- graph/topologicalsort.cpp
+++ graph/topologicalsort.cpp
@@ -1,67 +1,45 @@
+#ifndef call_from_test
 #include<bits/stdc++.h>
 using namespace std;
 using Int = long long;
+#endif
 //BEGIN CUT HERE
 struct TopologicalSort{
-  int n;
-  vector<set<int> > G;
-  vector<bool> used;
-  vector<int> indeg,p;
-  
+  vector< set<int> > G;
+  vector<int> used,indeg,ps;
+
   TopologicalSort(){}
-  TopologicalSort(int sz):n(sz),G(n),used(n),indeg(n),p(0){}
+  TopologicalSort(int n):G(n),used(n,0),indeg(n,0){}
 
   void add_edge(int s,int t){
-    G[s].insert(t);
+    G[s].emplace(t);
+    indeg[t]++;
   }
-  
+
   void bfs(int s){
-    queue<int> q;
-    q.push(s);
+    queue<int> que;
+    que.emplace(s);
     used[s]=1;
-    while(!q.empty()){
-      int v=q.front();q.pop();
-      p.push_back(v);
+    while(!que.empty()){
+      int v=que.front();que.pop();
+      ps.emplace_back(v);
       for(int u:G[v]){
         indeg[u]--;
         if(indeg[u]==0&&!used[u]){
           used[u]=1;
-          q.push(u);
+          que.emplace(u);
         }
       }
     }
   }
-  
-  vector<int>  build(){
-    fill(used.begin(),used.end(),0);
-    fill(indeg.begin(),indeg.end(),0);
-    
-    for(int i=0;i<n;i++)
-      for(int v:G[i]) indeg[v]++;
-    
+
+  vector<int> build(){
+    int n=G.size();
     for(int i=0;i<n;i++)
       if(indeg[i]==0&&!used[i]) bfs(i);
-    
-    return p;
+    return ps;
   }
 };
 //END CUT HERE
-
-signed main(){
-  int n,e;
-  cin>>n>>e;
-  TopologicalSort ts(n);
-  for(int i=0;i<e;i++){
-    int s,t;
-    cin>>s>>t;
-    ts.add_edge(s,t);
-  }
-  auto p=ts.build();
-  for(int i:p) cout<<i<<endl;
-  return 0;
-}
-
-/*
-  verified on 2017/12/09
-  http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_4_B&lang=jp
-*/
+#ifndef call_from_test
+#endif
---
+++ test/aoj/GRL_4_B.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_4_B"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../graph/topologicalsort.cpp"
+#undef call_from_test
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n,e;
+  cin>>n>>e;
+  TopologicalSort ts(n);
+  for(int i=0;i<e;i++){
+    int s,t;
+    cin>>s>>t;
+    ts.add_edge(s,t);
+  }
+
+  auto ps=ts.build();
+  for(int i:ps) cout<<i<<endl;
+  return 0;
+}
