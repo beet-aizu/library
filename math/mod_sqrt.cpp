05d92091865bf746299d88f9390efb7c72d8094a
beet-aizu <aki.bdash@gmail.com>
2019-09-25 12:08:40 +0900

    add mod_sqrt / verify carmichael

---
+++ math/mod_sqrt.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+using Int = long long;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+template<typename T>
+T extgcd(T a,T b,T& x,T& y){
+  T d=a;
+  if(b!=0){
+    d=extgcd(b,a%b,y,x);
+    y-=(a/b)*x;
+  }else{
+    x=1;y=0;
+  }
+  return d;
+}
+
+template<typename T>
+T mod_inverse(T a,T mod){
+  T x,y;
+  extgcd(a,mod,x,y);
+  return (x%mod+mod)%mod;
+}
+
+//BEGIN CUT HERE
+template<typename T>
+int jacobi(T a,T mod){
+  int s=1;
+  if(a<0) a=a%mod+mod;
+  while(mod>1){
+    a%=mod;
+    if(a==0) return 0;
+    int r=__builtin_ctz(a);
+    if((r&1)&&((mod+2)&4)) s=-s;
+    a>>=r;
+    if(a&mod&2) s=-s;
+    swap(a,mod);
+  }
+  return s;
+}
+
+template<typename T>
+vector<T> mod_sqrt(T a,T mod){
+  if(mod==2) return {a&1};
+  int j=jacobi(a,mod);
+  if(j== 0) return {0};
+  if(j==-1) return {};
+
+  using ll = long long;
+  mt19937 mt;
+  ll b,d;
+  while(1){
+    b=mt()%mod;
+    d=(b*b-a)%mod;
+    if(d<0) d+=mod;
+    if(jacobi<ll>(d,mod)==-1) break;
+  }
+
+  ll f0=b,f1=1,g0=1,g1=0;
+  for(ll e=(mod+1)>>1;e;e>>=1){
+    if(e&1){
+      ll tmp=(g0*f0+d*((g1*f1)%mod))%mod;
+      g1=(g0*f1+g1*f0)%mod;
+      g0=tmp;
+    }
+    ll tmp=(f0*f0+d*((f1*f1)%mod))%mod;
+    f1=(2*f0*f1)%mod;
+    f0=tmp;
+  }
+  if(g0>mod-g0) g0=mod-g0;
+  return {T(g0),T(mod-g0)};
+}
+//END CUT HERE
+
+struct FastIO{
+  FastIO(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+  }
+}fastio_beet;
+
+//INSERT ABOVE HERE
+signed main(){
+  int T;
+  cin>>T;
+  for(int t=0;t<T;t++){
+    int y,p;
+    cin>>y>>p;
+    auto res=mod_sqrt(y,p);
+    if(res.empty()) cout<<-1<<"\n";
+    else cout<<res[0]<<"\n";
+  }
+  cout<<flush;
+  return 0;
+}
---
+++ test/aoj/2720.test.cpp
@@ -0,0 +1,70 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2720"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../math/carmichael.cpp"
+#undef call_from_test
+
+// x^n mod m
+template<typename T>
+T mpow(T x,T n,T m){
+  T r=1;
+  x%=m;
+  while(n){
+    if(n&1) r=r*x%m;
+    x=x*x%m;
+    n>>=1;
+  }
+  return r;
+}
+
+signed main(){
+  using ll = long long;
+  ll n;
+  cin>>n;
+  if(n==2){
+    cout<<1<<endl;
+    return 0;
+  }
+  for(ll i=2;i*i<=n;i++){
+    if(n%(i*i)==0){
+      cout<<-1<<endl;
+      return 0;
+    }
+  }
+
+  ll a=carmichael_lambda(n);
+  ll b=carmichael_lambda(a);
+  assert(n!=1);
+  assert(a!=1);
+  assert(b!=0);
+  {
+    ll x=mpow(n,b,a)!=1;
+    ll y=__gcd(n%a,a)!=1;
+    assert(x==y);
+  }
+  if(mpow(n,b,a)!=1){
+    assert(n%a!=1);
+    cout<<-1<<endl;
+    return 0;
+  }
+
+  if(n%a==1) b=1;
+  for(ll i=2;i*i<=b;i++){
+    if(b%i!=0) continue;
+    while(b%i==0){
+      if(mpow(n,b/i,a)==1) b/=i;
+      else break;
+    }
+    ll j=b/i;
+    if(j==1) continue;
+    while(b%j==0){
+      if(mpow(n,b/j,a)==1) b/=j;
+      else break;
+    }
+  }
+  cout<<b<<endl;
+  return 0;
+}
