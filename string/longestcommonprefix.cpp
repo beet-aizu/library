d218e2a267ee83853ea75820e9847a90f39f246b
beet-aizu <aki.bdash@gmail.com>
2019-09-25 13:24:36 +0900

    verify lcp

--- string/longestcommonprefix.cpp
+++ string/longestcommonprefix.cpp
@@ -1,13 +1,14 @@
+#ifndef call_from_test
 #include<bits/stdc++.h>
 using namespace std;
 using Int = long long;
 template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
 template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
-//BEGIN CUT HERE
-struct SuffixArray{ 
+
+struct SuffixArray{
   string s;
   vector<int> sa,rev;
-  
+
   SuffixArray(){}
   SuffixArray(const string &S):s(S){
     int n=s.size();
@@ -22,7 +23,7 @@ struct SuffixArray{
     vector<int> c(n+1,0),r(n+1),cnt(n+1);
     for(int i=0;i<=n;i++) r[i]=s[i];
     for(int len=1;len<=n;len*=2){
-      for(int i=0;i<=n;i++){       
+      for(int i=0;i<=n;i++){
         c[sa[i]]=i;
         if(i>0 &&
            r[sa[i-1]]==r[sa[i]] &&
@@ -38,17 +39,17 @@ struct SuffixArray{
       c.swap(r);
     }
     rev.resize(n+1);
-    for(int i=0;i<=n;i++) rev[sa[i]]=i;    
+    for(int i=0;i<=n;i++) rev[sa[i]]=i;
   }
   int operator[](int i) const{return sa[i];}
-  
+
   bool lt_substr(string &t,int si,int ti){
     int sn=s.size(),tn=t.size();
     while(si<sn&&ti<tn){
       if(s[si]<t[ti]) return 1;
       if(s[si]>t[ti]) return 0;
       si++;ti++;
-    }    
+    }
     return si==sn&&ti<tn;
   }
 
@@ -61,28 +62,30 @@ struct SuffixArray{
     }
     return r;
   }
-  
+
   int upper_bound(string& t){
     t.back()++;
     int res=lower_bound(t);
     t.back()--;
     return res;
   }
-  
+
   // O(|T|*log|S|)
   int count(string& T){
     return upper_bound(T)-lower_bound(T);
-  }  
+  }
 };
+#endif
+//BEGIN CUT HERE
 struct LongestCommonPrefix{
   SuffixArray sa;
-  
+
   vector<int> lcp,ht;
   vector<vector<int> > dat;
   LongestCommonPrefix(string &s):sa(s){
     int n=s.size();
     lcp.assign(n,0);
-    
+
     int t=0;
     lcp[0]=0;
     for(int i=0;i<n;i++){
@@ -93,32 +96,38 @@ struct LongestCommonPrefix{
       }
       lcp[sa.rev[i]-1]=t;
     }
-    
+
     int h=1;
     while((1<<h)<n) h++;
     dat.assign(h,vector<int>(n));
     ht.assign(n+1,0);
     for(int j=2;j<=n;j++) ht[j]=ht[j>>1]+1;
-    
+
     for(int j=0;j<n;j++) dat[0][j]=lcp[j];
     for(int i=1,p=1;i<h;i++,p<<=1)
       for(int j=0;j<n;j++)
         dat[i][j]=min(dat[i-1][j],dat[i-1][min(j+p,n-1)]);
   }
-  
-  int query(int a,int b){
-    a=sa.rev[a];b=sa.rev[b];
+
+  int query_with_sa(int a,int b){
+    assert(a!=b);
     if(a>b) swap(a,b);
     int l=b-a;
     return min(dat[ht[l]][a],dat[ht[l]][b-(1<<ht[l])]);
-  }  
+  }
+
+  // a, b are indices for string
+  int query(int a,int b){
+    return query_with_sa(sa.rev[a],sa.rev[b]);
+  }
 };
 //END CUT HERE
+#ifndef call_from_test
 //INSERT ABOVE HERE
 signed ARC060_F(){
   cin.tie(0);
   ios::sync_with_stdio(0);
-  
+
   string s;
   cin>>s;
   int n=s.size();
@@ -130,59 +139,64 @@ signed ARC060_F(){
       return 0;
     }
   }
-  
+
   vector<vector<int> > v(n+1);
   for(int i=1;i<=n;i++)
     for(int j=i+i;j<=n;j+=i)
       v[j].emplace_back(i);
 
-  LongestCommonPrefix lcp(s);  
+  LongestCommonPrefix lcp(s);
   auto check=
-    [&](int l,int r)->int{    
+    [&](int l,int r)->int{
       for(int x:v[r-l])
         if(lcp.query(l,l+x)>=r-l-x) return 0;
       return 1;
     };
-  
+
   if(check(0,n)){
     cout<<1<<endl<<1<<endl;
     return 0;
   }
-  
+
   int ans=0;
   for(int i=1;i<n;i++)
     ans+=check(0,i)&&check(i,n);
-  
+
   cout<<2<<endl<<ans<<endl;
   return 0;
 }
 /*
-  verified on 2019/05/23
-  https://beta.atcoder.jp/contests/arc060/tasks/arc060_d
+  verified on 2019/09/25
+  https://atcoder.jp/contests/arc060/tasks/arc060_d
 */
 
 signed SPOJ_LCS(){
   cin.tie(0);
   ios::sync_with_stdio(0);
-  
+
   string a,b;
   cin>>a>>b;
   string c=a+b;
   LongestCommonPrefix lcp(c);
-  
+
   int n=a.size(),m=b.size();
   int ans=0;
   for(int i=1;i<n+m;i++){
     if(lcp.sa[i]< n&&lcp.sa[i+1]< n) continue;
     if(lcp.sa[i]>=n&&lcp.sa[i+1]>=n) continue;
-    chmax(ans,lcp.lcp[i]);    
+    chmax(ans,lcp.lcp[i]);
   }
   cout<<ans<<endl;
   return 0;
 }
+/*
+  verified on 2019/09/25
+  https://www.spoj.com/problems/LCS/
+*/
 
 signed main(){
   //ARC060_F();
   //SPOJ_LCS();
   return 0;
 };
+#endif
---
+++ test/aoj/3112.test.cpp
@@ -0,0 +1,59 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=3112"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/chminmax.cpp"
+#include "../../string/suffixarray.cpp"
+#include "../../string/longestcommonprefix.cpp"
+#include "../../segtree/basic/dual.cpp"
+#undef call_from_test
+
+signed main(){
+  string s,t;
+  cin>>s>>t;
+
+  string b=s+'%'+t;
+  LongestCommonPrefix lcp(b);
+
+  int n=s.size(),m=t.size();
+
+  set<int> ss;
+  for(int i=0;i<=n+1+m;i++){
+    if(!isalpha(b[lcp.sa[i]])) continue;
+    if(n<lcp.sa[i]) ss.emplace(i);
+  }
+
+  vector<int> nx(n+1,-1);
+  for(int i=0;i<=n+1+m;i++){
+    if(!isalpha(b[lcp.sa[i]])) continue;
+    int k=lcp.sa[i];
+    if(k>=n) continue;
+
+    auto it=ss.upper_bound(i);
+    if(it!=ss.end()) chmax(nx[k],lcp.query_with_sa(i,*it));
+    if(it!=ss.begin()) it--;
+    if(it!=ss.end()) chmax(nx[k],lcp.query_with_sa(i,*it));
+  }
+
+  int len;
+  cin>>len;
+
+  auto h=[&](int a,int b){return a||b;};
+  int ei=0;
+  SegmentTree<int> seg(h,ei);
+  seg.init(n+m+1000);
+
+  seg.set_val(0,1);
+  for(int i=0;i<n;i++){
+    if(!seg.get_val(i)) continue;
+    if(nx[i]<len) continue;
+    int nl=i+len;
+    int nr=i+nx[i]+1;
+    seg.update(nl,nr,1);
+  }
+
+  cout<<(seg.get_val(n)?"Yes":"No")<<endl;
+  return 0;
+}
