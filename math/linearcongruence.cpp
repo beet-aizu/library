5c40175b820133c6cef421ebebb22c14511f19b0
beet-aizu <aki.bdash@gmail.com>
2019-10-08 13:47:12 +0900

    verify linearcongruence

--- math/linearcongruence.cpp
+++ math/linearcongruence.cpp
@@ -1,31 +1,28 @@
+#ifndef call_from_test
 #include<bits/stdc++.h>
 using namespace std;
 using Int = long long;
 template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
 template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
-
-//BEGIN CUT HERE
-
 template<typename T>
-T extgcd(T a,T b,T &x,T &y){
+T extgcd(T a,T b,T& x,T& y){
   T d=a;
   if(b!=0){
     d=extgcd(b,a%b,y,x);
     y-=(a/b)*x;
   }else{
-    x=1;
-    y=0;
+    x=1;y=0;
   }
   return d;
 }
-
 template<typename T>
 T mod_inverse(T a,T mod){
   T x,y;
   extgcd(a,mod,x,y);
-  return (mod+x%mod)%mod;
+  return (x%mod+mod)%mod;
 }
-
+#endif
+//BEGIN CUT HERE
 template<typename T>
 pair<T, T> linear_congruence(const vector<T> &A,
                              const vector<T> &B,
@@ -41,61 +38,23 @@ pair<T, T> linear_congruence(const vector<T> &A,
   return make_pair((x%m+m)%m,m);
 }
 //END CUT HERE
+#ifndef call_from_test
 
-signed AOJ_2659(){
-  Int r[111][111];
-  cin.tie(0);
-  ios::sync_with_stdio(0);
-  Int n,m,d;
-  cin>>n>>m>>d;
-  vector<Int> a(m);
-  for(Int i=0;i<m;i++) cin>>a[i];
-
-  for(Int i=0;i<d;i++)
-    for(Int j=0;j<m;j++)
-      cin>>r[i][j];
-
-  for(Int i=0;i<d;i++){
-    vector<Int> A,B,M;
-    for(Int j=0;j<m;j++){
-      if(r[i][j]<0) continue;
-      A.emplace_back(1);
-      B.emplace_back(r[i][j]);
-      M.emplace_back(a[j]);
-    }
-    auto p=linear_congruence(A,B,M);
-    if(p.second<0){
-      n=-1;
-      break;
-    }
-    if(p.second<0) p.first+=p.second;
-    n=(n-p.first)/p.second*p.second+p.first;
-  }
-  
-  cout<<n<<endl;
-  return 0;
-}
-/* 
-   verified on 2018/11/23
-   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2659
-*/
-
-
-Int isprime(Int x){
+int is_prime(Int x){
   if(x<=1) return 0;
   for(Int i=2;i*i<=x;i++)
     if(x%i==0) return 0;
   return 1;
 }
 
-signed DDCC2018QUALB_D(){  
+signed DDCC2018QUALB_D(){
   const Int n = 30;
   vector<Int> A,B,M;
 
   vector<Int> x(n+1);
   for(Int i=2;i<=n;i++){
     cin>>x[i];
-    if(!isprime(i-1)) continue;
+    if(!is_prime(i-1)) continue;
     A.emplace_back(1);
     B.emplace_back(x[i]%(i-1));
     M.emplace_back(i-1);
@@ -121,14 +80,14 @@ signed DDCC2018QUALB_D(){
       cout<<res<<endl;
       return 0;
     }
-    res+=dif;    
-  }  
+    res+=dif;
+  }
   cout<<"invalid"s<<endl;
   return 0;
 }
-/* 
-   verified on 2018/11/23
-   https://beta.atcoder.jp/contests/ddcc2019-qual/tasks/ddcc2018_qual_d
+/*
+   verified on 2019/10/08
+   https://atcoder.jp/contests/ddcc2019-qual/tasks/ddcc2018_qual_d
 */
 
 signed YUKI_186(){
@@ -139,15 +98,15 @@ signed YUKI_186(){
   else cout<<(p.first?p.first:p.second)<<endl;
   return 0;
 }
-/* 
-   verified on 2019/03/26
+/*
+   verified on 2019/10/08
    https://yukicoder.me/problems/no/186
 */
 
 //INSERT ABOVE HERE
 signed main(){
-  //AOJ_2659();
   //DDCC2018QUALB_D();
   //YUKI_186();
   return 0;
 }
+#endif
---
+++ test/aoj/2659.test.cpp
@@ -0,0 +1,49 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2659"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../math/extgcd.cpp"
+#include "../../math/linearcongruence.cpp"
+#undef call_from_test
+
+int r[111][111];
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n,m,d;
+  cin>>n>>m>>d;
+  vector<int> a(m);
+  for(int i=0;i<m;i++) cin>>a[i];
+
+  for(int i=0;i<d;i++)
+    for(int j=0;j<m;j++)
+      cin>>r[i][j];
+
+  using ll = long long;
+  ll ans=n;
+
+  for(int i=0;i<d;i++){
+    vector<ll> A,B,M;
+
+    for(int j=0;j<m;j++){
+      if(r[i][j]<0) continue;
+      A.emplace_back(1);
+      B.emplace_back(r[i][j]);
+      M.emplace_back(a[j]);
+    }
+    auto p=linear_congruence(A,B,M);
+    if(p.second<0){
+      ans=-1;
+      break;
+    }
+    if(p.second<0) p.first+=p.second;
+    ans=(ans-p.first)/p.second*p.second+p.first;
+  }
+
+  cout<<ans<<endl;
+  return 0;
+}
