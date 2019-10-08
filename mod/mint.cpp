ad98e738a87bf6f3a662fbf90f2041a742be5212
beet-aizu <aki.bdash@gmail.com>
2019-10-08 12:25:46 +0900

    split log

--- mod/mint.cpp
+++ mod/mint.cpp
@@ -43,29 +43,6 @@ struct Mint{
   bool operator!=(const Mint a)const{return v!=a.v;}
   bool operator <(const Mint a)const{return v <a.v;}
 
-  // find x s.t. a^x = b
-  static T log(T a,T b){
-    const T sq=40000;
-    unordered_map<T, T> dp;
-    dp.reserve(sq);
-    Mint res(1);
-    for(int r=0;r<sq;r++){
-      if(!dp.count(res.v)) dp[res.v]=r;
-      res*=a;
-    }
-    Mint p=Mint(a).inv().pow(sq);
-    res=b;
-    for(int q=0;q<=MOD/sq+1;q++){
-      if(dp.count(res.v)){
-        T idx=q*sq+dp[res.v];
-        if(idx>0) return idx;
-      }
-      res*=p;
-    }
-    assert(0);
-    return T(-1);
-  }
-
   static Mint comb(long long n,int k){
     Mint num(1),dom(1);
     for(int i=0;i<k;i++){
@@ -80,114 +57,7 @@ template<typename T,T MOD>
 ostream& operator<<(ostream &os,Mint<T, MOD> m){os<<m.v;return os;}
 //END CUT HERE
 #ifndef call_from_test
-template<typename R>
-struct Kitamasa{
-  using VR = vector<R>;
-  VR c;
-  vector<VR> rs;
-  int m;
-
-  Kitamasa(const VR &C)
-    :c(C),rs(1),m(c.size()){
-    rs[0].assign(2*m+1,R::add_identity());
-    rs[0][1]=R::mul_identity();
-  }
-
-  VR merge(const VR &x,const VR &y){
-    VR z(2*m+1,R::add_identity());
-    for(int i=1;i<=m;i++)
-      for(int j=1;j<=m;j++)
-        z[i+j]=z[i+j]+(x[i]*y[j]);
-
-    for(int i=2*m;i>m;z[i--]=R::add_identity())
-      for(int j=1;j<=m;j++)
-        z[i-j]=z[i-j]+(c[m-j]*z[i]);
-    return z;
-  }
-
-  R calc(const VR &A,long long n){
-    assert((int)A.size()==m);
-    VR res(rs[0]);
-    for(int i=0;n;i++,n>>=1){
-      if(i>=(int)rs.size())
-        rs.emplace_back(merge(rs[i-1],rs[i-1]));
-      if(~n&1) continue;
-      res=merge(res,rs[i]);
-    }
-    R ans=R::add_identity();
-    for(int i=1;i<=m;i++) ans=ans+(res[i]*A[i-1]);
-    return ans;
-  }
-};
-
-template<typename T>
-T extgcd(T a,T b,T& x,T& y){
-  T d=a;
-  if(b!=0){
-    d=extgcd(b,a%b,y,x);
-    y-=(a/b)*x;
-  }else{
-    x=1;y=0;
-  }
-  return d;
-}
-
-template<typename T>
-T mod_inverse(T a,T mod){
-  T x,y;
-  extgcd(a,mod,x,y);
-  return (mod+x%mod)%mod;
-}
-
 //INSERT ABOVE HERE
-signed CFR536_F(){
-  const int MOD = 998244353;
-  using M = Mint<int, MOD>;
-  using P = Mint<int, MOD-1>;
-
-  cin.tie(0);
-  ios::sync_with_stdio(0);
-
-  int k;
-  cin>>k;
-
-  vector<P> bs(k);
-  for(int i=0;i<k;i++){
-    int b;
-    cin>>b;
-    bs[i]=P(b);
-  }
-
-  int n,m;
-  cin>>n>>m;
-
-  reverse(bs.begin(),bs.end());
-  Kitamasa<P> ktms(bs);
-  vector<P> as(k,0);
-  as[k-1]=1;
-
-  int v=ktms.calc(as,--n).v;
-  int h=M::log(3,m)%(MOD-1);
-
-  if(v!=0||h!=0){
-    int g=__gcd(v,h);
-    v/=g;
-    h/=g;
-  }
-
-  if(h!=0&&__gcd(v,MOD-1)!=1){
-    cout<<-1<<endl;
-    return 0;
-  }
-
-  cout<<M(3).pow((long long)h*mod_inverse(v,MOD-1))<<endl;
-  return 0;
-}
-/*
-  verified on 2019/06/12
-  https://codeforces.com/contest/1106/problem/F
-*/
-
 signed ABC127_E(){
   cin.tie(0);
   ios::sync_with_stdio(0);
@@ -212,9 +82,7 @@ signed ABC127_E(){
   https://atcoder.jp/contests/abc127/tasks/abc127_e
 */
 
-
 signed main(){
-  //CFR536_F();
   //ABC127_E();
   return 0;
 }
