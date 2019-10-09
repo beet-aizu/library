73c15d6c8634cb337ac6a0955c0f1cb813b9c17d
beet-aizu <aki.bdash@gmail.com>
2019-10-09 11:06:58 +0900

    verify rangeslide

--- datastructure/rangeslide.cpp
+++ datastructure/rangeslide.cpp
@@ -2,8 +2,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 using Int = long long;
-template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
-template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 #endif
 //BEGIN CUT HERE
 template<typename T, typename F>
@@ -40,33 +38,12 @@ struct RangeSlide{
 };
 //END CUT HERE
 #ifndef call_from_test
-template<typename T>
-vector<T> compress(vector<T> v){
-  sort(v.begin(),v.end());
-  v.erase(unique(v.begin(),v.end()),v.end());
-  return v;
-}
-
-template<typename F>
-struct FixPoint : F{
-  FixPoint(F&& f):F(forward<F>(f)){}
-  template<typename... Args>
-  decltype(auto) operator()(Args&&... args) const{
-    return F::operator()(*this,forward<Args>(args)...);
-  }
-};
-template<typename F>
-inline decltype(auto) MFP(F&& f){
-  return FixPoint<F>{forward<F>(f)};
-}
-
 struct FastIO{
   FastIO(){
     cin.tie(0);
     ios::sync_with_stdio(0);
   }
 }fastio_beet;
-
 
 struct Precision{
   Precision(){
@@ -75,29 +52,6 @@ struct Precision{
 }precision_beet;
 
 //INSERT ABOVE HERE
-
-signed AOJ_DSL_3_D(){
-  int n,l;
-  cin>>n>>l;
-  vector<int> as(n);
-  for(int i=0;i<n;i++) cin>>as[i];
-
-  auto cmp=[](int a,int b){return a<b;};
-  RangeSlide<int, decltype(cmp)> rs(as,cmp);
-
-  for(int i=0;i+l<=n;i++) rs.add_range(i,i+l);
-  auto res=rs.build();
-  for(int i=0;i+l<=n;i++){
-    if(i) cout<<" ";
-    cout<<as[res[i]];
-  }
-  cout<<endl;
-  return 0;
-}
-/*
-  verified on 2019/05/27
-  http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_3_D&lang=jp
-*/
 signed YUKI_576(){
   int n,k;
   cin>>n>>k;
@@ -142,7 +96,6 @@ signed YUKI_576(){
 */
 
 signed main(){
-  //AOJ_DSL_3_D();
   //YUKI_576();
   return 0;
 }
---
+++ test/aoj/DSL_3_D.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_3_D"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../datastructure/rangeslide.cpp"
+#undef call_from_test
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n,l;
+  cin>>n>>l;
+  vector<int> as(n);
+  for(int i=0;i<n;i++) cin>>as[i];
+
+  auto cmp=[](int a,int b){return a<b;};
+  RangeSlide<int, decltype(cmp)> rs(as,cmp);
+
+  for(int i=0;i+l<=n;i++) rs.add_range(i,i+l);
+  auto res=rs.build();
+  for(int i=0;i+l<=n;i++){
+    if(i) cout<<" ";
+    cout<<as[res[i]];
+  }
+  cout<<endl;
+  return 0;
+}
