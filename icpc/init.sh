61bd45ec0018e19811b9a65f43bd0149b302ac75
beet-aizu <aki.bdash@gmail.com>
2019-10-14 23:06:55 +0900

    add bashrc

---
+++ icpc/init.sh
@@ -0,0 +1,8 @@
+alias g++='g++ -std=c++14 -O2 -Wall -Wextra'
+alias d++='g++ -std=c++14 -O2 -Wall -Wextra -D_GLIBCXX_DEBUG'
+
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+ulimit -s 65532
