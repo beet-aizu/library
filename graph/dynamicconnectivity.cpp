04a6a775f2b0b74bafc620e7cbe5043ccab53e39
beet-aizu <aki.bdash@gmail.com>
2019-10-15 18:59:42 +0900

    verify dynamicconnectivity

--- graph/dynamicconnectivity.cpp
+++ graph/dynamicconnectivity.cpp
@@ -1,6 +1,8 @@
+#ifndef call_from_test
 #include<bits/stdc++.h>
 using namespace std;
 using Int = long long;
+#endif
 //BEGIN CUT HERE
 struct PersistentUnionFind{
   using T = pair<int, int>;
@@ -8,7 +10,8 @@ struct PersistentUnionFind{
   vector<int> r,p;
   stack<T> st;
   PersistentUnionFind(){}
-  PersistentUnionFind(int sz):n(sz),r(sz,1),p(sz,0){iota(p.begin(),p.end(),0);}
+  PersistentUnionFind(int sz):
+    n(sz),r(sz,1),p(sz,0){iota(p.begin(),p.end(),0);}
   int find(int x){
     return x==p[x]?p[x]:find(p[x]);
   }
@@ -37,18 +40,18 @@ struct PersistentUnionFind{
 
 struct DynamicConnectivity{
   using edge = pair<int, int>;
-  using range = edge;
+  using range = pair<int, int>;
 
-  int n,q;
+  int q;
   PersistentUnionFind puf;
-  vector<vector<edge> > edges;
-  vector<pair<range, edge> > prc;
+  vector< vector<edge> > edges;
+  vector <pair<range, edge> > prc;
   map<edge, int> cnt,app;
 
   DynamicConnectivity(){}
-  DynamicConnectivity(int n,int q_):n(n),q(1),puf(n){
+  DynamicConnectivity(int n,int q_):q(1),puf(n){
     while(q<q_) q<<=1;
-    edges.resize(q*2-1);
+    edges.resize(q*2);
   }
 
   void insert(int t,int u,int v){
@@ -61,19 +64,19 @@ struct DynamicConnectivity{
     if(--cnt[e]==0) prc.emplace_back(range(app[e],t),e);
   }
 
-  void add(int a,int b,const edge &e,int k,int l,int r){
+  void add(int a,int b,edge e,int k,int l,int r){
     if(r<=a||b<=l) return;
     if(a<=l&&r<=b){
       edges[k].emplace_back(e);
       return;
     }
     int m=(l+r)>>1;
-    add(a,b,e,k*2+1,l,m);
-    add(a,b,e,k*2+2,m,r);
+    add(a,b,e,(k<<1)|0,l,m);
+    add(a,b,e,(k<<1)|1,m,r);
   }
 
-  void add(range &r,const edge &e){
-    add(r.first,r.second,e,0,0,q);
+  void add(range r,edge e){
+    add(r.first,r.second,e,1,0,q);
   }
 
   void build(){
@@ -85,42 +88,24 @@ struct DynamicConnectivity{
       add(s.first,s.second);
   }
 
-  void exec(function<void(int)> &f,int k=0){
+  template<typename F>
+  void exec(const F &f,int k=1){
     for(auto &e:edges[k])
       puf.unite(e.first,e.second);
 
-    if(k<q-1){
-      exec(f,k*2+1);
-      exec(f,k*2+2);
+    if(k<q){
+      exec(f,(k<<1)|0);
+      exec(f,(k<<1)|1);
     }else{
-      int x=k-(q-1);
-      f(x);
+      f(k-q);
     }
 
     puf.undo(edges[k].size());
   }
 };
 //END CUT HERE
-
+#ifndef call_from_test
 signed main(){
-  int n,q;
-  cin>>n>>q;
-  DynamicConnectivity dc(n,q);
-  vector<int> t(q),u(q),v(q);
-  for(int i=0;i<q;i++){
-    cin>>t[i]>>u[i]>>v[i];
-    if(t[i]==1) dc.insert(i,u[i],v[i]);
-    if(t[i]==2) dc.erase(i,u[i],v[i]);
-  }
-  dc.build();
-  function<void(int)> f=[&](int x){
-                          if(x>=q||t[x]!=3) return;
-                          cout<<(dc.puf.same(u[x],v[x])?"YES":"NO")<<endl;
-                        };
-  dc.exec(f);
   return 0;
 }
-/*
-  verified on 2018/01/07
-  http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2235
-*/
+#endif
---
+++ test/aoj/2235.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2235"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../graph/dynamicconnectivity.cpp"
+#undef call_from_test
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n,q;
+  cin>>n>>q;
+  DynamicConnectivity dc(n,q);
+  vector<int> ts(q),us(q),vs(q);
+  for(int i=0;i<q;i++){
+    cin>>ts[i]>>us[i]>>vs[i];
+    if(ts[i]==1) dc.insert(i,us[i],vs[i]);
+    if(ts[i]==2) dc.erase( i,us[i],vs[i]);
+  }
+  dc.build();
+  auto f=
+    [&](int x){
+      if(x>=q||ts[x]!=3) return;
+      cout<<(dc.puf.same(us[x],vs[x])?"YES":"NO")<<endl;
+    };
+  dc.exec(f);
+  return 0;
+}
