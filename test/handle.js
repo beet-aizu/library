e6477d80c156422f156d19ea8e81e75256e32730
beet-aizu <aki.bdash@gmail.com>
2019-10-16 11:48:03 +0900

    add escaping

--- test/handle.js
+++ test/handle.js
@@ -8,10 +8,33 @@ function getFile(url){
 	      });
 }
 
+// copied from https://iwb.jp/jquery-javascript-html-escape/
+const htmlEscape = (str) => {
+    if (!str) return;
+    return str.replace(/[<>&"'`]/g, (match) => {
+        const escape = {
+            '<': '&lt;',
+            '>': '&gt;',
+            '&': '&amp;',
+            '"': '&quot;',
+            "'": '&#39;',
+            '`': '&#x60;'
+        };
+        return escape[match];
+    });
+}
+
+function format(data){
+    data = htmlEscape(data);
+    l = data.search("BEGIN CUT HERE") + 15;
+    r = data.search("END CUT HERE");
+    return '<pre><code>' + data.substr(l, r - l) + '</pre></code>'
+}
+
 $(document).ready(function(){
     var query = window.location.search.substring(1);
     var url = decodeURIComponent(query.split('&')[0].split('=')[1]);
     getFile(url).done(function(data){
-        $("#status").html(data);
+        $("#status").html(format(data));
     });
 });
