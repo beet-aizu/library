83f8aff3364efb7ba0a5b93d511de696cc55fda8
beet-aizu <aki.bdash@gmail.com>
2019-10-17 21:41:31 +0900

    verify leastflow

--- flow/leastflow.cpp
+++ flow/leastflow.cpp
@@ -1,3 +1,4 @@
+#ifndef call_from_test
 #include<bits/stdc++.h>
 using namespace std;
 using Int = long long;
@@ -78,6 +79,7 @@ struct Dinic{
     return flow(s,t,numeric_limits<T>::max()/2);
   }
 };
+#endif
 //BEGIN CUT HERE
 template<typename T>
 struct LeastFlow{
@@ -103,47 +105,8 @@ struct LeastFlow{
   }
 };
 //END CUT HERE
-
+#ifndef call_from_test
 //INSERT ABOVE HERE
-signed AOJ_1615(){
-  cin.tie(0);
-  ios::sync_with_stdio(0);
-
-  int n,m;
-  while(cin>>n>>m,n){
-    vector<int> xs(m),ys(m);
-    for(int i=0;i<m;i++) cin>>xs[i]>>ys[i],xs[i]--,ys[i]--;
-
-    for(int d=0;d<=n;d++){
-      for(int l=n-d;l>=0;l--){
-        int r=l+d;
-
-        LeastFlow<int> G(n+m+2);
-        int S=n+m,T=n+m+1;
-        for(int i=0;i<m;i++){
-          G.add_edge(S,i,1,1);
-          G.add_edge(i,m+ys[i],0,1);
-          G.add_edge(i,m+xs[i],0,1);
-        }
-        for(int i=0;i<n;i++)
-          G.add_edge(m+i,T,l,r);
-
-        if(G.flow(S,T)==m){
-          cout<<l<<" "<<r<<endl;
-          goto END;
-        }
-      }
-    }
-  END:
-    ;
-  }
-  return 0;
-}
-/*
-  verified on 2019/06/10
-  http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1615&lang=jp
-*/
-//signed main(){AOJ_1615();return 0;}
 
 class SRMDiv0Easy {
 public:
@@ -160,3 +123,4 @@ public:
   verified on 2019/06/10
   https://vjudge.net/problem/TopCoder-14319
 */
+#endif
---
+++ test/aoj/1615.test.cpp
@@ -0,0 +1,46 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1615"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../flow/dinic.cpp"
+#include "../../flow/leastflow.cpp"
+#undef call_from_test
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n,m;
+  while(cin>>n>>m,n){
+    vector<int> xs(m),ys(m);
+    for(int i=0;i<m;i++) cin>>xs[i]>>ys[i],xs[i]--,ys[i]--;
+
+    for(int d=0;d<=n;d++){
+      for(int l=n-d;l>=0;l--){
+        int r=l+d;
+
+        int S=n+m,T=n+m+1;
+        LeastFlow<int> G(n+m+2);
+
+        for(int i=0;i<m;i++){
+          G.add_edge(S,i,1,1);
+          G.add_edge(i,m+ys[i],0,1);
+          G.add_edge(i,m+xs[i],0,1);
+        }
+
+        for(int i=0;i<n;i++)
+          G.add_edge(m+i,T,l,r);
+
+        if(G.flow(S,T)==m){
+          cout<<l<<" "<<r<<endl;
+          goto END;
+        }
+      }
+    }
+  END:
+    ;
+  }
+  return 0;
+}
