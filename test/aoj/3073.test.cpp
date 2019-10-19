10f2b2fad66a3cd98ca08b9335c108db3498d484
beet-aizu <aki.bdash@gmail.com>
2019-10-19 16:03:50 +0900

    verify 3065

---
+++ test/aoj/3073.test.cpp
@@ -0,0 +1,80 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=3073"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../string/trie.cpp"
+#include "../../string/ahocorasick.cpp"
+#include "../../segtree/basic/chien.cpp"
+#undef call_from_test
+
+signed main(){
+  string s;
+  cin>>s;
+  int n,q;
+  cin>>n>>q;
+
+  auto conv=[](char c){return c-'a';};
+  AhoCorasick<26> aho(conv);
+  aho.add(s,0);
+  aho.build(false);
+
+  using A = array<int, 32>;
+  A ti,ei;
+  fill(ti.begin(),ti.end(),0);
+  iota(ei.begin(),ei.end(),0);
+  auto f=[&](A a,A b){
+           A c;
+           for(int i=0;i<(int)a.size();i++) c[i]=a[i]+b[i];
+           return c;
+         };
+  auto g=[&](A a,A b){
+           A c(ti);
+           for(int i=0;i<(int)a.size();i++) c[b[i]]+=a[i];
+           return c;
+         };
+  auto h=[&](A a,A b){
+           A c;
+           for(int i=0;i<(int)a.size();i++) c[i]=b[a[i]];
+           return c;
+         };
+
+  SegmentTree<A, A> seg(f,g,h,ti,ei);
+  vector<A> va(n,ti);
+  for(int i=0;i<n;i++) va[i][0]=1;
+  seg.build(va);
+
+  vector<A> mv(26);
+  for(int x=0;x<26;x++){
+    for(int i=0;i<(int)mv[x].size();i++)
+      mv[x][i]=aho.move(i,char('a'+x));
+  }
+
+  for(int i=0;i<q;i++){
+    int t;
+    cin>>t;
+    if(t==1){
+      int l,r;
+      string c;
+      cin>>l>>r>>c;
+      l--;
+      A a=ei;
+      for(char x:c) a=h(a,mv[x-'a']);
+      seg.update(l,r,a);
+    }
+    if(t==2){
+      int l,r;
+      cin>>l>>r;
+      l--;
+      A res=seg.query(l,r);
+      int ans=0;
+      for(int j=0;j<(int)res.size();j++)
+        if(aho.count(j)) ans+=res[j];
+      cout<<ans<<"\n";
+    }
+  }
+  cout<<flush;
+  return 0;
+}
--- tools/fastio.cpp
+++ tools/fastio.cpp
@@ -1,6 +1,8 @@
+#ifndef call_from_test
 #include<bits/stdc++.h>
 using namespace std;
 using Int = long long;
+#endif
 //BEGIN CUT HERE
 struct FastIO{
   FastIO(){
@@ -9,8 +11,8 @@ struct FastIO{
   }
 }fastio_beet;
 //END CUT HERE
-
+#ifndef call_from_test
 signed main(){
-  
   return 0;
 }
+#endif
