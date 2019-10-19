b725d5fed7a6377641d9a803cd54babc3bc9e664
beet-aizu <aki.bdash@gmail.com>
2019-10-19 16:58:43 +0900

    verify dinic

---
+++ test/aoj/3047.test.cpp
@@ -0,0 +1,44 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=3047"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../flow/dinic.cpp"
+#undef call_from_test
+
+const int K = 28;
+int cap[K][K];
+
+signed main(){
+  int n;
+  cin>>n;
+  vector<string> s(n);
+  for(int i=0;i<n;i++) cin>>s[i];
+
+  for(int i=0;i<26;i++){
+    memset(cap,0,sizeof(cap));
+
+    int x=26,y=27,z=0,e=0;
+    Dinic<int, true> G(K);
+
+    for(int j=0;j<n;j++){
+      int u=s[j].front()-'a';
+      int v=s[j].back()-'a';
+      if(u==i) z++;
+      if(v==i) e++;
+      if(u==i) u=x;
+      if(v==i) v=y;
+      if(u!=v) cap[u][v]++;
+    }
+
+    for(int u=0;u<K;u++)
+      for(int v=0;v<K;v++)
+        if(cap[u][v]) G.add_edge(u,v,cap[u][v]);
+
+    if((e&&!z)||(z&&G.flow(x,y)==z)) cout<<char('a'+i)<<endl;
+  }
+
+  return 0;
+}
---
+++ test/aoj/3058.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=3058"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../flow/dinic.cpp"
+#undef call_from_test
+
+signed main(){
+  int n,m;
+  cin>>n>>m;
+  string s;
+  cin>>s;
+  vector<int> as(n);
+  for(int i=0;i<n;i++) cin>>as[i];
+
+  int S=n,T=n+1;
+  Dinic<int, true> G(n+2);
+
+  for(int i=0;i<m;i++){
+    int x,y,z;
+    cin>>x>>y>>z;
+    x--;y--;
+    if(x>y) swap(x,y);
+    G.add_edge(x,y,z);
+  }
+
+  for(int i=0;i<n;i++){
+    if(s[i]=='R') G.add_edge(S,i,as[i]);
+    if(s[i]=='L') G.add_edge(i,T,as[i]);
+  }
+
+  cout<<G.flow(S,T)<<endl;
+  return 0;
+}
