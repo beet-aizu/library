9c7c37e37f738b3691cc3270e7c2dbebaba0380a
beet-aizu <aki.bdash@gmail.com>
2019-10-22 12:36:09 +0900

    verify chien

---
+++ test/aoj/DSL_2_F.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_F"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../segtree/basic/chien.cpp"
+#undef call_from_test
+
+signed main(){
+  int n,q;
+  cin>>n>>q;
+
+  auto f=[](int a,int b){return min(a,b);};
+  auto g=[](int a,int b){(void)a;return b;};
+  SegmentTree<int, int> seg(f,g,g,INT_MAX,-1);
+  seg.build(vector<int>(n,INT_MAX));
+
+  for(int i=0;i<q;i++){
+    int c;
+    cin>>c;
+    if(c==0){
+      int s,t,x;
+      cin>>s>>t>>x;
+      seg.update(s,t+1,x);
+    }
+    if(c==1){
+      int s,t;
+      cin>>s>>t;
+      cout<<seg.query(s,t+1)<<"\n";
+    }
+  }
+  cout<<flush;
+  return 0;
+}
