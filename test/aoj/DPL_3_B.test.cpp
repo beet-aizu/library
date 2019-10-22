4aaa18095d20964c77defe35cef5f19c03f97450
beet-aizu <aki.bdash@gmail.com>
2019-10-22 12:56:52 +0900

    verify rectangle

---
+++ test/aoj/DPL_3_B.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_3_B"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../tools/chminmax.cpp"
+#include "../../algorithm/largestrectangle.cpp"
+#undef call_from_test
+
+signed main(){
+  int h,w;
+  cin>>h>>w;
+
+  vector< vector<int> > cs(h,vector<int>(w));
+  for(int i=0;i<h;i++)
+    for(int j=0;j<w;j++)
+      cin>>cs[i][j];
+
+  int res=0;
+  vector<int> dp(w,0);
+  for(int i=0;i<h;i++){
+    for(int j=0;j<w;j++)
+      dp[j]=cs[i][j]==0?dp[j]+1:0;
+    chmax(res,largestrectangle(dp));
+  }
+
+  cout<<res<<endl;
+  return 0;
+}
