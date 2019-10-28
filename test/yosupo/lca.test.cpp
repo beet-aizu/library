564b56fd5af8238e529118f6842e8eac7536c5ef
beet-aizu <aki.bdash@gmail.com>
2019-10-28 14:51:27 +0900

    verify lowestcommonancestor

---
+++ test/yosupo/lca.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/lca"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../tree/lowestcommonancestor.cpp"
+#undef call_from_test
+
+signed main(){
+  int n,q;
+  cin>>n>>q;
+
+  LowestCommonAncestor G(n);
+  for(int i=1;i<n;i++){
+    int p;
+    cin>>p;
+    G.add_edge(p,i);
+  }
+  G.build();
+
+  for(int i=0;i<q;i++){
+    int u,v;
+    cin>>u>>v;
+    cout<<G.lca(u,v)<<"\n";
+  }
+  cout<<flush;
+  return 0;
+}
