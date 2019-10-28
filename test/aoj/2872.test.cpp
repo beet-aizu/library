2b010330ad819ef938dc8c099f0bad5dfd0fbb73
beet-aizu <aki.bdash@gmail.com>
2019-10-28 23:05:48 +0900

    verify negativeedge

---
+++ test/aoj/2872.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2872"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../flow/primaldual.cpp"
+#include "../../flow/negativeedge.cpp"
+#include "../../graph/bellmanford.cpp"
+#undef call_from_test
+
+signed main(){
+  using ll = long long;
+  const ll INF = 1e9;
+  int n,m,s,t;
+  cin>>n>>m>>s>>t;
+  s--;t--;
+  NegativeEdge<ll, ll> G(n);
+  BellmanFord<int> H(n);
+  for(int i=0;i<m;i++){
+    int u,v,d,c;
+    cin>>u>>v>>d>>c;
+    u--;v--;
+    G.add_edge(u,v,c,d);
+    H.add_edge(u,v,d);
+  }
+  int neg_loop;
+  int len=H.shortest_path(s,t,neg_loop);
+  assert(!neg_loop);
+  G.add_edge(t,s,INF,-(len+1));
+  int ok;
+  cout<<-G.flow(ok)<<endl;
+  assert(ok);
+  return 0;
+}
