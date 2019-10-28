2cb4c3b3898ebbea48aa53a6c0b095c37c0a37f0
beet-aizu <aki.bdash@gmail.com>
2019-10-28 15:01:27 +0900

    verify unionfind & bit

---
+++ test/yosupo/point_add_range_sum.test.cpp
@@ -0,0 +1,37 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../datastructure/binaryindexedtree.cpp"
+#undef call_from_test
+
+signed main(){
+  int n,q;
+  cin>>n>>q;
+
+  vector<int> as(n);
+  for(int i=0;i<n;i++) cin>>as[i];
+
+  BIT<long long> bit(n,0);
+  for(int i=0;i<n;i++) bit.add0(i,as[i]);
+
+  for(int i=0;i<q;i++){
+    int t;
+    cin>>t;
+    if(t==0){
+      int p,x;
+      cin>>p>>x;
+      bit.add0(p,x);
+    }
+    if(t==1){
+      int l,r;
+      cin>>l>>r;
+      cout<<bit.query0(l,r)<<"\n";
+    }
+  }
+  cout<<flush;
+  return 0;
+}
---
+++ test/yosupo/unionfind.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../datastructure/unionfindtree.cpp"
+#undef call_from_test
+
+signed main(){
+  int n,q;
+  cin>>n>>q;
+
+  UnionFind uf(n);
+
+  for(int i=0;i<q;i++){
+    int t,u,v;
+    cin>>t>>u>>v;
+    if(t==0) uf.unite(u,v);
+    if(t==1) cout<<uf.same(u,v)<<"\n";
+  }
+  cout<<flush;
+  return 0;
+}
