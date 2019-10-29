9a42864566c064d3e58ab814f2e3ffa850e69fd2
beet-aizu <aki.bdash@gmail.com>
2019-10-29 13:03:24 +0900

    verify HLD BIT rangecount

---
+++ test/yosupo/rectangle_sum.test.cpp
@@ -0,0 +1,41 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/rectangle_sum"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../tools/compress.cpp"
+#include "../../segtree/count/dynamic_offline.cpp"
+#undef call_from_test
+
+signed main(){
+  int n,q;
+  cin>>n>>q;
+
+  vector<int> xs(n),ys(n),ws(n);
+  for(int i=0;i<n;i++) cin>>xs[i]>>ys[i]>>ws[i];
+
+  auto vs=compress(xs);
+  auto idx=
+    [&](int x){return lower_bound(vs.begin(),vs.end(),x)-vs.begin();};
+
+  using ll = long long;
+  RangeCount<ll> seg(vs.size());
+
+  for(int i=0;i<n;i++)
+    seg.preupdate(idx(xs[i]),ys[i]);
+
+  seg.build();
+
+  for(int i=0;i<n;i++)
+    seg.update(idx(xs[i]),ys[i],ws[i]);
+
+  for(int i=0;i<q;i++){
+    int l,d,r,u;
+    cin>>l>>d>>r>>u;
+    cout<<seg.query(idx(l),idx(r),d,u)<<"\n";
+  }
+  cout<<flush;
+  return 0;
+}
---
+++ test/yosupo/vertex_add_path_sum.test.cpp
@@ -0,0 +1,51 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/vertex_add_path_sum"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../tree/heavylightdecomposition.cpp"
+#include "../../datastructure/binaryindexedtree.cpp"
+#undef call_from_test
+
+signed main(){
+  int n,q;
+  cin>>n>>q;
+
+  vector<int> as(n);
+  for(int i=0;i<n;i++) cin>>as[i];
+
+  HLD G(n);
+  for(int i=1;i<n;i++){
+    int u,v;
+    cin>>u>>v;
+    G.add_edge(u,v);
+  }
+  G.build();
+
+  using ll = long long;
+  BIT<ll> bit(n,0);
+  for(int i=0;i<n;i++)
+    bit.add0(G.vid[i],as[i]);
+
+  for(int i=0;i<q;i++){
+    int t;
+    cin>>t;
+    if(t==0){
+      int p,x;
+      cin>>p>>x;
+      bit.add0(G.vid[p],x);
+    }
+    if(t==1){
+      int u,v;
+      cin>>u>>v;
+      ll res=0;
+      auto f=[&](int l,int r){res+=bit.query0(l,r);};
+      G.for_each(u,v,f);
+      cout<<res<<"\n";
+    }
+  }
+  cout<<flush;
+  return 0;
+}
