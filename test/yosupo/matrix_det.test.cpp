6ace1b4319db3331c43247df9eeeb95d3567c687
beet-aizu <aki.bdash@gmail.com>
2019-10-29 13:19:28 +0900

    verify determinant

---
+++ test/yosupo/matrix_det.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/matrix_det"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../mod/mint.cpp"
+#include "../../linearalgebra/matrix.cpp"
+#undef call_from_test
+
+signed main(){
+  using M = Mint<int, 998244353>;
+  using Mat = Matrix<M>;
+  int n;
+  cin>>n;
+
+  Mat A(n,n);
+  for(int i=0;i<n;i++)
+    for(int j=0;j<n;j++)
+      cin>>A[i][j].v;
+
+  cout<<A.determinant()<<endl;
+  return 0;
+}
