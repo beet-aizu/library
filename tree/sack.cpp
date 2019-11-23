b1c508028acacc744e649a6efa9aa63ad966f4a8
beet-aizu <aki.bdash@gmail.com>
2019-11-23 16:34:50 +0900

    fix sack

--- tree/sack.cpp
+++ tree/sack.cpp
@@ -1,6 +1,7 @@
+#ifndef call_from_test
 #include<bits/stdc++.h>
 using namespace std;
-using Int = long long;
+#endif
 //BEGIN CUT HERE
 struct Sack{
   using F = function<void(int)>;
@@ -8,12 +9,12 @@ struct Sack{
   vector<int> sz,hvy,big;
   vector<vector<int> > G,Q;
   F expand,shrink,query,reset;
-  
+
   Sack(int n,F expand,F shrink,F query,F reset):
-    sz(n),hvy(n,-1),big(n,0),G(n),Q(n),
+    sz(n,1),hvy(n,-1),big(n,0),G(n),Q(n),
     expand(expand),shrink(shrink),
     query(query),reset(reset){}
-  
+
   void add_edge(int u,int v){
     G[u].emplace_back(v);
     G[v].emplace_back(u);
@@ -21,10 +22,10 @@ struct Sack{
 
   void add_query(int v,int k){
     Q[v].emplace_back(k);
+    sz[v]++;
   }
-  
+
   void build(int v=0,int p=-1){
-    sz[v]=1;
     for(int u:G[v]){
       if(u==p) continue;
       build(u,v);
@@ -52,19 +53,21 @@ struct Sack{
     if(~hvy[v]) big[hvy[v]]=0;
     if(!k) add(v,p,0),reset(v);
   }
-  
+
 };
 //END CUT HERE
+#ifndef call_from_test
 //INSERT ABOVE HERE
-signed ECR2_E(){
+signed ECR002_E(){
+  using ll = long long;
   int n;
   scanf("%d",&n);
   vector<int> c(n);
   for(int i=0;i<n;i++) scanf("%d",&c[i]);
   vector<int> cnt(n+1,0);
-  vector<Int> ans(n,0);
+  vector<ll> ans(n,0);
   int mx=0;
-  Int res=0;
+  ll res=0;
   auto expand=
     [&](int v){
       cnt[c[v]]++;
@@ -72,11 +75,11 @@ signed ECR2_E(){
         mx=cnt[c[v]];
         res=0;
       }
-      if(mx==cnt[c[v]]) res+=c[v];      
-    };  
+      if(mx==cnt[c[v]]) res+=c[v];
+    };
   auto shrink=[&](int v){cnt[c[v]]--;};
   auto query =[&](int v){ans[v]=res;};
-  auto reset =[&](int v){v++;mx=res=0;};  
+  auto reset =[&](int v){v++;mx=res=0;};
   Sack sc(n,expand,shrink,query,reset);
   for(int i=1;i<n;i++){
     int x,y;
@@ -94,31 +97,29 @@ signed ECR2_E(){
   puts("");
   return 0;
 }
-
 /*
-  verified on 2018/07/16
+  verified on 2019/11/23
   http://codeforces.com/contest/600/problem/E
 */
-
 
 signed CFR316_D(){
   int n,q;
   scanf("%d %d",&n,&q);
-  
+
   vector<int> c(n),dep(n);
   vector<int> vs(q),hs(q),ans(q);
   vector<int> cnt(n+1,0),odd(n+1,0);
-  
+
   auto flip=
     [&](int v){
       odd[dep[v]]-=(cnt[dep[v]]>>c[v])&1;
       cnt[dep[v]]^=(1<<c[v]);
       odd[dep[v]]+=(cnt[dep[v]]>>c[v])&1;
-    };  
-  
+    };
+
   auto query =[&](int k){ans[k]=odd[hs[k]]<=1;};
   auto reset =[&](int v){v++;};
-  
+
   Sack sc(n,flip,flip,query,reset);
   for(int i=1;i<n;i++){
     int p;
@@ -127,7 +128,7 @@ signed CFR316_D(){
     sc.add_edge(p,i);
   }
   sc.build();
-  
+
   {
     auto &G=sc.G;
     function<void(int,int,int)> dfs=
@@ -136,46 +137,45 @@ signed CFR316_D(){
         for(int u:G[v]){
           if(u==p) continue;
           dfs(u,v,d+1);
-        }	
+        }
       };
     dfs(0,-1,1);
   }
-  
+
   char* buf = new char[500500];
   scanf("%s",buf);
   for(int i=0;i<n;i++) c[i]=buf[i]-'a';
-  
+
   for(int i=0;i<q;i++){
     scanf("%d %d",&vs[i],&hs[i]);
     vs[i]--;
     sc.add_query(vs[i],i);
   }
-  
+
   sc.dfs();
   for(int i=0;i<q;i++)
     puts(ans[i]?"Yes":"No");
   return 0;
 }
 /*
-  verified on 2018/07/16
+  verified on 2019/11/23
   http://codeforces.com/contest/570/problem/D
 */
-
 
 
 signed CFR130_E(){
   int n;
   scanf("%d",&n);
-  
+
   vector<int> dep(n),rs;
   vector<int> vs,hs,ans;
   vector<int> cnt(n+1,0);
-  
-  auto expand=[&](int v){cnt[dep[v]]++;};  
+
+  auto expand=[&](int v){cnt[dep[v]]++;};
   auto shrink=[&](int v){cnt[dep[v]]--;};
   auto query =[&](int k){ans[k]=cnt[dep[vs[k]]+hs[k]]-1;};
   auto reset =[&](int v){v++;};
-  
+
   Sack sc(n,expand,shrink,query,reset);
   for(int i=0;i<n;i++){
     int p;
@@ -185,7 +185,7 @@ signed CFR130_E(){
     else rs.emplace_back(i);
   }
   sc.build();
-  
+
   vector<vector<int> > ps(20,vector<int>(n,-1));
   {
     auto &G=sc.G;
@@ -196,11 +196,11 @@ signed CFR130_E(){
         for(int u:G[v]){
           if(u==p) continue;
           dfs(u,v,d+1);
-        }	
+        }
       };
     for(int r:rs) dfs(r,-1,1);
   }
-  
+
   int q;
   scanf("%d",&q);
   vs.assign(q,0);
@@ -210,21 +210,20 @@ signed CFR130_E(){
   for(int k=0;k+1<20;k++)
     for(int i=0;i<n;i++)
       ps[k+1][i]=ps[k][i]<0?-1:ps[k][ps[k][i]];
-  
-  auto calc=
-                          [&](int v,int k){
-                            for(int i=0;~v&&i<20;i++)
-                              if((k>>i)&1) v=ps[i][v];
-                            return v;
-                          };
-  
+
+  auto calc=[&](int v,int k){
+              for(int i=0;~v&&i<20;i++)
+                if((k>>i)&1) v=ps[i][v];
+              return v;
+            };
+
   for(int i=0;i<q;i++){
     scanf("%d %d",&vs[i],&hs[i]);
     vs[i]--;
     vs[i]=calc(vs[i],hs[i]);
     if(~vs[i]) sc.add_query(vs[i],i);
   }
-  
+
   for(int r:rs) sc.dfs(r,-1,0);
   for(int i=0;i<q;i++){
     if(i) printf(" ");
@@ -234,13 +233,14 @@ signed CFR130_E(){
   return 0;
 }
 /*
-  verified on 2018/07/16
+  verified on 2019/11/23
   https://codeforces.com/contest/208/problem/E
 */
 
 signed main(){
-  //ECR2_E();
+  //ECR002_E();
   //CFR316_D();
-  CFR130_E();
+  //CFR130_E();
   return 0;
 }
+#endif
