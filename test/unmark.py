56311a167a2d6e1e4130e600106c2efa244ee881
beet-aizu <aki.bdash@gmail.com>
2019-11-30 20:53:43 +0900

    unmark

--- test/unmark.py
+++ test/unmark.py
@@ -6,6 +6,6 @@ import os
 
 h = hashlib.md5()
 h.update(sys.argv[1].encode('utf-8'))
-s = 'test/timestamp/' + h.hexdigest()
+s = '.verify-helper/timestamp/' + h.hexdigest()
 print('remove ' + s)
-os.remove('test/timestamp/' + h.hexdigest())
+os.remove('.verify-helper/timestamp/' + h.hexdigest())
