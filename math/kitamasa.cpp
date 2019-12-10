d32b44b6ef7868a3785593a10f723498644f5054
beet-aizu <aki.bdash@gmail.com>
2019-12-10 13:41:29 +0900

    verify kitamasa

--- math/kitamasa.cpp
+++ math/kitamasa.cpp
@@ -6,26 +6,26 @@ using namespace std;
 template<typename R>
 struct Kitamasa{
   using VR = vector<R>;
-  VR c;
+  VR cs;
   vector<VR> rs;
   int m;
 
   Kitamasa(const VR &C)
-    :c(C),rs(1),m(c.size()){
+    :cs(C),rs(1),m(C.size()){
     rs[0].assign(2*m+1,R::add_identity());
     rs[0][1]=R::mul_identity();
   }
 
-  VR merge(const VR &x,const VR &y){
-    VR z(2*m+1,R::add_identity());
+  VR merge(const VR &xs,const VR &ys){
+    VR zs(2*m+1,R::add_identity());
     for(int i=1;i<=m;i++)
       for(int j=1;j<=m;j++)
-        z[i+j]=z[i+j]+(x[i]*y[j]);
+        zs[i+j]=zs[i+j]+(xs[i]*ys[j]);
 
-    for(int i=2*m;i>m;z[i--]=R::add_identity())
+    for(int i=2*m;i>m;zs[i--]=R::add_identity())
       for(int j=1;j<=m;j++)
-        z[i-j]=z[i-j]+(c[m-j]*z[i]);
-    return z;
+        zs[i-j]=zs[i-j]+(cs[m-j]*zs[i]);
+    return zs;
   }
 
   R calc(const VR &A,long long n){
@@ -57,14 +57,14 @@ signed TDPC_F(){
   scanf("%d %lld",&n,&k);
 
   using M = Mint<int>;
-  vector<M> a(n,1),c(n,1);
-  Kitamasa<M> fib(c);
+  vector<M> as(n,1),cs(n,1);
+  Kitamasa<M> fib(cs);
 
-  printf("%d\n",fib.calc(a,--k).v);
+  printf("%d\n",fib.calc(as,--k).v);
   return 0;
 }
 /*
-  verified on 2019/10/24
+  verified on 2019/12/10
   https://atcoder.jp/contests/tdpc/tasks/tdpc_fibonacci
 */
 
@@ -82,16 +82,16 @@ signed ABC009_D(){
     static M mul_identity(){return M(~0u);}
   };
 
-  vector<M> a(k),c(k);
-  for(int i=0;i<k;i++) scanf("%u",&a[i].v);
-  for(int i=0;i<k;i++) scanf("%u",&c[k-(i+1)].v);
-  Kitamasa<M> v(c);
+  vector<M> as(k),cs(k);
+  for(int i=0;i<k;i++) scanf("%u",&as[i].v);
+  for(int i=0;i<k;i++) scanf("%u",&cs[k-(i+1)].v);
+  Kitamasa<M> v(cs);
 
-  printf("%u\n",v.calc(a,--m).v);
+  printf("%u\n",v.calc(as,--m).v);
   return 0;
 }
 /*
-  verified on 2018/10/24
+  verified on 2019/12/10
   https://atcoder.jp/contests/abc009/tasks/abc009_4
 */
 
---
+++ test/aoj/0168.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0168"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../math/kitamasa.cpp"
+#undef call_from_test
+
+template<typename T>
+struct Z{
+  T v;
+  Z(){*this=add_identity();}
+  Z(T v):v(v){}
+  Z operator+(const Z &a)const{return Z(v+a.v);}
+  Z operator*(const Z &a)const{return Z(v*a.v);}
+  static Z add_identity(){return Z(0);}
+  static Z mul_identity(){return Z(1);}
+};
+
+signed main(){
+  using ZL = Z<long long>;
+
+  vector<ZL> as({0,0,1}),cs({1,1,1});
+  Kitamasa<ZL> K(cs);
+
+  int n;
+  while(cin>>n,n)
+    cout<<(K.calc(as,n+2).v+3650-1)/3650<<endl;
+
+  return 0;
+}
