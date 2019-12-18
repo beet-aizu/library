0b814e09d8f625b2e3d99ac3679476e8c784a985
beet-aizu <aki.bdash@gmail.com>
2019-12-18 11:12:58 +0900

    fix again

--- test/aoj/DPL_5_J.test.cpp
+++ test/aoj/DPL_5_J.test.cpp
@@ -6,11 +6,12 @@ using namespace std;
 #define call_from_test
 #include "../../mod/mint.cpp"
 #include "../../mod/enumeration.cpp"
+#include "../../mod/partition_function.cpp"
 #undef call_from_test
 
 signed main(){
   int n,k;
   scanf("%d %d",&n,&k);
-  printf("%d\n",Enumeration<Mint<int>>::D(n,k)[n][k].v);
+  printf("%d\n",Partition<Mint<int>>(n,k)(n,k).v);
   return 0;
 }
