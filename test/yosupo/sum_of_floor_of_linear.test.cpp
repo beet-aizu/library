c7f8cf1757c35b4e469109c2672814e6f7cb3394
beet-aizu <aki.bdash@gmail.com>
2019-12-21 18:06:58 +0900

    add sum_of_floor

---
+++ test/yosupo/sum_of_floor_of_linear.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sum_of_floor_of_linear"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../math/sum_of_floor.cpp"
+#undef call_from_test
+
+signed main(){
+  int T;
+  cin>>T;
+  for(int t=0;t<T;t++){
+    long long n,m,a,b;
+    cin>>n>>m>>a>>b;
+    cout<<sum_of_floor(n,m,a,b)<<"\n";
+  }
+  cout<<flush;
+  return 0;
+}
