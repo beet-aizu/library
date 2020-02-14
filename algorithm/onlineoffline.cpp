89f772796db7dc901903797e1145fd6c7adf6da7
beet-aizu <aki.bdash@gmail.com>
2020-02-15 01:15:34 +0900

    test update

--- algorithm/onlineoffline.cpp
+++ algorithm/onlineoffline.cpp
@@ -51,86 +51,8 @@ namespace OnlineOffline{
 };
 //END CUT HERE
 #ifndef call_from_test
-
-#define call_from_test
-#include "../tools/fastio.cpp"
-#undef call_from_test
-
 //INSERT ABOVE HERE
-using ll = long long;
-signed YUKI_703(){
-  int n;
-  cin>>n;
-  vector<ll> as(n),xs(n),ys(n);
-  for(int i=0;i<n;i++) cin>>as[i];
-  for(int i=0;i<n;i++) cin>>xs[i];
-  for(int i=0;i<n;i++) cin>>ys[i];
-
-  auto dist=
-    [&](int i,int j)->ll{
-      ll s=abs(xs[i]-as[j]);
-      ll t=abs(ys[i]);
-      return s*s+t*t;
-    };
-
-  cout<<OnlineOffline::solve<ll>(n,dist)<<endl;
-  return 0;
-}
-/*
-  verified on 2020/02/06
-  https://yukicoder.me/problems/no/703
-*/
-
-signed YUKI_704(){
-  int n;
-  cin>>n;
-  vector<ll> as(n),xs(n),ys(n);
-  for(int i=0;i<n;i++) cin>>as[i];
-  for(int i=0;i<n;i++) cin>>xs[i];
-  for(int i=0;i<n;i++) cin>>ys[i];
-
-  auto dist=
-    [&](int i,int j)->ll{
-      ll s=abs(xs[i]-as[j]);
-      ll t=abs(ys[i]);
-      return s+t;
-    };
-
-  cout<<OnlineOffline::solve<ll>(n,dist)<<endl;
-  return 0;
-}
-/*
-  verified on 2020/02/06
-  https://yukicoder.me/problems/no/704
-*/
-
-signed YUKI_705(){
-  int n;
-  cin>>n;
-  vector<ll> as(n),xs(n),ys(n);
-  for(int i=0;i<n;i++) cin>>as[i];
-  for(int i=0;i<n;i++) cin>>xs[i];
-  for(int i=0;i<n;i++) cin>>ys[i];
-
-  auto dist=
-    [&](int i,int j)->ll{
-      ll s=abs(xs[i]-as[j]);
-      ll t=abs(ys[i]);
-      return s*s*s+t*t*t;
-    };
-
-  cout<<OnlineOffline::solve<ll>(n,dist)<<endl;
-  return 0;
-}
-/*
-  verified on 2020/02/06
-  https://yukicoder.me/problems/no/705
-*/
-
 signed main(){
-  //YUKI_703();
-  //YUKI_704();
-  //YUKI_705();
   return 0;
 }
 #endif
---
+++ test/yukicoder/1948.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://yukicoder.me/problems/1948"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tools/fastio.cpp"
+#include "../../algorithm/onlineoffline.cpp"
+#undef call_from_test
+
+signed main(){
+  using ll = long long;
+
+  int n;
+  cin>>n;
+  vector<ll> as(n),xs(n),ys(n);
+  for(int i=0;i<n;i++) cin>>as[i];
+  for(int i=0;i<n;i++) cin>>xs[i];
+  for(int i=0;i<n;i++) cin>>ys[i];
+
+  auto dist=
+    [&](int i,int j)->ll{
+      ll s=abs(xs[i]-as[j]);
+      ll t=abs(ys[i]);
+      return s*s+t*t;
+    };
+
+  cout<<OnlineOffline::solve<ll>(n,dist)<<endl;
+  return 0;
+}
