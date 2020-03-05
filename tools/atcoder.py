42cc418cc5c4d34ef06b736dfec9fe4a88a3bafe
beet-aizu <aki.bdash@gmail.com>
2020-03-05 19:23:11 +0900

    add atcoder.py

--- tools/atcoder.py
+++ tools/atcoder.py
@@ -6,10 +6,18 @@ assert (len(args) >= 3)
 assert (not os.path.exists("./{}".format(args[1])))
 
 os.system("mkdir ./{}".format(args[1]))
-os.system("$EDITOR ./{}/t.cpp".format(args[1]))
+os.system("cp ~/.emacs.d/insert/t.cpp ./{}/t.cpp".format(args[1]))
 
 for i in range(int(args[2])):
     os.system("mkdir ./{}/{}".format(args[1], chr(ord("A") + i)))
     os.system("cp ./{}/t.cpp ./{}/{}/{}.cpp".format(args[1], args[1],
                                                     chr(ord("A") + i),
                                                     chr(ord("a") + i)))
+    os.system(
+        "oj d https://atcoder.jp/contests/{}/tasks/{}_{} -d ./{}/{}/test".
+        format(args[1], args[1], chr(ord("a") + i), args[1],
+               chr(ord("A") + i)))
+'''
+usage: write this in your .bashrc
+function atcoder () { python ~/git-library/tools/atcoder.py $1 $2 && cd $1;}
+'''
