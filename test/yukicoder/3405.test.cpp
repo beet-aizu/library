7206c448f3c3d632e8232ea74bca4dc265322c02
beet-aizu <aki.bdash@gmail.com>
2020-03-23 23:46:43 +0900

    add ET for BFS

---
+++ test/yukicoder/3405.test.cpp
@@ -0,0 +1,70 @@
+#define PROBLEM "https://yukicoder.me/problems/3405"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../tree/levelancestor.cpp"
+#include "../../tree/eulertourforbfs.cpp"
+#include "../../segtree/basic/chien.cpp"
+#undef call_from_test
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n;
+  cin>>n;
+  EulerTourForBFS G(n);
+  for(int i=1;i<n;i++){
+    int u,v;
+    cin>>u>>v;
+    G.add_edge(u,v);
+  }
+  G.build();
+
+  vector<int> as(n);
+  for(int i=0;i<n;i++) cin>>as[i];
+
+  using ll = long long;
+  auto f=[&](ll a,ll b){return a+b;};
+  auto g=[&](ll a,ll b){return a*b;};
+  SegmentTree<ll, ll> seg(f,g,g,0,1);
+
+  vector<ll> vs(n);
+  for(int i=0;i<n;i++)
+    vs[G.idx(i)]=as[i];
+  seg.build(vs);
+
+  int q;
+  cin>>q;
+  for(int i=0;i<q;i++){
+    int x;
+    cin>>x;
+
+    ll sum=0;
+    auto apply=
+      [&](int l,int r){
+        sum+=seg.query(l,r);
+        seg.update(l,r,0);
+      };
+
+    int p=G.par[0][x];
+    if(~p){
+      int pp=G.par[0][p];
+      if(~pp) G.exec(pp,0,apply);
+
+      G.exec(p,0,apply);
+      G.exec(p,1,apply);
+    }
+
+    G.exec(x,0,apply);
+    G.exec(x,1,apply);
+    G.exec(x,2,apply);
+
+    seg.set_val(G.idx(x),sum);
+    cout<<sum<<"\n";
+  }
+
+  return 0;
+}
---
+++ tree/eulertourforbfs.cpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#ifndef call_from_test
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "levelancestor.cpp"
+#undef call_from_test
+
+#endif
+
+//BEGIN CUT HERE
+struct EulerTourForBFS : LevelAncestor{
+  using super = LevelAncestor;
+  vector<int> ls;
+  vector<vector<int>> H;
+  EulerTourForBFS(int n):super(n),ls(n),H(n){}
+
+  using super::par;
+  using super::dep;
+  void build(int r=0){
+    super::build(r);
+    int pos=0;
+    queue<int> que;
+    que.emplace(r);
+    while(!que.empty()){
+      int v=que.front();que.pop();
+      ls[v]=pos++;
+      H[dep[v]].emplace_back(v);
+      for(int u:super::G[v]){
+        if(u==par[0][v]) continue;
+        que.emplace(u);
+      }
+    }
+  }
+
+  int idx(int v){return ls[v];}
+
+  int find(int v,int d,int a){
+    int l=-1,r=H[d].size();
+    while(l+1<r){
+      int m=(l+r)>>1;
+      int p=super::up(H[d][m],d-dep[v]);
+      if(ls[v]+a<=ls[p]) r=m;
+      else l=m;
+    }
+    return ls[H[d][0]]+r;
+  }
+
+  template<typename F>
+  void exec(int v,int d,F f){
+    if(dep[v]+d>=n or H[dep[v]+d].empty()) return;
+    int l=find(v,dep[v]+d,0);
+    int r=find(v,dep[v]+d,1);
+    if(l<r) f(l,r);
+  }
+};
+//END CUT HERE
+#ifndef call_from_test
+signed main(){
+  return 0;
+}
+#endif
