1357b267eee56e2df50308c53055932a0b8324be
beet-aizu <aki.bdash@gmail.com>
2020-03-26 19:55:08 +0900

    fix rerooting

--- tree/rerooting.cpp
+++ tree/rerooting.cpp
@@ -8,19 +8,20 @@ using namespace std;
  * @see https://ei1333.hateblo.jp/entry/2018/12/21/004022
  */
 //BEGIN CUT HERE
-template<typename Data, typename T>
+template<typename T, typename Edge>
 struct ReRooting{
   struct Node{
     int to,rev;
-    Data data;
-    Node(int to,int rev,Data data):to(to),rev(rev),data(data){}
+    Edge data;
+    Node(int to,Edge data):to(to),data(data){}
+    bool operator<(const Node &v)const{return to<v.to;};
   };
 
   using F1 = function<T(T, T)>;
-  using F2 = function<T(T, Data)>;
+  using F2 = function<T(T, Edge)>;
 
-  vector<vector<Node> > G;
-  vector<vector<T> > ld,rd;
+  vector< vector<Node> > G;
+  vector< vector<T> > ld,rd;
   vector<int> lp,rp;
 
   const F1 f1;
@@ -30,39 +31,59 @@ struct ReRooting{
   ReRooting(int n,const F1 f1,const F2 f2,const T id):
     G(n),ld(n),rd(n),lp(n),rp(n),f1(f1),f2(f2),id(id){}
 
-  void add_edge(int u,int v,Data d){
-    G[u].emplace_back(v,(int)G[v].size(),d);
-    G[v].emplace_back(u,(int)G[u].size()-1,d);
+  void add_edge(int u,int v,Edge d,Edge e){
+    G[u].emplace_back(v,d);
+    G[v].emplace_back(u,e);
   }
 
-  // p: idx for edge (not vertex)
-  T dfs(int v,int p){
-    while(lp[v]!=p&&lp[v]<(int)G[v].size()){
+  void add_edge(int u,int v,Edge d){add_edge(u,v,d,d);}
+
+  // k: idx for edge (not vertex)
+  T dfs(int v,int k){
+    while(lp[v]!=k and lp[v]<(int)G[v].size()){
       auto &e=G[v][lp[v]];
       ld[v][lp[v]+1]=f1(ld[v][lp[v]],f2(dfs(e.to,e.rev),e.data));
       lp[v]++;
     }
-    while(rp[v]!=p&&rp[v]>=0){
+    while(rp[v]!=k and rp[v]>=0){
       auto &e=G[v][rp[v]];
       rd[v][rp[v]]=f1(rd[v][rp[v]+1],f2(dfs(e.to,e.rev),e.data));
       rp[v]--;
     }
-    if(p<0) return rd[v][0];
-    return f1(ld[v][p],rd[v][p+1]);
+    if(k<0) return rd[v][0];
+    return f1(ld[v][k],rd[v][k+1]);
+  }
+
+  int search(vector<Node> &vs,int idx){
+    return lower_bound(vs.begin(),vs.end(),Node(idx,vs[0].data))-vs.begin();
   }
 
   vector<T> build(){
-    for(int i=0;i<(int)G.size();i++){
+    int n=G.size();
+    for(int i=0;i<n;i++){
+      sort(G[i].begin(),G[i].end());
       ld[i].assign((int)G[i].size()+1,id);
       rd[i].assign((int)G[i].size()+1,id);
       lp[i]=0;
       rp[i]=(int)G[i].size()-1;
     }
+
+    for(int i=0;i<n;i++)
+      for(Node &t:G[i])
+        t.rev=search(G[t.to],i);
+
     vector<T> res;
-    for(int i=0;i<(int)G.size();i++){
+    for(int i=0;i<n;i++)
       res.emplace_back(dfs(i,-1));
-    }
+
     return res;
+  }
+
+  // p: idx for vertex
+  T subtree(int v,int p){
+    int k=search(G[p],v);
+    assert(k<(int)G[p].size() and G[p][k].to==v);
+    return f2(dfs(v,G[p][k].rev),G[p][k].data);
   }
 };
 //END CUT HERE
