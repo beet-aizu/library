39b80b2efacea5c5a3515ac4e2411aa4a85d0efe
beet <20266375+beet-aizu@users.noreply.github.com>
2020-04-22 14:12:56 +0900

    add identity

---
+++ vector/identity.cpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef call_from_test
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+//BEGIN CUT HERE
+vector<int> identity(int n){
+  vector<int> ord(n);
+  iota(ord.begin(),ord.end(),0);
+  return ord;
+}
+//END CUT HERE
+#ifndef call_from_test
+signed main(){
+  return 0;
+}
+#endif
