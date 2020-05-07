360848c91b7a4ba2219df6be0bbb2bd341b448fb
beet <20266375+beet-aizu@users.noreply.github.com>
2020-05-07 17:33:53 +0900

    fix

--- graph/triangle.cpp
+++ graph/triangle.cpp
@@ -9,28 +9,28 @@ struct Triangle{
   // if not simple, use vector<set<int>>
   vector<vector<int>> G;
   Triangle(int n):G(n){}
+
   void add_edge(int u,int v){
     G[u].emplace_back(v);
     G[v].emplace_back(u);
   }
+
   template<typename F>
   void build(F f){
     int n=G.size();
-
-    vector<int> ord(n),rev(n);
-    auto cmp=[&](int i,int j){return G[i].size()>G[j].size();};
-    iota(ord.begin(),ord.end(),0);
-    sort(ord.begin(),ord.end(),cmp);
-    for(int i=0;i<n;i++) rev[ord[i]]=i;
+    using P = pair<int, int>;
+    vector<P> vp(n);
+    for(int i=0;i<n;i++) vp[i]=P(G[i].size(),i);
 
     vector<vector<int>> H(n);
     for(int i=0;i<n;i++)
       for(int j:G[i])
-        if(rev[i]<rev[j]) H[i].emplace_back(j);
+        if(vp[i]>vp[j])
+          H[i].emplace_back(j);
 
     vector<int> used(n,0);
     // x->y->z
-    for(int x:ord){
+    for(int x=0;x<n;x++){
       for(int z:H[x]) used[z]=1;
       for(int y:H[x])
         for(int z:H[y])
