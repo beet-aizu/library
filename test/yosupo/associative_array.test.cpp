9a1e705909f83604afef44d7eed7975cc3701860
beet <20266375+beet-aizu@users.noreply.github.com>
2020-05-24 17:33:44 +0900

    fix

---
+++ test/yosupo/associative_array.test.cpp
@@ -0,0 +1,35 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../datastructure/pb_ds_cc_hash_table.cpp"
+#undef call_from_test
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  const char newl = '\n';
+
+  using ll = long long;
+  gmap<ll, ll> mp;
+
+  int q;
+  cin>>q;
+  for(int i=0;i<q;i++){
+    int t;
+    cin>>t;
+    if(t==0){
+      ll k,v;
+      cin>>k>>v;
+      mp[k]=v;
+    }
+    if(t==1){
+      ll k;
+      cin>>k;
+      cout<<mp[k]<<newl;
+    }
+  }
+  return 0;
+}
