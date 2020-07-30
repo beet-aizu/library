32dfee34d8f814ed9ae535aaaf83f9f4a56c1305
beet <20266375+beet-aizu@users.noreply.github.com>
2020-07-30 16:21:01 +0900

    add read

---
+++ vector/read.cpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef call_from_test
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+//BEGIN CUT HERE
+template<typename T>
+vector<T> read(size_t n){
+  vector<T> ts(n);
+  for(size_t i=0;i<n;i++) cin>>ts[i];
+  return ts;
+}
+//END CUT HERE
+#ifndef call_from_test
+signed main(){
+  return 0;
+}
+#endif
