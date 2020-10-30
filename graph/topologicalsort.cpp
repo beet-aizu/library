9b3cc875c4ccccdbb3c750ff0dbd18ca02dcbddd
beet-aizu <20266375+beet-aizu@users.noreply.github.com>
2020-10-30 12:18:04 +0900

    refactor

--- graph/topologicalsort.cpp
+++ graph/topologicalsort.cpp
@@ -4,37 +4,39 @@ using namespace std;
 #endif
 //BEGIN CUT HERE
 struct TopologicalSort{
-  vector< set<int> > G;
-  vector<int> used,indeg,ps;
-
-  TopologicalSort(int n):G(n),used(n,0),indeg(n,0){}
+  vector<vector<int>> G;
+  vector<int> indeg;
+  TopologicalSort(int n):G(n),indeg(n,0){}
 
   void add_edge(int s,int t){
-    G[s].emplace(t);
+    G[s].emplace_back(t);
     indeg[t]++;
   }
 
-  void bfs(int s){
+  vector<int> build(){
+    int n=G.size();
+
     queue<int> que;
-    que.emplace(s);
-    used[s]=1;
+    vector<int> used(n,0);
+    auto push=[&](int v){
+      if(used[v]) return;
+      que.emplace(v);
+      used[v]=1;
+    };
+
+    for(int i=0;i<n;i++)
+      if(indeg[i]==0) push(i);
+
+    vector<int> ps;
     while(!que.empty()){
       int v=que.front();que.pop();
       ps.emplace_back(v);
       for(int u:G[v]){
         indeg[u]--;
-        if(indeg[u]==0 and !used[u]){
-          used[u]=1;
-          que.emplace(u);
-        }
+        if(indeg[u]==0) push(u);
       }
     }
-  }
 
-  vector<int> build(){
-    int n=G.size();
-    for(int i=0;i<n;i++)
-      if(indeg[i]==0 and !used[i]) bfs(i);
     if(n!=(int)ps.size()) return {};
     return ps;
   }
