e24aef618b7393c4033be4169f45ad310a5f4246
beet-aizu <20266375+beet-aizu@users.noreply.github.com>
2020-11-04 18:01:23 +0900

    c

--- bbst/rbst/impl/persistent.cpp
+++ bbst/rbst/impl/persistent.cpp
@@ -60,8 +60,55 @@ struct Persistent : RBST<Persistent<Data, LIM>, Data, typename Data::Node, LIM>{
 };
 //END CUT HERE
 #ifndef call_from_test
+
+#define call_from_test
+#include "../data/array.cpp"
+#undef call_from_test
+signed JOISC2012_COPYPASTE(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  int m;
+  string buf;
+  cin>>m>>buf;
+
+  using Data = Array<char>;
+  using Node = Data::Node;
+  const size_t LIM = 1e7;
+  Persistent<Data, LIM> pa;
+
+  vector<Node> vs(buf.begin(),buf.end());
+  auto rt=pa.build(vs);
+
+  int n;
+  cin>>n;
+  for(int i=0;i<n;i++){
+    int a,b,c;
+    cin>>a>>b>>c;
+    auto s=pa.split(rt,a);
+    auto t=pa.split(s.second,b-a);
+    auto u=pa.split(rt,c);
+    rt=pa.merge(pa.merge(u.first,t.first),u.second);
+
+    if((int)pa.count(rt)>m)
+      rt=pa.split(rt,m).first;
+
+    if(pa.almost_full()) rt=pa.rebuild(rt);
+  }
+
+  auto ds=pa.data.dump(rt);
+  buf.resize(ds.size());
+  for(int i=0;i<(int)ds.size();i++) buf[i]=ds[i].val;
+  cout<<buf<<endl;
+  return 0;
+}
+/*
+  verified on 2020/11/04
+  https://atcoder.jp/contests/joisc2012/tasks/joisc2012_copypaste
+*/
+
 //INSERT ABOVE HERE
 signed main(){
+  //JOISC2012_COPYPASTE();
   return 0;
 }
 #endif
