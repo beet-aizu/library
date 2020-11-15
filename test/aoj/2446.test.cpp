0f7e7bec726dbe1ba087dca3d15cee023cb47f11
beet-aizu <20266375+beet-aizu@users.noreply.github.com>
2020-11-15 19:03:57 +0900

    add and

---
+++ test/aoj/2446.test.cpp
@@ -0,0 +1,50 @@
+// verification-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2446
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../io/precision.cpp"
+#include "../../convolution/bitwise/fwht.cpp"
+#include "../../convolution/bitwise/and.cpp"
+#undef call_from_test
+
+#define ERROR 1e-7
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  using ll = long long;
+
+  int n;
+  ll m;
+  cin>>n>>m;
+  vector<ll> as(n),ps(n);
+  for(int i=0;i<n;i++) cin>>as[i];
+  for(int i=0;i<n;i++) cin>>ps[i];
+
+  vector<ll> dp(1<<n,0);
+  for(int bit=1;bit<(1<<n);bit++){
+    __int128_t l=1;
+    auto mlcm=[&](auto x,auto y){return x/gcd<ll>(x,y)*y;};
+    for(int i=0;i<n;i++){
+      if((~bit>>i)&1) continue;
+      l=mlcm(l,as[i]);
+      if(l>m) break;
+    }
+    dp[bit]=m/l;
+  }
+
+  fwht(dp,moebius);
+
+  double ans=0;
+  for(int bit=0;bit<(1<<n);bit++){
+    double po=1;
+    for(int i=0;i<n;i++)
+      if((bit>>i)&1) po*=ps[i]/100.0;
+      else po*=1.0-(ps[i]/100.0);
+    ans+=po*abs(dp[bit]);
+  }
+  cout<<ans<<endl;
+  return 0;
+}
