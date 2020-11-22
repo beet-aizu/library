ede7b42120d782965305ade4c91a6fa8b72d4a01
beet-aizu <20266375+beet-aizu@users.noreply.github.com>
2020-11-22 17:38:56 +0900

    add /includes

---
+++ includes/formalpowerseries.h
@@ -0,0 +1,22 @@
+#ifndef call_from_test
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+//BEGIN CUT HERE
+#define call_from_test
+#include "../mod/mint.cpp"
+#include "../combinatorics/enumeration.cpp"
+#include "../formalpowerseries/base.cpp"
+#include "../formalpowerseries/inv.cpp"
+#include "../formalpowerseries/div.cpp"
+#include "../formalpowerseries/mod.cpp"
+#include "../formalpowerseries/sqrt.cpp"
+#include "../formalpowerseries/diff.cpp"
+#include "../formalpowerseries/integral.cpp"
+#include "../formalpowerseries/log.cpp"
+#include "../formalpowerseries/exp.cpp"
+#include "../formalpowerseries/pow.cpp"
+#include "../formalpowerseries/shift.cpp"
+#undef call_from_test
+//END CUT HERE
