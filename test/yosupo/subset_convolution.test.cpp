6f3e357dfe69e602e02632df0ac268901b57d480
beet-aizu <20266375+beet-aizu@users.noreply.github.com>
2020-12-16 13:00:21 +0900

    add subset convolution

---
+++ test/yosupo/subset_convolution.test.cpp
@@ -0,0 +1,45 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/bitwise_and_convolution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../mod/mint.cpp"
+#include "../../convolution/bitwise/fwht.cpp"
+#include "../../convolution/bitwise/or.cpp"
+#undef call_from_test
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n;
+  cin>>n;
+
+  using M = Mint<int, 998244353>;
+  using V = valarray<M>;
+
+  int sz=1<<n;
+
+  vector<V> as(sz,V(n+1)),bs(sz,V(n+1));
+  for(int i=0;i<sz;i++) cin>>as[i][__builtin_popcount(i)].v;
+  for(int i=0;i<sz;i++) cin>>bs[i][__builtin_popcount(i)].v;
+
+  fwht(as,zeta);
+  fwht(bs,zeta);
+
+  vector<V> cs(sz,V(n+1));
+  for(int i=0;i<sz;i++)
+    for(int j=0;j<=n;j++)
+      for(int k=0;j+k<=n;k++)
+        cs[i][j+k]+=as[i][j]*bs[i][k];
+
+  fwht(cs,moebius);
+
+  for(int i=0;i<sz;i++){
+    if(i) cout<<' ';
+    cout<<cs[i][__builtin_popcount(i)];
+  }
+  cout<<endl;
+  return 0;
+}
