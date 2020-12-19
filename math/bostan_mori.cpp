5ddf52388e4176d39ed1f5e3fca13c0287f75080
beet-aizu <20266375+beet-aizu@users.noreply.github.com>
2020-12-19 16:10:28 +0900

    #69

---
+++ math/bostan_mori.cpp
@@ -0,0 +1,46 @@
+#ifndef call_from_test
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+//BEGIN CUT HERE
+// Find k-th term of linear recurrence
+template<typename T>
+struct BostanMori{
+  using Poly = vector<T>;
+  using Conv = function<Poly(Poly, Poly)>;
+
+  Conv conv;
+  BostanMori(Conv conv_):conv(conv_){}
+
+  Poly sub(Poly as,int odd){
+    Poly bs((as.size()+!odd)/2);
+    for(int i=odd;i<(int)as.size();i+=2) bs[i/2]=as[i];
+    return bs;
+  }
+
+  // as: initial values
+  // cs: monic polynomial
+  T build(long long k,Poly as,Poly cs){
+    reverse(cs.begin(),cs.end());
+    assert(cs[0]==T(1));
+    int n=cs.size()-1;
+    as.resize(n,0);
+    Poly bs=conv(as,cs);
+    bs.resize(n);
+    while(k){
+      Poly ds(cs);
+      for(int i=1;i<(int)ds.size();i+=2) ds[i]=-ds[i];
+      bs=sub(conv(bs,ds),k&1);
+      cs=sub(conv(cs,ds),0);
+      k>>=1;
+    }
+    return bs[0];
+  }
+};
+//END CUT HERE
+#ifndef call_from_test
+//INSERT ABOVE HERE
+signed main(){
+  return 0;
+}
+#endif
--- test/aoj/0168.test.cpp
+++ test/aoj/0168.test.cpp
@@ -1,32 +1,29 @@
 // verification-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0168
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define call_from_test
-#include "../../math/kitamasa.cpp"
+#include "../../math/bostan_mori.cpp"
 #undef call_from_test
 
-template<typename T>
-struct Z{
-  T v;
-  Z(){*this=add_identity();}
-  Z(T v):v(v){}
-  Z operator+(const Z &a)const{return Z(v+a.v);}
-  Z operator*(const Z &a)const{return Z(v*a.v);}
-  static Z add_identity(){return Z(0);}
-  static Z mul_identity(){return Z(1);}
-};
-
 signed main(){
-  using ZL = Z<long long>;
+  using ll = long long;
+  using Poly = vector<ll>;
+  auto conv=[](Poly as, Poly bs){
+    Poly cs(as.size()+bs.size()-1,0);
+    for(int i=0;i<(int)as.size();i++)
+      for(int j=0;j<(int)bs.size();j++)
+        cs[i+j]+=as[i]*bs[j];
+    return cs;
+  };
+  BostanMori<ll> bm(conv);
 
-  vector<ZL> as({0,0,1}),cs({1,1,1});
-  Kitamasa<ZL> K(cs);
+  Poly as({0,0,1}),cs({-1,-1,-1,1});
 
   int n;
   while(cin>>n,n)
-    cout<<(K.calc(as,n+2).v+3650-1)/3650<<endl;
+    cout<<(bm.build(n+2,as,cs)+3650-1)/3650<<endl;
 
   return 0;
 }
