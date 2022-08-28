8a1018b9563150a652addd44de23f48c19e173be
beet-aizu <20266375+beet-aizu@users.noreply.github.com>
2022-08-28 13:59:29 +0900

    verify segtree/2d/ushi.cpp

--- segtree/2d/ushi.cpp
+++ segtree/2d/ushi.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 #endif
 //BEGIN CUT HERE
-template<typename SegmentTree>
+template<typename index_t,typename SegmentTree>
 struct SegmentTree2D{
   using F = typename SegmentTree::F;
   using T = typename F::result_type;
@@ -11,23 +11,24 @@ struct SegmentTree2D{
   const T ti;
   SegmentTree2D(F f_,T ti_):f(f_),ti(ti_){}
 
-  vector<pair<int, int>> vp;
-  void preupdate(int p,int q){
+  vector<pair<index_t, index_t>> vp;
+  void preupdate(index_t p,index_t q){
     vp.emplace_back(p,q);
   }
 
-  void compress(vector<int> &vs){
+  void compress(vector<index_t> &vs){
     sort(vs.begin(),vs.end());
     vs.erase(unique(vs.begin(),vs.end()),vs.end());
   }
 
-  int idx(int v,const vector<int> &vs){
+  int idx(index_t v,const vector<index_t> &vs){
     return lower_bound(vs.begin(),vs.end(),v)-vs.begin();
   }
 
   int sz;
-  vector<int> ps;
-  vector<vector<int>> I,L,R;
+  vector<index_t> ps;
+  vector<vector<index_t>> I;
+  vector<vector<int>> L,R;
   vector<SegmentTree> seg;
   void build(){
     for(auto[p,q]:vp) ps.emplace_back(p);
@@ -43,7 +44,9 @@ struct SegmentTree2D{
     L.resize(sz);
     R.resize(sz);
     for(int k=sz-1;k>0;k--){
-      auto& cur=I[k],lft=I[(k<<1)|0],rgh=I[(k<<1)|1];
+      auto& cur=I[k];
+      const auto& lft=I[(k<<1)|0];
+      const auto& rgh=I[(k<<1)|1];
       cur.resize(lft.size()+rgh.size());
       merge(lft.begin(),lft.end(),rgh.begin(),rgh.end(),cur.begin());
       cur.erase(unique(cur.begin(),cur.end()),cur.end());
@@ -71,7 +74,9 @@ struct SegmentTree2D{
     update(p,L[k][q],v,(k<<1)|0,l,m);
     update(p,R[k][q],v,(k<<1)|1,m,r);
     T res=ti;
-    const auto& cur=I[k],lft=I[(k<<1)|0],rgh=I[(k<<1)|1];
+    const auto& cur=I[k];
+    const auto& lft=I[(k<<1)|0];
+    const auto& rgh=I[(k<<1)|1];
     if(L[k][q]<(int)lft.size() and cur[q]==lft[L[k][q]])
       res=f(res,seg[(k<<1)|0].query(L[k][q],L[k][q]+1));
     if(R[k][q]<(int)rgh.size() and cur[q]==rgh[R[k][q]])
@@ -79,7 +84,7 @@ struct SegmentTree2D{
     seg[k].set_val(q,res);
   }
 
-  void update(int p,int q,T v){
+  void update(index_t p,index_t q,T v){
     update(idx(p,ps),idx(q,I[1]),v,1,0,sz);
   }
 
@@ -92,7 +97,7 @@ struct SegmentTree2D{
   }
 
   // [pa, pb) x [qa, qb)
-  T query(int pa,int pb,int qa,int qb){
+  T query(index_t pa,index_t pb,index_t qa,index_t qb){
     return query(idx(pa,ps),idx(pb,ps),idx(qa,I[1]),idx(qb,I[1]),1,0,sz);
   }
 };
@@ -101,58 +106,53 @@ struct SegmentTree2D{
 
 #define call_from_test
 #include "../basic/ushi.cpp"
+#include "../../vector/zip.cpp"
 #include "../../tools/chminmax.cpp"
 #undef call_from_test
 
-signed ACPC2021DAY2_H(){
-  cin.tie(0);
-  ios::sync_with_stdio(0);
+signed ABC266_H(){
+  int n;
+  cin>>n;
+  vector<int> ts(n),xs(n),ys(n),as(n);
+  for(int i=0;i<n;i++) cin>>ts[i]>>xs[i]>>ys[i]>>as[i];
 
-  int n,m;
-  cin>>n>>m;
-  map<pair<int, int>, int> mp;
-  for(int i=0;i<m;i++){
-    int y,x,a;
-    cin>>y>>x>>a;
-    mp[{y,x}]=a;
+  {
+    auto zs=zip(ys,ts,xs,as);
+    sort(zs.begin(),zs.end());
+    for(int i=0;i<n;i++) tie(ys[i],ts[i],xs[i],as[i])=zs[i];
+  }
+  vector<int> bs(n),cs(n);
+  for(int i=0;i<n;i++){
+    bs[i]=ts[i]-ys[i]+xs[i];
+    cs[i]=ts[i]-ys[i]-xs[i];
   }
 
-  const int INF = 1e9+7;
-  using P = pair<int, int>;
-  auto f1=[&](int a,int b){return min(a,b);};
-  auto f2=[&](int a,int b){return a+b;};
-  auto f=[&](P a,P b){return P(f1(a.first,b.first),f2(a.second,b.second));};
-
-  SegmentTree2D<SegmentTree<P>> seg(f,P(INF,0));
-
-  for(auto[pos,a]:mp){
-    auto[y,x]=pos;
-    seg.preupdate(y,x);
+  using ll = long long;
+  auto f=[&](ll a,ll b){return max(a,b);};
+  SegmentTree2D<int, SegmentTree<ll>> seg(f,0LL);
+  for(int i=0;i<n;i++){
+    seg.preupdate(bs[i],cs[i]);
   }
   seg.build();
 
-  for(auto[pos,a]:mp){
-    auto[y,x]=pos;
-    seg.update(y,x,P(a,1));
+  ll ans=0;
+  for(int i=0;i<n;i++){
+    if(ts[i]<xs[i]+ys[i]) continue;
+    ll res=as[i]+seg.query(0,bs[i]+1,0,cs[i]+1);
+    seg.update(bs[i],cs[i],res);
+    chmax(ans,res);
   }
+  cout<<ans<<endl;
 
-  int q;
-  cin>>q;
-  for(int i=0;i<q;i++){
-    using ll = long long;
-    ll u,d,l,r;
-    cin>>u>>d>>l>>r;
-    d++;r++;
-    P res=seg.query(u,d,l,r);
-    ll num=(d-u)*(r-l);
-    if(res.second<num) chmin(res.first,0);
-    cout<<res.first<<'\n';
-  }
   return 0;
 }
+/*
+  verified on 2022/08/28
+  https://atcoder.jp/contests/abc266/tasks/abc266_h
+*/
 
 signed main(){
-  ACPC2021DAY2_H();
+  ABC266_H();
   return 0;
 }
 #endif
---
+++ test/aoj/3518.test.cpp
@@ -0,0 +1,57 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=3518"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../segtree/basic/ushi.cpp"
+#include "../../segtree/2d/ushi.cpp"
+#include "../../tools/chminmax.cpp"
+#undef call_from_test
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int n,m;
+  cin>>n>>m;
+  map<pair<int, int>, int> mp;
+  for(int i=0;i<m;i++){
+    int y,x,a;
+    cin>>y>>x>>a;
+    mp[{y,x}]=a;
+  }
+
+  const int INF = 1e9+7;
+  using P = pair<int, int>;
+  auto f1=[&](int a,int b){return min(a,b);};
+  auto f2=[&](int a,int b){return a+b;};
+  auto f=[&](P a,P b){return P(f1(a.first,b.first),f2(a.second,b.second));};
+
+  SegmentTree2D<int, SegmentTree<P>> seg(f,P(INF,0));
+
+  for(auto[pos,a]:mp){
+    auto[y,x]=pos;
+    seg.preupdate(y,x);
+  }
+  seg.build();
+
+  for(auto[pos,a]:mp){
+    auto[y,x]=pos;
+    seg.update(y,x,P(a,1));
+  }
+
+  int q;
+  cin>>q;
+  for(int i=0;i<q;i++){
+    using ll = long long;
+    ll u,d,l,r;
+    cin>>u>>d>>l>>r;
+    d++;r++;
+    P res=seg.query(u,d,l,r);
+    ll num=(d-u)*(r-l);
+    if(res.second<num) chmin(res.first,0);
+    cout<<res.first<<'\n';
+  }
+  return 0;
+}
